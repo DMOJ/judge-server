1b4f3bf4e4b7a934ac6e573468619557da3520fe
Tudor Brinedus <tbrindus@gmail.com>
2014-03-14 13:06:37 -0400

    Let server define submission id
    Allows for potential rejudging of submissions

--- packet.py
+++ packet.py
@@ -1,11 +1,6 @@
 import uuid
  
-class PacketManager(object):
- 
-        STATUS_TLE = "TLE"
-        STATUS_WA = "WA"
-        STATUS_OK = "OK"
- 
+class PacketManager(object):   
         def __init__(self, host, port, judge):
                 self.judge = judge
  
@@ -15,40 +10,35 @@ class PacketManager(object):
         def recieve_packet(self, packet):
                 name = packet["name"]
                 if name == "submission-request":
-                        self.submission_handshake_packet()
+                        self.judge.current_submission = packet["submission-id"]
                         self.judge.begin_grading(packet["problem-id"], packet["language"], packet["source"])
                 elif name == "get-current-submission":
                         self.current_submission_packet()
                 else:
                         print "ERROR: unknown packet %s, payload %s" % (name, packet)
  
-        def submission_handshake_packet(self):
-                self.judge.current_submission = uuid.uuid1().hex
-                self.send_packet({   "name": "handshake",
-                                "submission-id": self.judge.current_submission})
- 
         def test_case_status_packet(self, status, time, memory, output):
-                self.send_packet({   "name": "test-case-status",
-                                "submission-id": self.judge.current_submission,
-                                "status": status,
-                                "time": time,
-                                "memory": memory,
-                                "output": output})
+                self.send_packet({"name":               "test-case-status",
+                                  "submission-id":      self.judge.current_submission,
+                                  "status":             status,
+                                  "time":               time,
+                                  "memory":             memory,
+                                  "output":             output})
  
         def compile_error_packet(self, log):
-                self.send_packet({   "name": "compile-error",
-                                "submission-id": self.judge.current_submission,
-                                "log": log})
+                self.send_packet({"name":               "compile-error",
+                                  "submission-id":      self.judge.current_submission,
+                                  "log":                log})
  
         def begin_grading_packet(self):
-                self.send_packet({   "name": "grading-begin",
-                                "submission-id": self.judge.current_submission})
+                self.send_packet({"name":               "grading-begin",
+                                  "submission-id":      self.judge.current_submission})
  
         def grading_end_packet(self):
-                self.send_packet({   "name": "grading-end",
-                                "submission-id": self.judge.current_submission})
+                self.send_packet({"name":               "grading-end",
+                                  "submission-id":      self.judge.current_submission})
                 self.judge.current_submission = None
  
         def current_submission_packet(self):
-                self.send_packet({   "name": "current-submission-id",
-                                "submission-id": self.judge.current_submission})
+                self.send_packet({"name":               "current-submission-id",
+                                  "submission-id":      self.judge.current_submission})

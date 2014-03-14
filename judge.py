63939926fe266bf98e53a9e6959925fa0b03e467
Tim <li.yikuan@gmail.com>
2014-03-14 11:57:10 -0400

    fixed read deadlock

--- judge.py
+++ judge.py
@@ -2,16 +2,20 @@
 import os, Queue, subprocess, sys, thread, threading, time
 
 class Result(object):
+    AC=0x0
+    WA=0x1
     def __init__(self):
         self.start_time=None
         self.end_time=None
         self.elasped_time=None
         self.max_memory=None
+        self.result_flag=None
     def start(self):
         self.start_time=time.clock()
-    def stop(self):
+    def stop(self, result_flag=None):
         self.end_time=time.clock()
         self.elasped_time=self.end_time-self.start_time
+        self.result_flag=result_flag
     def memory_hook(self, lock, function, *args):
         while lock.acquire(False):
             self.update_memory(function(*args))
@@ -25,13 +29,16 @@ class Results(object):
     def __init__(self, total_results):
         self.results=[Result()]*total_results
         self.judge=None
+    def __getitem__(self, index):
+        return self.results[index]
     def link(self, judge):
         self.judge=judge
-    def run(self):
-        for result in self.results:
-            self.judge.run(result)
+    def run(self, input_files, output_files):
+        for result, input_file, output_file in zip(self.results, input_files, output_files):
+            self.judge.run(result, input_file, output_file)
 
 class Judge(object):
+    EOF=""
     def __init__(self, processname, language="", redirect=False, transfer=False, interact=False, results=None):
         if sys.platform=="win32":
             import memory # @UnresolvedImport
@@ -50,15 +57,7 @@ class Judge(object):
             sys.stdout=self
         self.results.link(self)
     def __del__(self):
-        if self.alive():
-            if self.result is not None:
-                self.result.stop()
-                self.result.update_memory(self.max_memory_usage())
-                self.memory_lock.acquire()
-            sys.stdin=self.old_stdin
-            sys.stdout=self.old_stdout
-            self.process.terminate()
-            self.stopped=True
+        self.close(True)
     def __enter__(self):
         return self
     def __exit__(self, exception_type, exception_value, traceback):
@@ -70,19 +69,19 @@ class Judge(object):
                 sys.stdin=self.old_stdin
                 sys.stdout=self.old_stdout
                 self.stopped=True
-                if self.result is not None:
-                    self.result.stop()
-                    self.memory_lock.acquire()
-        return not self.stopped
-    def close(self):
-        if self.alive():
-            if self.result is not None:
                 self.result.stop()
-                self.result.update_memory(self.max_memory_usage())
+                self.write_lock.acquire()
                 self.memory_lock.acquire()
+        return not self.stopped
+    def close(self, force_terminate=False):
+        if self.alive():
+            self.result.stop()
+            self.write_lock.acquire()
+            self.result.update_memory(self.max_memory_usage())
+            self.memory_lock.acquire()
             sys.stdin=self.old_stdin
             sys.stdout=self.old_stdout
-            if self.interact:
+            if force_terminate or self.interact:
                 self.process.terminate()
             else:
                 self.exitcode=self.process.wait()
@@ -96,24 +95,37 @@ class Judge(object):
         while not line and self.alive():
             line=self.process.stdout.readline().rstrip()
         return line.rstrip() if line else ""
-    def run(self, result):
+    def run(self, result, input_file, output_file):
         self.exitcode=None
         self.stopped=False
         self.write_queue=Queue.Queue()
+        self.write_lock=threading.Lock()
+        self.memory_lock=threading.Lock()
         self.process=subprocess.Popen([self.processname], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        if result is not None:
-            self.memory_lock=threading.Lock()
-            result.start()
-            thread.start_new_thread(result.memory_hook, (self.memory_lock, self.max_memory_usage,))
+        self.result=result
+        self.result.start()
+        thread.start_new_thread(result.memory_hook, (self.memory_lock, self.max_memory_usage,))
         if self.transfer:
             self.write(sys.stdin.read())
-        thread.start_new_thread(self.write_async, ())
+        thread.start_new_thread(self.write_async, (self.write_lock,))
+        result_flag=0
+        with open(input_file, "r") as fi, open(output_file, "r") as fo:
+            self.write(fi.read().strip())
+            self.write(Judge.EOF)
+            process_output=self.read().strip().replace('\r\n', '\n')
+            judge_output=fo.read().strip().replace('\r\n', '\n')
+            if process_output!=judge_output:
+                result_flag|=Result.WA
+        self.close()
+        self.stopped=True
+        self.result.stop(result_flag)
     def write(self, data):
         self.write_queue.put_nowait(data)
-    def write_async(self):
+    def write_async(self, write_lock):
         try:
             while True:
-                while self.alive():
+                while write_lock.acquire(False) and self.alive():
+                    write_lock.release()
                     try:
                         data=self.write_queue.get(False, 1)
                         break
@@ -121,11 +133,15 @@ class Judge(object):
                         pass
                 else:
                     break
-                data=data.replace('\r\n', '\n').replace('\r', '\n')
-                self.process.stdin.write(data)
-                if data=='\n':
-                    self.process.stdin.flush()
-                    os.fsync(self.process.stdin.fileno())
+                if data==Judge.EOF:
+                    self.process.stdin.close()
+                    break
+                else:
+                    data=data.replace('\r\n', '\n').replace('\r', '\n')
+                    self.process.stdin.write(data)
+                    if data=='\n':
+                        self.process.stdin.flush()
+                        os.fsync(self.process.stdin.fileno())
         except:
             pass
     def max_memory_usage(self):
@@ -134,26 +150,23 @@ class Judge(object):
         pass
 
 def main():
-    if len(sys.argv)<2: # gonna use command line later
+    if len(sys.argv)<2:
         print "Invalid arguments"
         #sys.exit()
         #sys.argv.append("Rivers.exe")
         sys.argv.append("Segment_Tree_Test.exe")
-    res=Result()
-    with open("segtree10.in", "r") as fi:
-        data=fi.read()
-    with Judge(sys.argv[1], redirect=True, interact=False, result=res) as process:
+    res=Results(1)
+    with Judge(sys.argv[1], redirect=False, interact=False, results=res) as judge:
         try:
-            print data.strip()
-            with open("segtree10.out", "r") as fo:
-                one=process.read().strip().replace('\r\n', '\n')
-                two=fo.read().strip().replace('\r\n', '\n')
-                if one!=two:
-                    print>>sys.__stdout__, "Mismatch"
+            res.run(["segtree10.in"], ["segtree10.out"])
         except Exception, e:
-            print>>process.old_stdout, e
-    print res.elasped_time, "seconds"
-    print res.max_memory/1024.0/1024.0, "MB"
+            print e
+    print res[0].elasped_time, "seconds"
+    print res[0].max_memory/1024.0/1024.0, "MB"
+    if res[0].result_flag&Result.WA:
+        print "Wrong Answer"
+    else:
+        print "Accepted"
     print "Done"
 
 if __name__=="__main__":

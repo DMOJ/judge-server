fe2acad112cb1148aa0fb6deda68d2270bbcc430
Tim <li.yikuan@gmail.com>
2014-03-18 18:15:41 -0400

    fixed python, added c++

--- aplusb.py
+++
@@ -1,2 +0,0 @@
-for n in xrange(int(raw_input())):
-    print sum(map(int, raw_input().split()))
--- judge.py
+++ judge.py
@@ -2,11 +2,15 @@
 import argparse
 import json
 import os
-import Queue
 import traceback
 import sys
 import thread
 import threading
+
+try:
+    import queue
+except ImportError:
+    import Queue as queue    
 
 import execute
 
@@ -40,13 +44,16 @@ class Judge(object):
         else:
             openfile = open
         self.packet_manager.begin_grading_packet()
+        case = 1
         for input_file, output_file, point_value in io_files:
-            case = 1
-            with ProgramJudge(arguments, *args) as judge:
+            # TODO: get time and memory limits
+            with ProgramJudge(arguments, 5, 16384, *args) as judge:
                 result = Result()
                 judge.run(result, openfile(input_file), openfile(output_file))
-                # TODO: get points
-                self.packet_manager.test_case_status_packet(case, point_value if result.result_flag == Result.AC else 0, point_value, result.result_flag, result.execution_time,
+                self.packet_manager.test_case_status_packet(case,
+                                                            point_value if result.result_flag == Result.AC else 0,
+                                                            point_value,
+                                                            result.result_flag, result.execution_time,
                                                             result.max_memory,
                                                             result.partial_output)
                 case += 1
@@ -54,18 +61,38 @@ class Judge(object):
         self.packet_manager.grading_end_packet()
 
     def begin_grading(self, problem_id, language, source_code):
-        if language not in ["PY2"]:
-            raise Exception("Not implemented!")
+        if language == "PY2":
+            source_code_file = str(self.current_submission) + ".py"
+            with open(source_code_file, "wb") as fo:
+                fo.write(source_code)
+            arguments = [sys.executable, source_code_file]
+        elif language == "CPP":
+            source_code_file = str(self.current_submission) + ".cpp"
+            with open(source_code_file, "wb") as fo:
+                fo.write(source_code)
+            if sys.platform == "win32":
+                compiled_extension = ".exe"
+                linker_options = "-Wl,--stack,8388608"
+            else:
+                compiled_extension = ""
+                linker_options = ""
+            output_file = str(self.current_submission) + compiled_extension
+            gcc_args = "g++ %s -O2 -std=c++0x %s -static -s -o %s" % (source_code_file, linker_options, output_file)
+            if os.system(gcc_args) != 0:
+                self.packet_manager.compile_error_packet("not implemented yet!")
+                return
+            arguments = [output_file]
+        else:
+            raise Exception("not implemented yet!")
         with open(os.path.join("data", "problems", problem_id, "init.json"), "r") as init_file:
             init_data = json.load(init_file)
             problem_type = init_data["type"]
-            data = os.path.join("data", "problems", problem_id)
+            if problem_type != "standard":
+                raise Exception("not implemented yet!")
             test_cases = init_data["test_cases"]
-            forward_test_cases = []
-            for case in test_cases:
-                forward_test_cases.append((case["in"], case["out"], case["points"]))
+            forward_test_cases = [(case["in"], case["out"], case["points"]) for case in test_cases]
             case = 1
-            for res in self.run([sys.executable, source_code], forward_test_cases, archive=os.path.join("data", "problems", problem_id, init_data["archive"])):
+            for res in self.run(arguments, forward_test_cases, archive=os.path.join("data", "problems", problem_id, init_data["archive"])):
                 print "Test case %s" % case
                 print "\t%f seconds" % res.execution_time
                 print "\t%.2f mb (%s kb)" % (res.max_memory / 1024.0, res.max_memory)
@@ -93,17 +120,17 @@ class LocalJudge(Judge):
                 return lambda *args, **kwargs: None
 
         self.packet_manager = LocalPacketManager()
-        self.current_submission = None
+        self.current_submission = "submission"
 
 
 class ProgramJudge(object):
     EOF = None
 
-    def __init__(self, process_name, redirect=False, transfer=False, interact=False):
+    def __init__(self, process_name, time_limit, memory_limit, redirect=False, transfer=False, interact=False):
         self.result = None
-        self.process = execute.execute(process_name, 5, 16384)
+        self.process = execute.execute(process_name, time_limit, memory_limit)
         self.write_lock = threading.Lock()
-        self.write_queue = Queue.Queue()
+        self.write_queue = queue.Queue()
         self.stopped = False
         self.exitcode = None
         self.process_name = process_name
@@ -166,8 +193,8 @@ class ProgramJudge(object):
         if self.transfer:
             self.write(sys.stdin.read())
         thread.start_new_thread(self.write_async, (self.write_lock,))
-        result_flag = 0
-        self.write(input_file.read().strip())
+        result_flag = Result.AC
+        self.write(input_file.read())
         self.write(ProgramJudge.EOF)
         process_output = self.read().strip().replace('\r\n', '\n')
         self.result.partial_output = process_output[:10]
@@ -182,29 +209,33 @@ class ProgramJudge(object):
         self.write_queue.put_nowait(data)
 
     def write_async(self, write_lock):
+        stdin = self.process.stdin
+        write_queue = self.write_queue
         try:
+            import time
             while True:
                 while write_lock.acquire(False) and self.alive():
                     write_lock.release()
                     try:
-                        data = self.write_queue.get(False, 1)
+                        data = write_queue.get(True, 1)
                         break
-                    except:
+                    except queue.Empty:
                         pass
                 else:
                     break
                 if data is ProgramJudge.EOF:
-                    self.process.stdin.close()
+                    stdin.close()
                     break
                 else:
                     data = data.replace('\r\n', '\n').replace('\r', '\n')
                     try:
-                        self.process.stdin.write(data)
+                        # BUGFIX: write takes a long time
+                        stdin.write(data)
                     except IOError:
                         break
                     if data == '\n':
-                        self.process.stdin.flush()
-                        os.fsync(self.process.stdin.fileno())
+                        stdin.flush()
+                        os.fsync(stdin.fileno())
         except Exception:
             traceback.print_exc()
 
@@ -221,9 +252,31 @@ def main():
 
     print "Running %s judge..." % (["local", "live"][args.server_host is not None])
 
+    cpp_source=r'''#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    int N, a, b;
+    scanf("%d",  &N);
+    for(int i=0; i<N; i++)
+    {
+        scanf("%d%d", &a, &b);
+        printf("%d\n", a+b);
+    }
+
+    return 0;
+}
+'''
+    py2_source=r'''for i in xrange(int(raw_input())):
+    print sum(map(int, raw_input().split()))
+'''
     with (LocalJudge if args.server_host is None else Judge)(args.server_host, args.server_port) as judge:
         try:
-            judge.begin_grading("aplusb", "PY2", "aplusb.py")
+            #judge.begin_grading("aplusb", "CPP", cpp_source)
+            judge.begin_grading("aplusb", "PY2", py2_source)
         except Exception:
             traceback.print_exc()
 

b67533ee1f108bb719f4408498b33f0a483dd1bf
Tudor Brinedus <tbrindus@gmail.com>
2014-03-18 12:27:47 -0400

    Add configurable limits to judge; make judge more tolerable of empty strings

--- nix_execute.py
+++ nix_execute.py
@@ -4,6 +4,7 @@ import os
 import time
 import resource
 import subprocess
+import argparse
 
 
 class nix_Process(object):
@@ -41,14 +42,23 @@ class nix_Process(object):
         return self._get_usages()[1]
 
 
-def execute(path):
-    process = subprocess.Popen([sys.executable, __file__] + path, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def execute(path, time, memory):
+    process = subprocess.Popen([sys.executable, __file__, '-t', str(time), '-m', str(memory), '--'] + path,
+                               stdin=subprocess.PIPE,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
     return nix_Process(process)
 
 
 if __name__ == "__main__":
-    child = sys.argv[1]
-    args = sys.argv[1:]
+    parser = argparse.ArgumentParser(description="Runs and monitors a process' usage stats on *nix systems")
+    parser.add_argument("child_args", nargs="+", help="The child process path followed by arguments; relative allowed")
+    parser.add_argument("-t", "--time", type=float, help="Time to limit process to, in seconds")
+    parser.add_argument("-m", "--memory", type=int, help="Memory to limit process to, in kb")
+    parsed = parser.parse_args()
+
+    child = parsed.child_args[0]
+    child_args = parsed.child_args
 
     pid = None
     rusage = None
@@ -64,21 +74,29 @@ if __name__ == "__main__":
     h = signal.signal(signal.SIGCHLD, sigchld)
     pid = os.fork()
     if not pid:
-        resource.setrlimit(resource.RLIMIT_AS, (32 * 1024 * 1024,) * 2)
+        resource.setrlimit(resource.RLIMIT_AS, (parsed.memory * 1024 + 16 * 1024 * 1024,) * 2)
         #resource.setrlimit(resource.RLIMIT_NOFILE, (4, 4))
         resource.setrlimit(resource.RLIMIT_NPROC, (0, 0))
 
+        # Merge the stderr (2) into stdout (1) so that the execute
+        # may be able to return usage stats through stderr
         os.dup2(1, 2)
+        # Close all file descriptors that are not standard
         os.closerange(3, os.sysconf("SC_OPEN_MAX"))
-        os.execvp(child, args)
-        os._exit(3306)  # When you reach here, you are screwed
+        # Replace current process with the child process
+        # This call does not return
+        os.execvp(child, child_args)
+        # Unless it does, of course, in which case you're screwed
+        # We don't cover this in the warranty
+        #  When you reach here, you are screwed
         # As much as being handed control of a MySQL server without
         # ANY SQL knowledge or docs. ENJOY.
+        os._exit(3306)
     else:
         sys.stdin.close()
         sys.stdout.close()
         start = time.time()
-        time.sleep(5)
+        time.sleep(parsed.time)
         duration = time.time() - start
         signal.signal(signal.SIGCHLD, h)
         tle = 0

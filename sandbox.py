296f4e075b738ebde6d938e2f98334e157b06834
Tudor Brinedus <tbrindus@gmail.com>
2014-03-28 21:52:40 -0400

    Implement proxying; disallow sys_write where fd != 1, 2

--- sandbox.py
+++ sandbox.py
@@ -7,15 +7,10 @@ import ctypes
 from posix import *
 from signal import *
 
-
-def os_bits():
-    return {'AMD64': 64, 'x86_64': 64, 'i386': 32, 'x86': 32}.get(
-        os.environ.get("PROCESSOR_ARCHITEW6432", os.environ.get('PROCESSOR_ARCHITECTURE', '')), None)
-
-
 libc = ctypes.CDLL('libc.so.6', use_errno=True)
 ptrace = libc.ptrace
 
+# ptrace constants
 PTRACE_TRACEME = 0
 PTRACE_PEEKDATA = 2
 PTRACE_GETREGS = 12
@@ -24,6 +19,7 @@ PTRACE_ATTACH = 8
 PTRACE_CONT = 7
 PTRACE_PEEKUSR = 3
 
+# x64 registers
 R15 = 0
 R14 = 1
 R13 = 2
@@ -52,74 +48,113 @@ ES = 24
 FS = 25
 GS = 26
 
-sys_read = 0
-sys_write = 1
-sys_open = 2
-sys_close = 3
-sys_stat = 4
-sys_fstat = 5
-sys_mmap = 9
-sys_mprotect = 10
-sys_munmap = 11
-sys_brk = 12
-sys_fcntl = 72  # Needed for Java and PHP to run
-sys_clone = 56
-sys_fork = 57
-sys_access = 21
-sys_arch_prctl = 158
-sys_set_tid_address = 218
-sys_set_robust_list = 273
-sys_futex = 202
-sys_rt_sigaction = 13
-sys_rt_sigprocmask = 14
-sys_getrlimit = 97
-sys_ioctl = 16
-sys_readlink = 89
-sys_getcwd = 79
-sys_geteuid = 107
-sys_getuid = 102
-sys_getegid = 108
-sys_getgid = 104
-sys_lstat = 6
-sys_openat = 257
-sys_getdents = 78
+# syscall ids
+# @formatter:off
+calls = {
+    'sys_read':             0,
+    'sys_write':            1,
+    'sys_open':             2,
+    'sys_close':            3,
+    'sys_stat':             4,
+    'sys_fstat':            5,
+    'sys_mmap':             9,
+    'sys_mprotect':         10,
+    'sys_munmap':           11,
+    'sys_brk':              12,
+    'sys_fcntl':            72,  # Needed for Java and PHP to run
+    'sys_clone':            56,
+    'sys_fork':             57,
+    'sys_access':           21,
+    'sys_arch_prctl':       158,
+    'sys_set_tid_address':  218,
+    'sys_set_robust_list':  273,
+    'sys_futex':            202,
+    'sys_rt_sigaction':     13,
+    'sys_rt_sigprocmask':   14,
+    'sys_getrlimit':        97,
+    'sys_ioctl':            16,
+    'sys_readlink':         89,
+    'sys_getcwd':           79,
+    'sys_geteuid':          107,
+    'sys_getuid':           102,
+    'sys_getegid':          108,
+    'sys_getgid':           104,
+    'sys_lstat':            6,
+    'sys_openat':           257,
+    'sys_getdents':         78,
+    'sys_lseek':            8,
 
-sys_execve = 59
+    'sys_execve':           59
+}
+# @formatter:on
 
-allowed_syscalls = [sys_read,
-                    sys_write,
-                    sys_open,
-                    sys_access,
-                    sys_close,
-                    sys_stat,
-                    sys_fstat,
-                    sys_mmap,
-                    sys_mprotect,
-                    sys_munmap,
-                    sys_brk,
-                    sys_fcntl,
-                    sys_arch_prctl,  # TODO: is this safe?
-                    sys_set_tid_address,
-                    sys_set_robust_list,
-                    sys_futex,
-                    sys_rt_sigaction,
-                    sys_rt_sigprocmask,
-                    sys_getrlimit,
-                    sys_ioctl,
-                    sys_readlink,
-                    sys_getcwd,
-                    sys_geteuid,
-                    sys_getuid,
-                    sys_getegid,
-                    sys_getgid,
-                    sys_lstat,
-                    sys_openat,
-                    sys_getdents,
+# Define all syscalls as variables
+for call, id in calls.iteritems():
+    vars()[call] = id
 
-                    sys_clone,
-                    sys_fork]
 
-counts = {}
+def call_to_string(call):
+    for v, val in calls.iteritems():
+        if val == call:
+            return v
+    return "unknown"
+
+
+allow = lambda: True
+
+
+def do_write():
+    fd = ptrace(PTRACE_PEEKUSR, pid, 8 * RDI, None)
+    print fd
+    # Only allow writing to stdout & stderr
+    return fd == 1 or fd == 2
+
+__execve_count = 0
+def do_execve():
+    global __execve_count
+    __execve_count += 1
+    if __execve_count > 2:
+        return False
+    return True
+
+# @formatter:off
+proxied_syscalls = {
+    sys_execve:             do_execve,
+    sys_read:               allow,
+    sys_write:              do_write,
+    sys_open:               allow,
+    sys_access:             allow,
+    sys_close:              allow,
+    sys_stat:               allow,
+    sys_fstat:              allow,
+    sys_mmap:               allow,
+    sys_mprotect:           allow,
+    sys_munmap:             allow,
+    sys_brk:                allow,
+    sys_fcntl:              allow,
+    sys_arch_prctl:         allow,  # TODO: is this safe?
+    sys_set_tid_address:    allow,
+    sys_set_robust_list:    allow,
+    sys_futex:              allow,
+    sys_rt_sigaction:       allow,
+    sys_rt_sigprocmask:     allow,
+    sys_getrlimit:          allow,
+    sys_ioctl:              allow,
+    sys_readlink:           allow,
+    sys_getcwd:             allow,
+    sys_geteuid:            allow,
+    sys_getuid:             allow,
+    sys_getegid:            allow,
+    sys_getgid:             allow,
+    sys_lstat:              allow,
+    sys_openat:             allow,
+    sys_getdents:           allow,
+    sys_lseek:              allow,
+
+    sys_clone:              allow,
+    sys_fork:               allow
+}
+# @formatter:on
 
 child = sys.argv[1]
 args = sys.argv[1:]
@@ -130,8 +165,6 @@ status = None
 pid = os.fork()
 if not pid:
     resource.setrlimit(resource.RLIMIT_AS, (32 * 1024 * 1024,) * 2)
-    #resource.setrlimit(resource.RLIMIT_NOFILE, (4, 4))
-    resource.setrlimit(resource.RLIMIT_NPROC, (0, 0))
     ptrace(PTRACE_TRACEME, 0, None, None)
     os.kill(os.getpid(), SIGSTOP)
     os.execvp(child, args)
@@ -150,21 +183,18 @@ else:
             break
 
         if WSTOPSIG(status) == SIGTRAP:
-            in_syscall = not in_syscall
             if in_syscall:
                 call = ptrace(PTRACE_PEEKUSR, pid, 8 * ORIG_RAX, None)
 
-                counts[call] = counts.get(call, 0) + 1
-                print "%d: %s" % (call, counts)
+                print call_to_string(call)
 
-                if call == sys_execve:
-                    # execve is called twice: once when we os.execvp(child, args),
-                    # and once when the child actually spawns
-                    # anything after these two calls is likely malicious
-                    if 0 and counts[sys_execve] > 2:
-                        raise Exception("YOU MOTHERFORKER")
-                elif call not in allowed_syscalls:
+                if call in proxied_syscalls:
+                    if not proxied_syscalls[call]():
+                        os.kill(pid, SIGKILL)
+                        print "You're doing Something Bad"
+                else:
                     raise Exception(call)
+            in_syscall = not in_syscall
 
         ptrace(PTRACE_SYSCALL, pid, None, None)
 

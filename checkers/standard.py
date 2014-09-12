97e1e702512ba40038ed04805628b832fac7c1d5
Tudor <tbrindus@gmail.com>
2014-09-11 22:30:21 -0400

    More kwarg improvements

--- checkers/standard.py
+++ checkers/standard.py
@@ -1,4 +1,4 @@
-def check(process_output, judge_output, data):
+def check(process_output, judge_output, data, **kwargs):
     for process_line, judge_line in zip(process_output.split('\n'), judge_output.split('\n')):
         process_line = process_line.rstrip()
         judge_line = judge_line.rstrip()

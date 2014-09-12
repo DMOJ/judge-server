49c7adf5a065cfcb799378c314b45dd48d6b4221
Tudor <tbrindus@gmail.com>
2014-09-11 22:17:28 -0400

    Fix minor naming error

--- checkers/standard.py
+++ checkers/standard.py
@@ -1,4 +1,4 @@
-def checker(process_output, judge_output, data):
+def check(process_output, judge_output, data):
     for process_line, judge_line in zip(process_output.split('\n'), judge_output.split('\n')):
         process_line = process_line.rstrip()
         judge_line = judge_line.rstrip()

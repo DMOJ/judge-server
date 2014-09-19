016fc089c6a72bdf93bbc489ca6e1185c013adbc
FatalEagle <li.yikuan@gmail.com>
2014-09-19 19:33:03 -0400

    checkers

--- checkers/sorted.py
+++ checkers/sorted.py
@@ -1,6 +1,5 @@
-import string
-
 def check(process_output, judge_output, data):
+    import string
     process_lines = filter(None, process_output.split('\n'))
     judge_lines = filter(None, judge_output.split('\n'))
     if len(process_lines) != len(judge_lines):

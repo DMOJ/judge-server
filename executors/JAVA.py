893533e5c4c590f3e6426e757aaddc1c044e8357
Xiaomao Chen <xiaomao5@live.com>
2014-09-26 19:12:18 -0400

    Java executor that might not work

--- executors/JAVA.py
+++ executors/JAVA.py
@@ -2,11 +2,31 @@ import os
 import subprocess
 from cptbox import SecurePopen, NullSecurity
 from error import CompileError
+from executors.utils import test_executor
 
 from .resource_proxy import ResourceProxy
 from judgeenv import env
 
-JAVA_FS = ['/usr/bin/java', '.*\.[so|jar]']
+
+class JavaPopen(object):
+    def __init__(self, args, executable):
+        self.process = subprocess.Popen(args, executable=executable,
+                                        stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.execution_time, self.tle = None, None
+        self.max_memory, self.mle = None, None
+        self.stderr = None
+
+    def communicate(self, stdin):
+        stdout, stderr = self.process.communicate()
+        self.execution_time, self.tle, self.max_memory, self.mle = map(int, stderr.split())
+        return stdout, None
+
+    @property
+    def r_execution_time(self):
+        return self.execution_time
+
+    def __getattr__(self, attr):
+        return getattr(self.process, attr)
 
 
 class Executor(ResourceProxy):
@@ -20,21 +40,27 @@ class Executor(ResourceProxy):
         javac_process = subprocess.Popen(javac_args, stderr=subprocess.PIPE)
         _, compile_error = javac_process.communicate()
         self._files = [source_code_file, output_file]
+        self._class_name = problem_id
         if javac_process.returncode != 0:
             os.unlink(source_code_file)
             raise CompileError(compile_error)
 
     def launch(self, *args, **kwargs):
-        return SecurePopen(['java', '-Djava.security.manager', '-Djava.security.policy=java_executor.policy', '-client',
-                            '-Xmx%sK' % kwargs.get('memory'), '-cp', '.',
-                            self._files[1]] + list(args),
-                           executable=env['runtime']['java'],
-                           security=NullSecurity(),
-                           time=kwargs.get('time'),
-                           memory=kwargs.get('memory'))
+        return JavaPopen(['java', '-Djava.security.manager', '-Djava.security.policy=java_executor.policy', '-client',
+                            '-Xmx%sK' % kwargs.get('memory'), '-cp', 'java_executor', 'JavaSafeExecutor', os.getcwd(),
+                            self._class_name, kwargs.get('time') * 1000] + list(args),
+                         executable=env['runtime']['java'])
 
 
 def initialize():
     if 'java' not in env['runtime']:
         return False
-    return os.path.isfile(env['runtime']['java'])
+    if not os.path.isfile(env['runtime']['java']):
+        return False
+    return test_executor('JAVA', Executor, r'''\
+public class self_test {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+''', problem='self_test')

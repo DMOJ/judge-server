16014fe39c60d5e33bf068ded5cf03aef41cb566
Xiaomao Chen <xiaomao5@live.com>
2014-09-29 18:45:31 -0400

    I guess python is not happy when cpython is happy

--- cptbox/sandbox.py
+++ cptbox/sandbox.py
@@ -43,7 +43,7 @@ def _eintr_retry_call(func, *args):
 
 class _SecurePopen(Process):
     def __init__(self, bitness, args, executable=None, security=None, time=0, memory=0, stdin=PIPE, stdout=PIPE,
-                 stderr=None, env=None, nproc=0, address_grace=4096, chdir=None):
+                 stderr=None, env=None, nproc=0, address_grace=4096, chdir=''):
         self._bitness = bitness
         self._executable = executable or _find_exe(args[0])
         self._args = args

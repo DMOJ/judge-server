b15e8d6beae1fcabfacb9816e3f42e3dc7b83d39
Xiaomao Chen <xiaomao5@live.com>
2014-09-29 20:38:06 -0400

    @Xyene wants -1 to be IR

--- executors/JAVA.py
+++ executors/JAVA.py
@@ -39,6 +39,8 @@ class JavaPopen(object):
         self.error_info = '\n'.join(stderr[:-1])
         self.execution_time, self.tle, self.max_memory, self.mle, self.returncode = map(int, stderr[-1].split())
         self.execution_time /= 1000.0
+        if self.returncode == -1:
+            self.returncode = 1
         return stdout, None
 
     @property

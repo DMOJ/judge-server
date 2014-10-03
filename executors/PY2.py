123ab5711d708bcaddbc475a72bf9feab669526c
Quantum <quantum2048@gmail.com>
2014-10-03 17:05:39 -0400

    Allowed /dev/urandom access.
    Made regexen more correct.
    
    @Xyene [so|py] is a character class containing s, o, |, p, and y.

--- executors/PY2.py
+++ executors/PY2.py
@@ -3,7 +3,8 @@ from .utils import test_executor
 from cptbox import SecurePopen, CHROOTSecurity
 from judgeenv import env
 
-PYTHON_FS = ['.*\.[so|py]', '.*/lib(?:32|64)?/python[\d.]+/.*', '.*/lib/locale/.*', '/proc/meminfo', '/etc/localtime']
+PYTHON_FS = ['.*\.(?:so|py[co]?)$', '.*/lib(?:32|64)?/python[\d.]+/.*', '.*/lib/locale/', '/proc/meminfo$', '/etc/localtime$',
+             '/dev/urandom$']
 
 
 class Executor(ResourceProxy):

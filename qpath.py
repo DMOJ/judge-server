5ec0cb9cc1240bcfa7baaf7b19fa9cb689bee993
Quantum <quantum2048@gmail.com>
2014-10-05 14:29:06 -0400

    Quantum's path class!

---
+++ qpath.py
@@ -0,0 +1,14 @@
+__author__ = 'Quantum'
+
+import os
+
+
+class path(str):
+    def __new__(cls, *args):
+        return str.__new__(os.path.join(*args))
+
+    def __div__(self, other):
+        return path(self, other)
+
+    __truediv__ = __div__
+    __floordiv__ = __div__

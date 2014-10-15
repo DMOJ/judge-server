11487f5d8f9d707c13a3f23e75d1fbb3c67deafc
FatalEagle <li.yikuan@gmail.com>
2014-10-14 20:52:12 -0400

    Allow /etc/localtime$

--- executors/PY3.py
+++ executors/PY3.py
@@ -5,7 +5,7 @@ from executors.utils import test_executor
 from .resource_proxy import ResourceProxy
 from judgeenv import env
 
-PYTHON_FS = ['/dev/urandom$', '.*\.(?:so|py[co]?$)', '.*/lib(?:32|64)?/python[\d.]+/.*',
+PYTHON_FS = ['/etc/localtime$', '/dev/urandom$', '.*\.(?:so|py[co]?$)', '.*/lib(?:32|64)?/python[\d.]+/.*',
              '.*/lib/locale/', '/usr/lib64', '.*/?pyvenv.cfg$', '/proc/meminfo$']
 if 'python3dir' in env:
     PYTHON_FS += [str(env['python3dir']) + '.*']

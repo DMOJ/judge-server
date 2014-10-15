2e9676bc553dfac0302e30da0f5412cd5a45ea7c
Quantum <quantum2048@gmail.com>
2014-10-15 16:24:05 -0400

    Avoids sys.modules mess by just making modules ourselves. No spam .py[co] creation.
    @Xyene, @FatalEagle, notify me if modules fail to load due to this change.

--- modload.py
+++ modload.py
@@ -1,23 +1,12 @@
 import os
 import imp
-import sys
 
 
-def load_module_from_file(filename, namespace):
+def load_module_from_file(filename):
     path, name = os.path.split(filename)
     name, ext = os.path.splitext(name)
 
-    modname = '%s_%s' % (namespace, name)
-
-    imp.acquire_lock()
-    try:
-        file, filename, data = imp.find_module(name, [path])
-        old_mod = sys.modules.get(modname, None)
-        mod = imp.load_module(modname, file, filename, data)
-        if old_mod is None:
-            del sys.modules[modname]
-        else:
-            sys.modules[modname] = old_mod
-        return mod
-    finally:
-        imp.release_lock()
+    mod = imp.new_module(name)
+    mod.__file__ = os.path.abspath(filename)
+    exec compile(open(mod.__file__).read(), mod.__file__, 'exec') in mod.__dict__
+    return mod

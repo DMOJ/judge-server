cf651e4ffe12300c9f481732301a9210507d30fb
Quantum <quantum2048@gmail.com>
2014-10-18 22:16:43 -0400

    Windows and the case issue, partly to blame PyCharm.

--- executors/CPP0X.py
+++ executors/CPP0X.py
@@ -1,7 +1,7 @@
 from judgeenv import env
 from .GCCExecutor import make_executor
 
-Executor, initialize = make_executor('CPP11', 'g++11', ['-std=' + env['runtime'].get('g++11std', 'c++0x')], '.cpp', r'''
+Executor, initialize = make_executor('CPP0X', 'g++11', ['-std=c++0x'], '.cpp', r'''
 #include <iostream>
 
 int main() {

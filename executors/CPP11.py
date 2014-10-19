53cdb243f660e0f2efda146efdfa104b1d7ea66c
Quantum <quantum2048@gmail.com>
2014-10-18 22:12:27 -0400

    Differentiated C++0x and C++11

--- executors/CPP11.py
+++ executors/CPP11.py
@@ -1,7 +1,7 @@
 from judgeenv import env
 from .GCCExecutor import make_executor
 
-Executor, initialize = make_executor('CPP11', 'g++11', ['-std=' + env['runtime'].get('g++11std', 'c++0x')], '.cpp', r'''
+Executor, initialize = make_executor('CPP11', 'g++11', ['-std=c++11'], '.cpp', r'''
 #include <iostream>
 
 int main() {

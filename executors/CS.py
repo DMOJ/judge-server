5bcb737b8bd36a1e577471e481c9e18b821183f7
Quantum <quantum2048@gmail.com>
2014-10-19 14:28:53 -0400

    Working C#. Incrediblely sketchy when nproc needs to be 34.

--- executors/CS.py
+++ executors/CS.py
@@ -1,4 +1,5 @@
 import os
+import re
 import subprocess
 
 from cptbox import CHROOTSecurity, SecurePopen, PIPE, ALLOW
@@ -8,14 +9,16 @@ from .utils import test_executor
 from .resource_proxy import ResourceProxy
 from judgeenv import env
 
-CS_FS = ['.*\.so', '/proc/(?:self/|xen)']
+CS_FS = ['.*\.so', '/proc/(?:self/|xen)', '/dev/shm/', '/proc/stat', '/usr/lib/mono/',
+         '/etc/nsswitch.conf$', '/etc/passwd$', '/etc/mono/', '/dev/null$', '.*/.mono/']
+UNLINK_FS = re.compile('/dev/shm/mono.\d+$')
 
 
 class Executor(ResourceProxy):
     def __init__(self, problem_id, source_code):
         super(Executor, self).__init__()
         source_code_file = self._file('%s.cs' % problem_id)
-        self.name = '%s.exe' % problem_id
+        self.name = self._file('%s.exe' % problem_id)
         with open(source_code_file, 'wb') as fo:
             fo.write(source_code)
         csc_args = [env['runtime']['csc'], source_code_file]
@@ -25,8 +28,25 @@ class Executor(ResourceProxy):
             raise CompileError(compile_error)
 
     def _get_security(self):
-        sec = CHROOTSecurity(CS_FS)
+        sec = CHROOTSecurity(CS_FS + [self._dir])
         sec[sys_sched_getaffinity] = ALLOW
+        sec[sys_statfs] = ALLOW
+        sec[sys_ftruncate64] = ALLOW
+        sec[sys_clock_getres] = ALLOW
+        sec[sys_socketcall] = ALLOW
+        sec[sys_sched_yield] = ALLOW
+
+        def tgkill(debugger):
+            return debugger.arg0() == debugger.getpid()
+        sec[sys_tgkill] = tgkill
+
+        def unlink(debugger):
+            path = debugger.readstr(debugger.uarg0())
+            if UNLINK_FS.match(path) is None:
+                print 'Not allowed to unlink:', UNLINK_FS
+                return False
+            return True
+        sec[sys_unlink] = unlink
         return sec
 
     def launch(self, *args, **kwargs):
@@ -36,7 +56,7 @@ class Executor(ResourceProxy):
                            time=kwargs.get('time'),
                            memory=kwargs.get('memory'),
                            stderr=(PIPE if kwargs.get('pipe_stderr', False) else None),
-                           env={}, cwd=self._dir)
+                           env={}, cwd=self._dir, nproc=34)
 
     def launch_unsafe(self, *args, **kwargs):
         return subprocess.Popen(['mono', self.name] + list(args),

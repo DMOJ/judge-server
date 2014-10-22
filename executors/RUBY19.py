8833016f1912cb43b8380d6dcda367a9895c647c
Quantum <quantum2048@gmail.com>
2014-10-22 19:00:56 -0400

    Ruby 1.9 likes to write to fd 4, apparently a pipe it creates.
    TODO: parse to verify fd 4 is indeed the same pipe as fd 3.

--- executors/RUBY19.py
+++ executors/RUBY19.py
@@ -1,8 +1,14 @@
+from cptbox.syscalls import *
 from .ruby import make_executor, make_initialize
 
 
 class Executor(make_executor('ruby19')):
     def _nproc(self):
         return -1
+
+    def _security(self):
+        sec = super(Executor, self)._security()
+        sec[sys_write] = lambda debugger: debugger.arg0() in (1, 2, 4)
+        return sec
 
 initialize = make_initialize('RUBY19', 'ruby19', Executor)

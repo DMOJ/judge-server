b01fe78a7da5ac6c6fe44f298d8960f870b4874e
Tudor <tbrindus@gmail.com>
2014-10-22 20:14:56 -0400

    Make that issue manifest in a less cryptic manner

--- judge.py
+++ judge.py
@@ -198,27 +198,31 @@ class Judge(object):
                 try:
                     # Launch an executor for the given language
                     # The executor is responsible for writing source files and compiling (if applicable)
-                    if 'handler' in init_data and language in ('C', 'CPP', 'CPP0X', 'CPP11'):
-                        aux_sources = {}
-                        handler_data = init_data['handler']
-                        entry_path = os.path.join(get_problem_root(problem_id), handler_data['entry'])
-                        header_path = os.path.join(get_problem_root(problem_id), handler_data['header'])
+                    if 'handler' in init_data:
+                        if language in ('C', 'CPP', 'CPP0X', 'CPP11'):
+                            aux_sources = {}
+                            handler_data = init_data['handler']
+                            entry_path = os.path.join(get_problem_root(problem_id), handler_data['entry'])
+                            header_path = os.path.join(get_problem_root(problem_id), handler_data['header'])
 
-                        if not os.path.exists(entry_path):
-                            raise IOError('entry path "%s" does not exist' % entry_path)
-                        if not os.path.exists(header_path):
-                            raise IOError('header path "%s" does not exist' % header_path)
+                            if not os.path.exists(entry_path):
+                                raise IOError('entry path "%s" does not exist' % entry_path)
+                            if not os.path.exists(header_path):
+                                raise IOError('header path "%s" does not exist' % header_path)
 
-                        with open(entry_path, 'r') as entry_point:
-                            with open(header_path, 'r') as header:
-                                aux_sources[problem_id + "_submission"] = ('#include "%s"\n#define main user_main\n' %
-                                                                           handler_data['header']) + source_code
-                                aux_sources[handler_data['header']] = header.read()
-                                source_code = entry_point.read()
-                        # Compile as CPP11 regardless of what the submission language is
-                        executor = executors['CPP11'].Executor(problem_id, source_code, aux_sources=aux_sources,
-                                                               writable=handler_data.get('writable', (1, 2)),
-                                                               fds=handler_data.get('fds', None))
+                            with open(entry_path, 'r') as entry_point:
+                                with open(header_path, 'r') as header:
+                                    aux_sources[problem_id + "_submission"] = (
+                                                                              '#include "%s"\n#define main user_main\n' %
+                                                                              handler_data['header']) + source_code
+                                    aux_sources[handler_data['header']] = header.read()
+                                    source_code = entry_point.read()
+                            # Compile as CPP11 regardless of what the submission language is
+                            executor = executors['CPP11'].Executor(problem_id, source_code, aux_sources=aux_sources,
+                                                                   writable=handler_data.get('writable', (1, 2)),
+                                                                   fds=handler_data.get('fds', None))
+                        else:
+                            raise CompileError('no valid handler compiler exists')
                     else:
                         executor = executors[language].Executor(problem_id, source_code)
                 except KeyError:

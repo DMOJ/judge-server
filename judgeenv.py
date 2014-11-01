8d7a5c2378dd66c53d98a0da16bbd1c09f30adc4
Quantum <quantum2048@gmail.com>
2014-11-01 19:53:38 -0400

    Must pass unicode paths to get unicode results back from os.listdir.

--- judgeenv.py
+++ judgeenv.py
@@ -1,18 +1,26 @@
 import json
 import os
+import sys
 
 __all__ = ['env', 'get_problem_root', 'get_problem_roots']
 
 _judge_dirs = ()
 _root = os.path.dirname(__file__)
 
+
+def unicodify(string):
+    if isinstance(string, str):
+        return string.decode(sys.getfilesystemencoding())
+    return string
+
+
 with open(os.path.join(_root, 'data', 'judge', 'judge.json')) as init_file:
     env = json.load(init_file)
     dirs = env.get('problem_storage_root', os.path.join('data', 'problems'))
     if isinstance(dirs, list):
-        _judge_dirs = tuple(os.path.normpath(os.path.join(_root, dir)) for dir in dirs)
+        _judge_dirs = tuple(unicodify(os.path.normpath(os.path.join(_root, dir))) for dir in dirs)
     else:
-        _judge_dirs = os.path.normpath(os.path.join(_root, dirs)),
+        _judge_dirs = unicodify(os.path.normpath(os.path.join(_root, dirs))),
 
 
 def get_problem_root(pid):

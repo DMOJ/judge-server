cbc017e08019a17ed67b8ab4dc77d04adeeffea4
Quantum <quantum2048@gmail.com>
2014-11-10 18:01:53 -0500

    Much much fix error

--- executors/python.py
+++ executors/python.py
@@ -24,8 +24,8 @@ __import__('sys').stdin = __import__('os').fdopen(0, 'r', 65536)
 
     def launch(self, *args, **kwargs):
         return SecurePopen(['python', '-BS', self._script] + list(args),
-                           executable=self.get_security(),
-                           security=self.get_executable(),
+                           executable=self.get_executable(),
+                           security=self.get_security(),
                            time=kwargs.get('time'),
                            memory=kwargs.get('memory'),
                            address_grace=131072,

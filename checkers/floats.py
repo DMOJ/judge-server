0713f20629ffe6befacadbe15b18ce19bce0aeb5
FatalEagle <li.yikuan@gmail.com>
2014-11-20 23:14:15 -0500

    Update checkers

--- checkers/floats.py
+++ checkers/floats.py
@@ -1,19 +1,18 @@
 def check(process_output, judge_output, precision, **kwargs):
-    from judge import TerminateGrading
-    epsilon = 10 ** -int(precision)
+    from itertools import izip
     process_lines = filter(None, process_output.split('\n'))
     judge_lines = filter(None, judge_output.split('\n'))
     if len(process_lines) != len(judge_lines):
         return False
-    for process_line, judge_line in zip(process_lines, judge_lines):
-        try:
+    epsilon = 10 ** -int(precision)
+    try:
+        for process_line, judge_line in izip(process_lines, judge_lines):
             process_floats = map(float, process_line.split())
             judge_floats = map(float, judge_line.split())
-        except TerminateGrading:
-            raise
-        except:
-            return False
-        for process_float, judge_float in zip(process_floats, judge_floats):
-            if abs(process_float - judge_float) > epsilon and (abs(judge_float) < epsilon or abs(1.0 - process_float / judge_float) > epsilon):
+            if any(abs(process_float - judge_float) > epsilon and 
+                   (abs(judge_float) < epsilon or abs(1.0 - process_float / judge_float) > epsilon)
+                     for process_float, judge_float in izip(process_floats, judge_floats)):
                 return False
+    except:
+        return False
     return True
--- checkers/standard.py
+++ checkers/standard.py
@@ -1,11 +1,10 @@
 def check(process_output, judge_output, **kwargs):
+    from itertools import izip
     process_lines = filter(None, process_output.split('\n'))
     judge_lines = filter(None, judge_output.split('\n'))
     if len(process_lines) != len(judge_lines):
         return False
-    for process_line, judge_line in zip(process_lines, judge_lines):
-        process_line = process_line.split()
-        judge_line = judge_line.split()
-        if process_line != judge_line:
+    for process_line, judge_line in izip(process_lines, judge_lines):
+        if process_line.split() != judge_line.split():
             return False
     return True

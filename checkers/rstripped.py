f2693a72f99a2d414b7ee76d0f542535aa0d9ec0
FatalEagle <li.yikuan@gmail.com>
2014-11-29 22:57:24 -0500

    Remove debug prints

--- checkers/rstripped.py
+++ checkers/rstripped.py
@@ -5,13 +5,9 @@ def check(process_output, judge_output, **kwargs):
     if 'filter_new_line' in kwargs:
         process_lines = filter(None, process_lines)
         judge_lines = filter(None, judge_lines)
-    print process_lines
-    print judge_lines
     if len(process_lines) != len(judge_lines):
-        print "fail 1"
         return False
     for process_line, judge_line in izip(process_lines, judge_lines):
         if process_line.rstrip() != judge_line.rstrip():
-            print "fail 2"
             return False
     return True

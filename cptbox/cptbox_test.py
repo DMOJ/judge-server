b489927a26f13edc5845265c924e2eb4f511b97e
Quantum <quantum2048@gmail.com>
2014-12-04 21:54:40 -0500

    Debugging code for socket

--- cptbox/cptbox_test.py
+++ cptbox/cptbox_test.py
@@ -1,24 +1,24 @@
 from _cptbox import Process
-
-allowed = [
-    0, 1, 3, 5, 9, 10, 11, 12, 13, 14, 16, 21, 59, 72,
-    78, 97, 137, 158, 202, 218, 231, 273,
-]
+import sys
+import os
 
 
 class MyProcess(Process):
     def _callback(self, syscall):
-        if syscall == 2:
+        if syscall == 5:
             print 'Access:', self.debugger.readstr(self.debugger.uarg0)
+        elif syscall == 102:
+            print 'Socket call'
         return True
 
 
 def main():
-    proc = MyProcess(64)
-    for call in allowed:
+    proc = MyProcess(32)
+    for call in xrange(341):
         proc._handler(call, 1)
-    proc._handler(2, 2)
-    proc._spawn('/bin/ls', ['ls'])
+    proc._handler(5, 2)
+    proc._handler(102, 2)
+    proc._spawn(sys.executable, ['python', os.path.join(os.path.dirname(__file__), 'cptbox_test_slave.py')])
     print 'Return: %d' % proc._monitor()
     print 'Memory usage: %d KB' % proc.max_memory
 

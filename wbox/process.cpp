05b5600024dc7e44a719417e55295ff0a9fe82eb
Quantum <quantum2048@gmail.com>
2014-12-06 00:53:35 -0500

    Added limit setting.

--- wbox/process.cpp
+++ wbox/process.cpp
@@ -2,8 +2,10 @@
 
 
 JobbedProcessManager::JobbedProcessManager() {
+	ZeroMemory(&extLimits, sizeof extLimits);
+	extLimits.BasicLimitInformation.ActiveProcessLimit = 1;
+	extLimits.BasicLimitInformation.LimitFlags = JOB_OBJECT_LIMIT_ACTIVE_PROCESS;
 }
-
 
 bool JobbedProcessManager::spawn() {
 	
@@ -13,6 +15,30 @@ bool JobbedProcessManager::terminate(unsigned code) {
 	if (hProcess)
 		return TerminateProcess(hProcess, code);
 	return false;
+}
+
+void JobbedProcessManager::time(double seconds) {
+	if (seconds) {
+		extLimits.BasicLimitInformation.PerJobUserTimeLimit.QuadPart = uint64_t(seconds * 1000 * 1000 * 10);
+		extLimits.BasicLimitInformation.LimitFlags |= JOB_OBJECT_LIMIT_JOB_TIME;
+	} else
+		extLimits.BasicLimitInformation.LimitFlags &= ~JOB_OBJECT_LIMIT_JOB_TIME;
+}
+
+void JobbedProcessManager::memory(size_t bytes) {
+	if (bytes) {
+		extLimits.JobMemoryLimit = bytes;
+		extLimits.BasicLimitInformation.LimitFlags |= JOB_OBJECT_LIMIT_JOB_MEMORY;
+	} else
+		extLimits.BasicLimitInformation.LimitFlags &= ~JOB_OBJECT_LIMIT_JOB_MEMORY;
+}
+
+void JobbedProcessManager::processes(int count) {
+	if (count) {
+		extLimits.BasicLimitInformation.ActiveProcessLimit = count;
+		extLimits.BasicLimitInformation.LimitFlags |= JOB_OBJECT_LIMIT_ACTIVE_PROCESS;
+	} else
+		extLimits.BasicLimitInformation.LimitFlags &= ~JOB_OBJECT_LIMIT_ACTIVE_PROCESS;
 }
 
 JobbedProcessManager::~JobbedProcessManager() {
--- wbox/process.h
+++ wbox/process.h
@@ -4,17 +4,23 @@
 #define id_83DB5799_5792_4F64_BC72_4086C9C0C735
 
 #include <windows.h>
+#include <cinttypes>
 #include "helpers.h"
 
 class JobbedProcessManager {
 	AutoHandle hProcess;
 	AutoHandle hJob;
+	JOBOBJECT_EXTENDED_LIMIT_INFORMATION extLimits;
 public:
 	JobbedProcessManager();
 	virtual ~JobbedProcessManager();
 
 	virtual bool spawn();
 	virtual bool terminate(unsigned code);
+
+	void time(double seconds);
+	void memory(size_t bytes);
+	void processes(int count);
 
 	HANDLE process() { return hProcess; }
 	HANDLE job() { return hJob; }

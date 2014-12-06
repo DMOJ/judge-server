c10f1029e3193fe75d6cdccaf6fa64676b1ff729
Quantum <quantum2048@gmail.com>
2014-12-05 23:48:16 -0500

    Helper to deal with HANDLEs.

---
+++ wbox/helpers.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <windows.h>
+
+#ifndef id_26FCBE24_1243_4829_89F4_B266B158236E
+#define id_26FCBE24_1243_4829_89F4_B266B158236E
+
+class AutoHandle {
+	HANDLE handle;
+public:
+	AutoHandle() : handle(nullptr) {}
+	explicit AutoHandle(HANDLE h) : handle(h) {}
+	~AutoHandle() { CloseHandle(handle); }
+	operator HANDLE() { return handle; }
+
+	AutoHandle &operator=(HANDLE h) {
+		if (handle)
+			CloseHandle(handle);
+		handle = h;
+		return *this;
+	}
+
+	HANDLE detach() {
+		HANDLE h = handle;
+		handle = nullptr;
+		return h;
+	}
+};
+
+#endif

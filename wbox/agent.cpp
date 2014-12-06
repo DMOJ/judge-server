2c8b46d8d55d3049e88fcd687d48eaffef22ee59
Quantum <quantum2048@gmail.com>
2014-12-06 12:14:38 -0500

    Probably working agent code.

--- wbox/agent.cpp
+++ wbox/agent.cpp
@@ -1,5 +1,7 @@
 #include <windows.h>
 #include <iostream>
+#include <strsafe.h>
+#include "helpers.h"
 #include "tools.h"
 
 
@@ -7,7 +9,34 @@ int wmain(int argc, wchar_t *argv[]) {
 	if (argc < 3)
 		return 2;
 
-	LPWSTR guid = argv[1];
+	HANDLE handle;
+	LPWSTR szGuid = argv[1];
+	WCHAR szName[MAX_PATH];
+
 	std::wstring cmdline = list2cmdline(argv + 2, argv + argc);
 	std::wcout << cmdline << '\n';
+
+	try {
+		StringCchCopy(szName, MAX_PATH, L"wbox_job_");
+		StringCchCat(szName, MAX_PATH, szGuid);
+		if (!(handle = OpenJobObject(JOB_OBJECT_ASSIGN_PROCESS, FALSE, szName)))
+			throw WindowsException("OpenJobObject");
+
+		AutoHandle hJob(handle);
+		STARTUPINFO si = { sizeof(STARTUPINFO), 0 };
+		PROCESS_INFORMATION pi;
+
+		if (!CreateProcess(nullptr, &cmdline.front(), nullptr, nullptr, FALSE,
+						   NORMAL_PRIORITY_CLASS | CREATE_SUSPENDED | CREATE_BREAKAWAY_FROM_JOB,
+						   nullptr, nullptr, &si, &pi))
+			throw WindowsException("CreateProcess");
+
+		AssignProcessToJobObject(hJob, pi.hProcess);
+		CloseHandle(pi.hProcess);
+		CloseHandle(pi.hThread);
+
+		return pi.dwProcessId;
+	} catch (WindowsException &e) {
+		std::cout << e.what() << '\n';
+	}
 }

273fde116a2f60145cc0660802ab043209975328
Quantum <quantum2048@gmail.com>
2014-12-06 12:31:08 -0500

    Get the executable directory.

--- wbox/main.cpp
+++ wbox/main.cpp
@@ -1,6 +1,13 @@
 #include "user.h"
 #include "process.h"
+#include <shlwapi.h>
 
 int wmain() {
-	
+	UserManager user_manager;
+	JobbedProcessManager process;
+	WCHAR szDirectory[MAX_PATH];
+
+	GetModuleFileName(nullptr, szDirectory, MAX_PATH);
+	*(PathFindFileName(szDirectory) - 1) = '\0';
+	process.time(2).memory(65536 * 1024).processes(1).command(L"hello.exe").directory(szDirectory);
 }

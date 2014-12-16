ac471f4ff30ab0ac60f4be2bf26d47376aff0ac1
Tudor Brinedus <tbrindus@gmail.com>
2014-12-16 16:56:10 -0500

    I didn't actually mean root!
    I mean, you better not run the judge as root.

--- java_executor/src/ca/dmoj/java/JavaSafeExecutor.java
+++ java_executor/src/ca/dmoj/java/JavaSafeExecutor.java
@@ -48,8 +48,8 @@ public class JavaSafeExecutor {
     /**
      * Flag to indicate that the security manager should be deactivated.
      * Should be set to false before running the user's submission, and false after it has finished running.
-     * If this flag somehow is set to false while a user's submission is running, the judging server is
-     * compromised: the submission has root access.
+     * If this flag somehow is set to false while a user's submission is running, the judging server may be
+     * compromised: the submission gains the same access level as the user the judge is running under.
      */
     static boolean _safeBlock = false;
     /**

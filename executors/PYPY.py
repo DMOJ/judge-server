6d7b837ed01b14f03e0ac079b5f48a0d8fb6f650
Quantum <quantum2048@gmail.com>
2014-12-18 19:37:36 -0500

    Got PYPY to run, however it refuses to actually work.

--- executors/PYPY.py
+++ executors/PYPY.py
@@ -1,14 +1,19 @@
 from executors.python import PythonExecutor
 from .utils import test_executor
-from cptbox import CHROOTSecurity
 from judgeenv import env
+
+try:
+    from cptbox import CHROOTSecurity
+except ImportError:
+    CHROOTSecurity = None
 
 PYTHON_FS = ['.*\.(?:so|py[co]?$)', '/proc/cpuinfo$', '/proc/meminfo$']
 
 
 class Executor(PythonExecutor):
-    def get_security(self):
-        return CHROOTSecurity(PYTHON_FS + ([env['runtime']['pypydir']] if 'pypydir' in env['runtime'] else []))
+    if CHROOTSecurity is not None:
+        def get_security(self):
+            return CHROOTSecurity(PYTHON_FS + ([env['runtime']['pypydir']] if 'pypydir' in env['runtime'] else []))
 
     def get_executable(self):
         return env['runtime']['pypy']
--- executors/PYPY3.py
+++ executors/PYPY3.py
@@ -4,21 +4,26 @@ from .PYPY import PYTHON_FS
 from executors.python import PythonExecutor
 from executors.utils import test_executor
 from judgeenv import env
-from cptbox import CHROOTSecurity
-from cptbox.syscalls import *
+
+try:
+    from cptbox import CHROOTSecurity
+    from cptbox.syscalls import *
+except ImportError:
+    CHROOTSecurity = None
 
 
 class Executor(PythonExecutor):
-    def get_security(self):
-        sec = CHROOTSecurity(PYTHON_FS + ([env['runtime']['pypy3dir']] if 'pypy3dir' in env['runtime'] else []))
+    if CHROOTSecurity is not None:
+        def get_security(self):
+            sec = CHROOTSecurity(PYTHON_FS + ([env['runtime']['pypy3dir']] if 'pypy3dir' in env['runtime'] else []))
 
-        def unsafe_pypy3dir(debugger):
-            # Relies on the fact this user can't access here.
-            return debugger.readstr(debugger.uarg0).startswith(env['runtime']['pypy3dir'])
-        if not os.access(env['runtime']['pypy3dir'], os.W_OK):
-            sec[sys_mkdir] = unsafe_pypy3dir
-            sec[sys_unlink] = unsafe_pypy3dir
-        return sec
+            def unsafe_pypy3dir(debugger):
+                # Relies on the fact this user can't access here.
+                return debugger.readstr(debugger.uarg0).startswith(env['runtime']['pypy3dir'])
+            if not os.access(env['runtime']['pypy3dir'], os.W_OK):
+                sec[sys_mkdir] = unsafe_pypy3dir
+                sec[sys_unlink] = unsafe_pypy3dir
+            return sec
 
     def get_executable(self):
         return env['runtime']['pypy3']

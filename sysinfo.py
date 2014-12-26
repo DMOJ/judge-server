9b9866c09c75c97ce6e5e3f74e5396ddf31389b7
Quantum <quantum2048@gmail.com>
2014-12-26 12:55:14 -0500

    More accurate load on windows.
    Most importantly, doesn't require a child process.

--- sysinfo.py
+++ sysinfo.py
@@ -1,8 +1,5 @@
 import os
 from multiprocessing import cpu_count
-from threading import Thread
-from collections import deque
-import subprocess
 
 _cpu_count = cpu_count()
 
@@ -11,33 +8,34 @@ if hasattr(os, 'getloadavg'):
     def load_fair():
         return 'load', os.getloadavg()[0] / _cpu_count
 else:
+    from winperfmon import PerformanceCounter
+    from threading import Thread
+    from collections import deque
+    from time import sleep
+
     class SystemLoadThread(Thread):
         def __init__(self):
-            super(SystemLoadThread, self).__init__(None, None, None, (), None, None)
+            super(SystemLoadThread, self).__init__()
             self.daemon = True
-            self.process = None
-            self.terminate = False
-            self.samples = deque(maxlen=60)
+            self.samples = deque(maxlen=10)
             self.load = 0.5
+            self.counter = PerformanceCounter(r'\System\Processor Queue Length', r'\Processor(_Total)\% Processor Time')
 
         def run(self):
-            self.process = subprocess.Popen(['typeperf', 'System\Processor Queue Length'], stdout=subprocess.PIPE)
-            read = self.process.stdout
-            while not read.readline().strip():
-                pass
-
             while True:
-                line = read.readline()
-                if self.terminate or '","' not in line:
-                    break
-                self.samples.append(float(line.strip().split('","')[1][:-1]))
-                self.load = sum(self.samples) / len(self.samples)
+                pql, pt = self.counter.query()
+                self.samples.append(pql)
+                if pt >= 100:
+                    self.load = max(sum(self.samples) / len(self.samples) / _cpu_count, pt / 100.)
+                else:
+                    self.load = pt / 100.
+                sleep(1)
 
     load_thread = SystemLoadThread()
     load_thread.start()
 
     def load_fair():
-        return 'load', load_thread.load / _cpu_count
+        return 'load', load_thread.load
 
 
 def cpu_count():

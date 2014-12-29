6701c3b57e4fc303b7987fea6a9d047b5f714be2
FatalEagle <li.yikuan@gmail.com>
2014-12-28 20:52:33 -0500

    Easier easy

--- checkers/easy.py
+++ checkers/easy.py
@@ -3,4 +3,4 @@ def check(process_output, judge_output, **kwargs):
     from collections import Counter
     process_all = re.sub(r'\s+', '', process_output)
     judge_all = re.sub(r'\s+', '', judge_output)
-    return Counter(process_all) == Counter(judge_all)
+    return Counter(process_all.lower()) == Counter(judge_all.lower())

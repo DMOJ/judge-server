15350273981d694ca52328e20ac4a62f48511e82
Quantum <quantum2048@gmail.com>
2014-12-30 17:07:28 -0500

    Ruby 1.9 executor for Windows.
    Must say it's not safe for use on linux.

--- executors/RUBY19.py
+++ executors/RUBY19.py
@@ -1,14 +1,21 @@
-from cptbox.syscalls import *
 from .ruby import make_executor, make_initialize
+
+try:
+    from cptbox.syscalls import *
+except ImportError:
+    cptbox = False
+else:
+    cptbox = True
 
 
 class Executor(make_executor('ruby19')):
     def _nproc(self):
         return -1
 
-    def _security(self):
-        sec = super(Executor, self)._security()
-        sec[sys_write] = lambda debugger: debugger.arg0 in (1, 2, 4)
-        return sec
+    if cptbox:
+        def _security(self):
+            sec = super(Executor, self)._security()
+            sec[sys_write] = lambda debugger: debugger.arg0 in (1, 2, 4)
+            return sec
 
 initialize = make_initialize('RUBY19', 'ruby19', Executor)

6c7e6302e849576b57bcc27b21ee8295ee33dae1
Quantum <quantum2048@gmail.com>
2015-01-08 18:45:19 -0500

    Ruby 1.8 executor for Windows.

--- executors/RUBY18.py
+++ executors/RUBY18.py
@@ -1,4 +1,4 @@
 from .ruby import make_executor, make_initialize
 
-Executor = make_executor('ruby')
-initialize = make_initialize('RUBY18', 'ruby', Executor)
+Executor = make_executor('ruby18')
+initialize = make_initialize('RUBY18', 'ruby18', Executor)

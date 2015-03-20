47a7c8f4eae773f582795fdb36b8195526c828b1
Quantum <quantum2048@gmail.com>
2015-03-20 14:41:49 -0400

    Some insane flags to optimize. #25

--- checkers/build_checker.py
+++ checkers/build_checker.py
@@ -1,6 +1,7 @@
 from distutils.core import Extension
 from distutils.command.build_ext import build_ext
 from distutils.dist import Distribution
+from distutils.msvccompiler import MSVCCompiler
 from distutils import log
 
 import os
@@ -13,6 +14,19 @@ log.set_verbosity(3)
 distribution = Distribution({
     'ext_modules': [Extension('_checker', sources=['_checker.c'])]
 })
+
+
+class Command(build_ext):
+    def build_extensions(self):
+        if isinstance(self.compiler, MSVCCompiler):
+            self.compiler.initialize()
+            self.compiler.compile_options.remove('/W3')
+            self.compiler.compile_options.remove('/MD')
+            if '/GS-' in self.compiler.compile_options:
+                self.compiler.compile_options.remove('/GS-')
+            self.compiler.compile_options += ['/Ox', '/W4', '/EHsc', '/GL', '/MT']
+            self.compiler.ldflags_shared += ['/OPT:REF,ICF', '/LTCG']
+        build_ext.build_extensions(self)
 
 command = build_ext(distribution)
 command.finalize_options()

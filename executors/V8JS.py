ae93732f902a7efb49f5ce6cc02410de13a6d610
Quantum <quantum2048@gmail.com>
2015-05-08 22:05:11 -0400

    V8 on Windows.

--- executors/V8JS.py
+++ executors/V8JS.py
@@ -1,7 +1,10 @@
 import os
 import subprocess
-from cptbox import SecurePopen, CHROOTSecurity, ALLOW
-from cptbox.syscalls import *
+try:
+    from cptbox import SecurePopen, CHROOTSecurity
+except ImportError:
+    from wbox import WBoxPopen
+    SecurePopen = None
 
 from .utils import test_executor
 from .resource_proxy import ResourceProxy
@@ -15,18 +18,20 @@ class Executor(ResourceProxy):
         with open(source_code_file, 'wb') as fo:
             fo.write(source_code)
 
-    #def _get_security(self):
-    #    security = CHROOTSecurity(NODE_FS)
-    #    security[sys_pipe2] = ALLOW
-    #    return security
-
-    def launch(self, *args, **kwargs):
-        return SecurePopen([env['runtime']['v8dmoj'], self._script] + list(args),
-                           security=CHROOTSecurity([]),#self._get_security(),
-                           time=kwargs.get('time'),
-                           memory=kwargs.get('memory'),
-                           address_grace=65536,
-                           env={}, cwd=self._dir)
+    if SecurePopen is None:
+        def launch(self, *args, **kwargs):
+            return WBoxPopen(['v8dmoj', self._script] + list(args),
+                             time=kwargs.get('time'), memory=kwargs.get('memory'),
+                             cwd=self._dir, executable=env['runtime']['v8dmoj'],
+                             network_block=True)
+    else:
+        def launch(self, *args, **kwargs):
+            return SecurePopen([env['runtime']['v8dmoj'], self._script] + list(args),
+                               security=CHROOTSecurity([]),
+                               time=kwargs.get('time'),
+                               memory=kwargs.get('memory'),
+                               address_grace=65536,
+                               env={}, cwd=self._dir)
 
     def launch_unsafe(self, *args, **kwargs):
         return subprocess.Popen([env['runtime']['v8dmoj'], self._script] + list(args),

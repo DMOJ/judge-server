c690ae73db4bf3a6b625a9819729802e247e5b76
Quantum <quantum2048@gmail.com>
2015-05-20 15:50:36 -0400

    Untested new class-based GCC executor.

--- executors/GCCExecutor.py
+++ executors/GCCExecutor.py
@@ -13,8 +13,6 @@ from .utils import test_executor
 from .resource_proxy import ResourceProxy
 from judgeenv import env
 
-C_FS = ['.*\.so', '/proc/meminfo', '/dev/null']
-
 GCC_ENV = env['runtime'].get('gcc_env', {})
 GCC_COMPILE = os.environ.copy()
 
@@ -24,6 +22,116 @@ if os.name == 'nt':
     GCC_ENV = dict((k.encode('mbcs'), v.encode('mbcs')) for k, v in GCC_ENV.iteritems())
 else:
     GCC_COMPILE.update(env['runtime'].get('gcc_compile', {}))
+
+
+class GCCExecutor(ResourceProxy):
+    ext = None
+    defines = []
+    command = None
+    flags = []
+    test_program = ''
+    name = 'GCC'
+
+    def __init__(self, problem_id, main_source, aux_sources=None, fds=None, writable=(1, 2)):
+        super(GCCExecutor, self).__init__()
+        if not aux_sources:
+            aux_sources = {}
+        aux_sources[problem_id + self.ext] = main_source
+        sources = []
+        for name, source in aux_sources.iteritems():
+            if '.' not in name:
+                name += self.ext
+            with open(self._file(name), 'wb') as fo:
+                fo.write(source)
+            sources.append(name)
+        defines = ['-DONLINE_JUDGE'] + self.defines
+
+        if os.name == 'nt':
+            defines.append('-DWIN32')
+
+        self.warning = None
+        self.name = problem_id
+        output_file = self.compile(defines, sources)
+        self._executable = output_file
+        self._fds = fds
+        self._writable = writable
+
+    def get_ldflags(self):
+        if os.name == 'nt':
+            return ['-Wl,--stack,67108864']
+        return []
+
+    def get_executable_ext(self):
+        return ['', '.exe'][os.name == 'nt']
+
+    def compile(self, defines, sources):
+        output_file = self._file('%s%s' % (self.name, self.get_executable_ext()))
+
+        gcc_args = ([self.command, '-Wall'] + sources + defines + ['-O2', '-lm', '-march=native']
+                    + self.flags + self.get_ldflags() + ['-s', '-o', output_file])
+        gcc_process = subprocess.Popen(gcc_args, stderr=subprocess.PIPE,
+                                       cwd=self._dir, env=GCC_COMPILE)
+
+        _, output = gcc_process.communicate()
+        if gcc_process.returncode != 0:
+            raise CompileError(output)
+        self.warning = output
+        return output_file
+
+    def get_fs(self):
+        return ['.*\.so', '/proc/meminfo', '/dev/null']
+
+    def get_security(self):
+        return CHROOTSecurity(self.get_fs(), writable=self._writable)
+
+    if SecurePopen is None:
+        def launch(self, *args, **kwargs):
+            return WBoxPopen([self.name] + list(args), executable=self._executable,
+                             time=kwargs.get('time'), memory=kwargs.get('memory'),
+                             cwd=self._dir, env=GCC_ENV, network_block=True)
+    else:
+        def launch(self, *args, **kwargs):
+            return SecurePopen([self.name] + list(args),
+                               executable=self._executable,
+                               security=self.get_security(),
+                               time=kwargs.get('time'),
+                               memory=kwargs.get('memory'),
+                               stderr=(PIPE if kwargs.get('pipe_stderr', False) else None),
+                               env=GCC_ENV,
+                               cwd=self._dir, fds=self._fds)
+
+    def launch_unsafe(self, *args, **kwargs):
+        return subprocess.Popen([self.name] + list(args),
+                                executable=self._executable,
+                                env=GCC_ENV,
+                                cwd=self._dir,
+                                **kwargs)
+
+    @classmethod
+    def initialize(cls):
+        if cls.command is None:
+            return False
+        if not os.path.isfile(cls.command):
+            return False
+        if not cls.test_program:
+            return True
+
+        print 'Self-testing: %s executor:' % cls.name,
+        try:
+            executor = cls('self-test', cls.test_program)
+            proc = executor.launch(time=1, memory=16384)
+            test_message = 'echo: Hello, World!'
+            stdout, stderr = proc.communicate(test_message)
+            res = stdout.strip() == test_message and not stderr
+            print ['Failed', 'Success'][res]
+            if stderr:
+                print>>sys.stderr, stderr
+            return res
+        except Exception:
+            print 'Failed'
+            import traceback
+            traceback.print_exc()
+            return False
 
 
 def make_executor(code, command, args, ext, test_code, _defines=None):

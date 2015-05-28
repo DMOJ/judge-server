3f5dde65e625b1fd0f93be486e629bfe5975c47c
Quantum <quantum2048@gmail.com>
2015-05-28 17:41:45 -0400

    Objective C executor. #40

---
+++ executors/OBJC.py
@@ -0,0 +1,41 @@
+from GCCExecutor import GCCExecutor
+from judgeenv import env
+from subprocess import check_output, CalledProcessError
+
+
+class Executor(GCCExecutor):
+    ext = '.m'
+    objc_flags = []
+    objc_ldflags = []
+    command = env['runtime'].get('gobjc')
+    name = 'OBJC'
+
+    test_program = r'''
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[]) {
+        NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+        NSString *hello = @"Hello, World!";
+        printf("%s\n", [hello cStringUsingEncoding:NSUTF8StringEncoding]);
+        [pool drain];
+        return 0;
+}
+'''
+
+    def get_flags(self):
+        return self.objc_flags + super(Executor, self).get_flags()
+
+    def get_ldflags(self):
+        return self.objc_ldflags + super(Executor, self).get_ldflags()
+
+    @classmethod
+    def initialize(cls):
+        if 'gnustep-config' not in env['runtime']:
+            return False
+        try:
+            cls.objc_flags = check_output([env['runtime']['gnustep-config'], '--objc-flags']).split()
+            cls.objc_ldflags = check_output([env['runtime']['gnustep-config'], '--base-libs']).split()
+        except CalledProcessError:
+            return False
+
+initialize = Executor.initialize

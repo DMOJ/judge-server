da49e6e3f20953c0d50cc7925c029043f1571fc3
Quantum <quantum2048@gmail.com>
2015-05-30 16:54:40 -0400

    Missing testing program

--- executors/D.py
+++ executors/D.py
@@ -8,6 +8,13 @@ class Executor(CompiledExecutor):
     address_grace = 32768
     fs = ['.*\.so', '/proc/self/maps$']
     command = env['runtime'].get('dmd')
+    test_program = '''\
+import std.stdio;
+
+void main() {
+    writeln(readln());
+}
+'''
 
     def get_compile_args(self):
         return [self.get_command(), '-O', '-inline', '-release', '-w', self._code, '-of%s' % self.problem]

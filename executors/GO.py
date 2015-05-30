b31a9b5ff977582b8bc9b1ac355e05c49d0c4de5
Quantum <quantum2048@gmail.com>
2015-05-30 17:06:23 -0400

    New Go executor #49

--- executors/GO.py
+++ executors/GO.py
@@ -1,73 +1,28 @@
-import os
-import re
-import subprocess
-
-from cptbox import CHROOTSecurity, SecurePopen, PIPE, ALLOW
-from cptbox.syscalls import *
-from error import CompileError
-from .utils import test_executor
-from .resource_proxy import ResourceProxy
+from .base_executor import CompiledExecutor
 from judgeenv import env
 
-GO_FS = ['.*\.so', '/proc/stat$']
 
-
-class Executor(ResourceProxy):
-    def __init__(self, problem_id, source_code):
-        super(Executor, self).__init__()
-        source_code_file = self._file('%s.go' % problem_id)
-        self.name = self._file(problem_id)
-        with open(source_code_file, 'wb') as fo:
-            fo.write(source_code)
-        ghc_args = [env['runtime']['go'], 'build', source_code_file]
-        go_process = subprocess.Popen(ghc_args, stderr=subprocess.PIPE, cwd=self._dir)
-        _, compile_error = go_process.communicate()
-        if go_process.returncode != 0:
-            raise CompileError(compile_error)
-        self.warning = compile_error
-
-    def _get_security(self):
-        sec = CHROOTSecurity(GO_FS)
-
-        sec[sys_getpid] = ALLOW
-        sec[sys_getppid] = ALLOW
-        sec[sys_clock_getres] = ALLOW
-        sec[sys_timer_create] = ALLOW
-        sec[sys_timer_settime] = ALLOW
-        sec[sys_timer_delete] = ALLOW
-        sec[sys_modify_ldt] = ALLOW
-
-        return sec
-
-    def launch(self, *args, **kwargs):
-        return SecurePopen([self.name] + list(args),
-                           executable=self.name,
-                           security=self._get_security(),
-                           address_grace=131072,
-                           time=kwargs.get('time'),
-                           memory=kwargs.get('memory'),
-                           stderr=(PIPE if kwargs.get('pipe_stderr', False) else None),
-                           env={}, cwd=self._dir, nproc=-1)
-
-    def launch_unsafe(self, *args, **kwargs):
-        return subprocess.Popen([self.name] + list(args),
-                                executable=self.name,
-                                env={},
-                                cwd=self._dir,
-                                **kwargs)
-
-
-def initialize():
-    if 'go' not in env['runtime']:
-        return False
-    if not os.path.isfile(env['runtime']['go']):
-        return False
-    return test_executor('GO', Executor, '''\
+class Executor(CompiledExecutor):
+    ext = '.go'
+    name = 'GO'
+    address_grace = 131072
+    fs = ['.*\.so', '/proc/stat$']
+    syscalls = ['getpid', 'getppid', 'clock_getres', 'timer_create', 'timer_settime',
+                'timer_delete', 'modify_ldt']
+    command = env['runtime'].get('go')
+    test_program = '''\
 package main
 
-import "fmt"
+import "fmt" "bufio"
 
 func main() {
-    fmt.Println("Hello, World!")
-}
-''')
+    bio := bufio.NewReader(os.Stdin)
+    text, _ := bio.ReadString(0)
+    fmt.Print(text)
+}'''
+
+    def get_compile_args(self):
+        return [self.get_command(), 'build', self._code]
+
+
+initialize = Executor.initialize

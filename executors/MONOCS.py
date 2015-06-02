8f86c231bc1f70c2e39eef9fa1b7feafa6ac1797
Quantum <quantum2048@gmail.com>
2015-06-02 18:29:40 -0400

    Allowed the latest version of Mono to work. (4.0)

--- executors/MONOCS.py
+++ executors/MONOCS.py
@@ -1,7 +1,9 @@
+import sys
 import os
 import re
 import errno
 import subprocess
+from collections import defaultdict
 
 from cptbox import CHROOTSecurity, SecurePopen, PIPE, ALLOW
 from cptbox.syscalls import *
@@ -10,8 +12,11 @@ from .utils import test_executor
 from .resource_proxy import ResourceProxy
 from judgeenv import env
 
-CS_FS = ['.*\.so', '/proc/(?:self/|xen)', '/dev/shm/', '/proc/stat', '/usr/lib/mono/',
-         '/etc/nsswitch.conf$', '/etc/passwd$', '/etc/mono/', '/dev/null$', '.*/.mono/']
+
+CS_FS = ['.*\.so', '/proc/(?:self/|xen)', '/dev/shm/', '/proc/stat', '/usr/lib/mono',
+         '/etc/nsswitch.conf$', '/etc/passwd$', '/etc/mono/', '/dev/null$', '.*/.mono/',
+         '/sys/']
+WRITE_FS = ['/proc/self/task/\d+/comm$']
 UNLINK_FS = re.compile('/dev/shm/mono.\d+$')
 
 
@@ -30,7 +35,8 @@ class Executor(ResourceProxy):
         self.warning = compile_error
 
     def _get_security(self):
-        sec = CHROOTSecurity(CS_FS + [self._dir])
+        fs = CS_FS + [self._dir]
+        sec = CHROOTSecurity(fs)
         sec[sys_sched_getaffinity] = ALLOW
         sec[sys_statfs] = ALLOW
         sec[sys_ftruncate64] = ALLOW
@@ -38,20 +44,42 @@ class Executor(ResourceProxy):
         sec[sys_socketcall] = ALLOW
         sec[sys_sched_yield] = ALLOW
 
-        def tgkill(debugger):
-            return debugger.arg0 == debugger.pid
-        sec[sys_tgkill] = tgkill
+        fs = sec.fs_jail
+        write_fs = re.compile('|'.join(WRITE_FS))
+        writable = defaultdict(bool)
+        writable[1] = writable[2] = True
+
+        def handle_open(debugger):
+            file = debugger.readstr(debugger.uarg0)
+            if fs.match(file) is None:
+                print>>sys.stderr, 'Not allowed to access:', file
+                return False
+            can = write_fs.match(file) is not None
+
+            def update():
+                writable[debugger.result] = can
+            debugger.on_return(update)
+            return True
+
+        def handle_close(debugger):
+            writable[debugger.arg0] = False
+            return True
+
+        def handle_dup(debugger):
+            writable[debugger.arg1] = writable[debugger.arg0]
+            return True
+
+        def handle_write(debugger):
+            return writable[debugger.arg0]
 
         def handle_kill(debugger):
+            # Mono likes to signal other instances of it, but doesn't care if it fails.
             def kill_return():
                 debugger.result = -errno.EPERM
             if debugger.arg0 != debugger.pid:
                 debugger.syscall = debugger.getpid_syscall
                 debugger.on_return(kill_return)
             return True
-        sec[sys_kill] = handle_kill
-
-        # Mono uses sys_kill to signal all other instances of it.
 
         def unlink(debugger):
             path = debugger.readstr(debugger.uarg0)
@@ -59,6 +87,14 @@ class Executor(ResourceProxy):
                 print 'Not allowed to unlink:', UNLINK_FS
                 return False
             return True
+
+        sec[sys_open] = handle_open
+        sec[sys_close] = handle_close
+        sec[sys_dup2] = handle_dup
+        sec[sys_dup3] = handle_dup
+        sec[sys_write] = handle_write
+        sec[sys_kill] = handle_kill
+        sec[sys_tgkill] = handle_kill
         sec[sys_unlink] = unlink
         return sec
 

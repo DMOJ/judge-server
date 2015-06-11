0e04bcb120663512cb6605b3a50857534d0ff29c
Quantum <quantum2048@gmail.com>
2015-06-10 22:04:55 -0400

    A new OCAML executor. #49

--- executors/OCAML.py
+++ executors/OCAML.py
@@ -1,51 +1,16 @@
-import os
-import subprocess
-
-from cptbox import CHROOTSecurity, SecurePopen, PIPE, ALLOW
-from error import CompileError
-from .utils import test_executor
-from .resource_proxy import ResourceProxy
+from executors.base_executor import CompiledExecutor
 from judgeenv import env
 
-OCAML_FS = ['.*\.so']
+
+class Executor(CompiledExecutor):
+    ext = '.ml'
+    name = 'OCAML'
+    fs = ['.*\.so']
+    command = env['runtime'].get('ocaml')
+    test_program = 'print_endline read_line'
+
+    def get_compile_args(self):
+        return [env['runtime']['ocaml'], self._code, '-o', self.problem]
 
 
-class Executor(ResourceProxy):
-    def __init__(self, problem_id, source_code):
-        super(Executor, self).__init__()
-        source_code_file = self._file('%s.ml' % problem_id)
-        self.name = self._file(problem_id)
-        with open(source_code_file, 'wb') as fo:
-            fo.write(source_code)
-        ghc_args = [env['runtime']['ocaml'], source_code_file, '-o', problem_id]
-        ghc_process = subprocess.Popen(ghc_args, stderr=subprocess.PIPE, cwd=self._dir)
-        _, compile_error = ghc_process.communicate()
-        if ghc_process.returncode != 0:
-            raise CompileError(compile_error)
-
-    def launch(self, *args, **kwargs):
-        return SecurePopen([self.name] + list(args),
-                           executable=self.name,
-                           security=CHROOTSecurity(OCAML_FS),
-                           address_grace=131072,
-                           time=kwargs.get('time'),
-                           memory=kwargs.get('memory'),
-                           stderr=(PIPE if kwargs.get('pipe_stderr', False) else None),
-                           env={}, cwd=self._dir)
-
-    def launch_unsafe(self, *args, **kwargs):
-        return subprocess.Popen([self.name] + list(args),
-                                executable=self.name,
-                                env={},
-                                cwd=self._dir,
-                                **kwargs)
-
-
-def initialize():
-    if 'ocaml' not in env['runtime']:
-        return False
-    if not os.path.isfile(env['runtime']['ocaml']):
-        return False
-    return test_executor('OCAML', Executor, '''\
-print_endline "Hello, World!"
-''')
+initialize = Executor.initialize

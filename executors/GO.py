8d3326598d6a90f6c6d58c974165365e163ebe27
Quantum <quantum2048@gmail.com>
2015-06-13 15:10:24 -0400

    Go really wants 768MB of address space. #60

--- executors/GO.py
+++ executors/GO.py
@@ -6,7 +6,7 @@ from judgeenv import env
 class Executor(CompiledExecutor):
     ext = '.go'
     name = 'GO'
-    address_grace = 131072
+    address_grace = 786432
     fs = ['.*\.so', '/proc/stat$']
     syscalls = ['getpid', 'getppid', 'clock_getres', 'timer_create', 'timer_settime',
                 'timer_delete', 'modify_ldt', 'sched_getaffinity']

53010f233275a016a6a048af819cfb31c2e17f09
Quantum <quantum2048@gmail.com>
2015-06-13 00:43:55 -0400

    Actually making PHP work. #49

--- executors/PHP.py
+++ executors/PHP.py
@@ -12,5 +12,11 @@ class Executor(ScriptExecutor):
     if 'phpconfdir' in env['runtime']:
         fs += [env['runtime']['phpconfdir']]
 
+    def get_cmdline(self):
+        return ['php', self._code]
+
+    def get_fs(self):
+        return self.fs + [self._code]
+
 
 initialize = Executor.initialize

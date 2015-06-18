8789edf8bb078772ab2b9602c78259dcd8abe639
Tudor Brindus <tbrindus@gmail.com>
2015-06-17 21:58:28 -0400

    Allow PYPY to access itself

---
+++ judge/executors/PYPY.py
@@ -0,0 +1,13 @@
+from .python import PythonExecutor
+from judgeenv import env
+
+PYTHON_FS = ['.*\.(?:so|py[co]?$)', '/proc/cpuinfo$', '/proc/meminfo$', '/etc/localtime$', '/dev/urandom$']
+
+
+class Executor(PythonExecutor):
+    command = env['runtime'].get('pypy')
+    test_program = "print __import__('sys').stdin.read()"
+    name = 'PYPY'
+    fs = PYTHON_FS + [env['runtime']['pypy']] + ([env['runtime']['pypydir']] if 'pypydir' in env['runtime'] else [])
+
+initialize = Executor.initialize

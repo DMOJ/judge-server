b086a9d14f4e8d320b1ca43b4cc9863774066c35
Quantum <quantum2048@gmail.com>
2015-08-08 20:16:32 -0400

    Added @Xyene's SCM executor based on CHICKEN.
    Closes #68

---
+++ executors/SCM.py
@@ -0,0 +1,18 @@
+from .base_executor import CompiledExecutor
+from judgeenv import env
+
+
+class Executor(CompiledExecutor):
+    ext = '.scm'
+    name = 'SCM'
+    fs = ['.*\.so', '/usr/', '/etc/localtime$', '/dev/null$']
+    command = env['runtime'].get('chicken-csc')
+    syscalls = ['getpid', 'getppid', 'clock_getres', 'timer_create', 'timer_settime',
+                'timer_delete', 'newselect', 'select']
+    test_program = '(print "echo: Hello, World!")'
+
+    def get_compile_args(self):
+        return [self.get_command(), self._code]
+
+
+initialize = Executor.initialize

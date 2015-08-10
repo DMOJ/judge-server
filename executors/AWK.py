2c9bfee42486de7d612e197b6dbf2ea15c20d981
Quantum <quantum2048@gmail.com>
2015-08-09 23:50:30 -0400

    It helps if echo works on AWK. @Xyene #74

--- executors/AWK.py
+++ executors/AWK.py
@@ -7,9 +7,9 @@ class Executor(ScriptExecutor):
     ext = '.awk'
     name = 'AWK'
     command = env['runtime'].get('awk')
-    test_program = 'BEGIN { print "echo: Hello, World!" }'
-    fs = ['.*\.(so|awk)', '/dev/urandom$', '/proc/self/maps$']
-    syscalls = ['getgroups32']
+    test_program = '{ print $0 }'
+    fs = ['.*\.(so|awk)', '/dev/(?:urandom|null)$', '/proc/self/maps$']
+    syscalls = ['getgroups32', 'dup2']
 
     def get_cmdline(self):
         return [self.get_command(), '-f', self._code]

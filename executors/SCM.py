142e0f3aed47029c6b9f31842bd8fc13f584f896
Quantum <quantum2048@gmail.com>
2015-08-12 14:27:30 -0400

    SCM executor now actually echos.

--- executors/SCM.py
+++ executors/SCM.py
@@ -9,7 +9,7 @@ class Executor(CompiledExecutor):
     command = env['runtime'].get('chicken-csc')
     syscalls = ['getpid', 'getppid', 'clock_getres', 'timer_create', 'timer_settime',
                 'timer_delete', 'newselect', 'select']
-    test_program = '(print "echo: Hello, World!")'
+    test_program = '(declare (uses extras)) (map print (read-lines))'
 
     def get_compile_args(self):
         return [self.get_command(), self._code]

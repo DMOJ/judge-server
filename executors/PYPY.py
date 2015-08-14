be29166d8497a30730e72f59afcaa1f52e4a2fd3
Quantum <quantum2048@gmail.com>
2015-08-14 12:39:27 -0400

    Why a variable?

--- executors/PYPY.py
+++ executors/PYPY.py
@@ -1,13 +1,12 @@
 from .python import PythonExecutor
 from judgeenv import env
 
-PYTHON_FS = ['.*\.(?:so|py[co]?$)', '/proc/cpuinfo$', '/proc/meminfo$', '/etc/localtime$', '/dev/urandom$']
-
 
 class Executor(PythonExecutor):
     command = env['runtime'].get('pypy')
     test_program = "print __import__('sys').stdin.read()"
     name = 'PYPY'
-    fs = PYTHON_FS + [command] + ([env['runtime']['pypydir']] if 'pypydir' in env['runtime'] else [])
+    fs = ['.*\.(?:so|py[co]?$)', '/proc/cpuinfo$', '/proc/meminfo$', '/etc/localtime$', '/dev/urandom$'] + [command] \
+      + ([env['runtime']['pypydir']] if 'pypydir' in env['runtime'] else [])
 
 initialize = Executor.initialize

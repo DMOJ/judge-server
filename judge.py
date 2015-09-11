2fc6ee088bef84abb2c0737576e11cb2593e7403
Tudor <tbrindus@gmail.com>
2015-09-10 23:00:23 -0400

    Escape HTML; https://github.com/DMOJ/site/issues/189

--- judge.py
+++ judge.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+from collections import OrderedDict
 from textwrap import dedent
 
 import judgeenv
@@ -52,6 +53,14 @@ try:
         return ansi2html.Ansi2HTMLConverter(inline=True).convert(s)
 except ImportError:
     def format_ansi(s):
+        escape = OrderedDict([
+            ('&', '&amp;'),
+            ('<', '&lt;'),
+            ('>', '&gt;'),
+        ])
+        for a, b in escape.items():
+            s = s.replace(a, b)
+
         # http://stackoverflow.com/questions/13506033/filtering-out-ansi-escape-sequences
         return re.sub(r'\x1b\[([0-9,A-Z]{1,2}(;[0-9]{1,2})?(;[0-9]{3})?)?[m|K]?', '', s)
 

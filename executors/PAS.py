c4cd494a34ad5428fa3249580980ced1dee25f86
Tudor Brindus <tbrindus@gmail.com>
2015-10-04 12:09:19 -0400

    Fully fix #95

--- executors/PAS.py
+++ executors/PAS.py
@@ -23,9 +23,9 @@ end.
     def get_compile_args(self):
         return [env['runtime']['fpc'], '-Fe/dev/stderr', '-So', '-O2', self._code, '-o' + self.get_compiled_file()]
 
-    # def get_compile_output(self, process):
-    #     output = process.communicate()[0]
-    #     return output if 'Warning:' in output or 'Note:' in output else None
+    def get_compile_output(self, process):
+        output = process.communicate()[1]
+        return output if 'Fatal:' in output or 'Warning:' in output or 'Note:' in output else ''
 
 
 initialize = Executor.initialize

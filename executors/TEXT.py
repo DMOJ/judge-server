8ac1b220390720701ec80e8e1bdf5e61fa7c013d
Tudor Brindus <tbrindus@gmail.com>
2015-12-18 23:45:34 -0500

    Add TEXT executor; #87

---
+++ executors/TEXT.py
@@ -0,0 +1,13 @@
+from .base_executor import ScriptExecutor
+from judgeenv import env
+
+
+class Executor(ScriptExecutor):
+    ext = '.txt'
+    name = 'TEXT'
+    command = env['runtime'].get('cat', '/bin/cat')
+    test_program = 'echo: Hello, World!\n'
+    fs = ['.*\.(?:so|txt)']
+    syscalls = ['fadvise64_64']
+
+initialize = Executor.initialize

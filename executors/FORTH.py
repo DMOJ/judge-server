96dda4083b8b2e9ff56170b7442a9e758e5e4aef
Tudor Brindus <tbrindus@gmail.com>
2015-12-18 23:48:30 -0500

    Add Forth executor; #98

---
+++ executors/FORTH.py
@@ -0,0 +1,19 @@
+from judgeenv import env
+from .base_executor import ScriptExecutor
+
+
+class Executor(ScriptExecutor):
+    name = 'FORTH'
+    command = env['runtime'].get('gforth')
+    ext = '.fs'
+    test_program = '''\
+: HELLO  ( -- ) ." echo: Hello, World!" CR ;
+
+HELLO
+'''
+    fs = ['.*?\.(?:so|fs|fi|gforth-history)']
+
+    def get_cmdline(self):
+         return [self.get_command(), self._code, '-e', 'bye']
+
+initialize = Executor.initialize

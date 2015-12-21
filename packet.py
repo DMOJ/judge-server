dd728588b46ec13f6f1b1ca9b9e0fe5f89e0770f
Quantum <quantum2048@gmail.com>
2015-12-21 12:47:09 -0500

    set is not JSON serializable

--- packet.py
+++ packet.py
@@ -320,7 +320,7 @@ class AMQPPacketManager(object):
         logger.info('Update problems')
         self._send_ping_packet({
             'name': 'problem-update',
-            'problems': self.problems,
+            'problems': list(self.problems),
         })
 
     def supported_executors_packet(self):

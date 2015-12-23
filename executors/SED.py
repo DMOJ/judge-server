fcac525d3f974c7d4a1e90c1adc444c6d6e72018
Quantum <quantum2048@gmail.com>
2015-12-23 14:25:12 -0500

    Add sed executor #123

---
+++ executors/SED.py
@@ -0,0 +1,18 @@
+
+from .base_executor import ScriptExecutor
+from judgeenv import env
+
+
+class Executor(ScriptExecutor):
+    ext = '.sed'
+    name = 'SED'
+    command = env['runtime'].get('sed')
+    test_program = '''s/.*/echo: Hello, World!/
+q'''
+    fs = ['.*\.(so|sed)', '/dev/urandom$', '/proc/self/maps$', '/proc/filesystems$', '/+lib/charset.alias$']
+    syscalls = ['getgroups32', 'statfs64']
+
+    def get_cmdline(self):
+        return [self.get_command(), '-f', self._code]
+
+initialize = Executor.initialize

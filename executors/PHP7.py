3299fa72ebe21c6e7a29618a4010f6a44f801aac
Quantum <quantum2048@gmail.com>
2015-12-23 15:28:38 -0500

    PHP7 uses a different .ini organization #117

--- executors/PHP7.py
+++ executors/PHP7.py
@@ -5,6 +5,7 @@ from judgeenv import env
 class Executor(PHPExecutor):
     name = 'PHP7'
     command = env['runtime'].get('php7')
+    fs = ['.*\.so', '/etc/localtime$', '.*\.ini$']
 
 
 initialize = Executor.initialize

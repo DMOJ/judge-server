cc4241ab9844cc30b2d261af0d00fbf4c5f00d08
Tudor Brindus <tbrindus@gmail.com>
2015-12-23 15:16:18 -0500

    Add PHP7 executor #117

---
+++ executors/PHP7.py
@@ -0,0 +1,21 @@
+from .base_executor import ScriptExecutor
+from judgeenv import env
+
+
+class Executor(ScriptExecutor):
+    ext = '.php'
+    name = 'PHP7'
+    command = env['runtime'].get('php7')
+    address_grace = 131072
+    test_program = '<?php while($f = fgets(STDIN)) echo $f;'
+
+    fs = ['.*\.so', '/etc/localtime$', '.*\.ini$']
+
+    def get_cmdline(self):
+        return ['php', self._code]
+
+    def get_fs(self):
+        return self.fs + [self._code]
+
+
+initialize = Executor.initialize

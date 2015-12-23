61b08137aad6b5b1c41bbf8d12a1e956892a6c04
Quantum <quantum2048@gmail.com>
2015-12-23 15:11:06 -0500

    Add CoffeeScript executor #124

---
+++ executors/COFFEE.py
@@ -0,0 +1,18 @@
+from .base_executor import CompiledExecutor
+from judgeenv import env
+
+
+class Executor(CompiledExecutor):
+    ext = '.coffee'
+    name = 'COFFEE'
+    fs = ['.*\.so', '/usr/', '/etc/localtime$', '/dev/null$']
+    command = env['runtime'].get('chicken-csc')
+    syscalls = ['getpid', 'getppid', 'clock_getres', 'timer_create', 'timer_settime',
+                'timer_delete', 'newselect', 'select']
+    test_program = '(declare (uses extras)) (map print (read-lines))'
+
+    def get_compile_args(self):
+        return [self.get_command(), self._code]
+
+
+initialize = Executor.initialize

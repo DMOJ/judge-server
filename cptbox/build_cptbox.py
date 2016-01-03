4b0db82f7f0400f59b153952eda66822a7de49fc
Quantum <quantum2048@gmail.com>
2016-01-03 16:52:00 -0500

    Allow compilation of cptbox without cython #132

--- cptbox/build_cptbox.py
+++ cptbox/build_cptbox.py
@@ -1,13 +1,15 @@
 import os
 import shutil
-from distutils.core import Extension
-from distutils.command.build_ext import build_ext
-from distutils.dist import Distribution
 from distutils import log
+from distutils.command.build_ext import build_ext
+from distutils.core import Extension
+from distutils.dist import Distribution
 from distutils.sysconfig import get_config_vars
 
-from Cython.Build import cythonize
-
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    cythonize = None
 
 if os.path.dirname(__file__):
     os.chdir(os.path.dirname(__file__))
@@ -17,13 +19,27 @@ opt, = get_config_vars('OPT')
 os.environ['OPT'] = ' '.join(flag for flag in opt.split() if flag != '-Wstrict-prototypes')
 extra_compile_args = ['-march=native', '-O3']
 
-sources = ['_cptbox.pyx', 'ptdebug.cpp', 'ptdebug_x86.cpp', 'ptdebug_x64.cpp',
+sources = ['ptdebug.cpp', 'ptdebug_x86.cpp', 'ptdebug_x64.cpp',
            'ptdebug_x86_on_x64.cpp', 'ptdebug_x32.cpp', 'ptdebug_arm.cpp', 'ptproc.cpp']
 
+if cythonize is None:
+    if os.path.exists('_cptbox.cpp'):
+        sources.insert(0, '_cptbox.cpp')
+    else:
+        print 'You need to have either cython or _cptbox.cpp prebuilt.'
+        raise SystemExit(1)
+else:
+    sources.insert(0, '_cptbox.pyx')
+
+ext_modules = [Extension('_cptbox', sources=sources,
+                         language='c++', libraries=['rt'],
+                         extra_compile_args=extra_compile_args)]
+
+if cythonize is not None:
+    ext_modules = cythonize(ext_modules)
+
 distribution = Distribution({
-    'ext_modules': cythonize([Extension('_cptbox', sources=sources,
-                                        language='c++', libraries=['rt'],
-                                        extra_compile_args=extra_compile_args)])
+    'ext_modules': ext_modules
 })
 
 command = build_ext(distribution)

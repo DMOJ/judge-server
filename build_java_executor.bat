bd0467b4071c7127df2d555dfe4a9dddf0001aa9
Tudor Brindus <tbrindus@gmail.com>
2016-01-10 13:07:24 -0500

    Build for 1.7 even with 1.8 javac

--- build_java_executor.bat
+++ build_java_executor.bat
@@ -3,7 +3,7 @@ del /s /q objs
 rmdir objs
 md objs
 cd src
-javac -d ..\objs ca\dmoj\java\*.java
+javac -source 1.7 -target 1.7 -d ..\objs ca\dmoj\java\*.java
 cd ..\objs
 del ..\..\executors\java_executor.jar
 jar cmf ..\src\META-INF\MANIFEST.MF ..\..\executors\java_executor.jar ca\dmoj\java\*.class

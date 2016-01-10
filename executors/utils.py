5851319a2a913b248e1f3ef781eac78698dcd69b
Quantum <quantum2048@gmail.com>
2016-01-10 18:16:10 -0500

    16M is too restrictive

--- executors/utils.py
+++ executors/utils.py
@@ -5,7 +5,7 @@ def test_executor(name, Executor, code, problem='self-test', sandbox=True):
     print 'Self-testing: %s executor:' % name,
     try:
         executor = Executor(problem, code)
-        proc = executor.launch(time=1, memory=16384) if sandbox else executor.launch_unsafe()
+        proc = executor.launch(time=1, memory=65536) if sandbox else executor.launch_unsafe()
         stdout, stderr = proc.communicate()
         res = stdout.strip() == 'Hello, World!' and not stderr
         print ['Failed', 'Success'][res]

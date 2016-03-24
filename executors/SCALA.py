ca2805210179f25d600a3fc809951f61cb078e85
Quantum <quantum2048@gmail.com>
2016-03-24 18:24:46 -0400

    Made Scala work with new sandbox #155

--- executors/SCALA.py
+++ executors/SCALA.py
@@ -1,27 +1,18 @@
-import os
-import errno
-
-from error import CompileError
-from .java_executor import JavaExecutor, JavaProcess, deunicode, JAVA_SANDBOX
 from judgeenv import env
-
-
-class ScalaProcess(JavaProcess):
-    def get_command_line(self, class_name, cwd, memory_limit, time_limit, vm):
-        return ['scala', JAVA_SANDBOX, cwd, class_name, str(int(time_limit * 1000)), 'state']
-
-    def get_environ(self, class_name, cwd, java, memory_limit, time_limit, vm):
-        env = os.environ.copy()
-        env.update({'JAVA_OPTS': '-Xmx%sK -%s' % (memory_limit, vm)})
-        return env
+from .java_executor import JavaExecutor
 
 
 class Executor(JavaExecutor):
     name = 'SCALA'
     ext = '.scala'
+
     compiler = env['runtime'].get('scalac')
-    vm = env['runtime'].get('scala')
-    process_class = ScalaProcess
+    vm = env['runtime'].get('java')
+
+    # Simply run bash -x $(which scala) and copy all arguments after -Xmx and -Xms
+    # and add it as a list in the configuration.
+    args = env['runtime'].get('scala_args')
+
     test_program = '''\
 object self_test {
   def main(args: Array[String]) {
@@ -30,21 +21,17 @@ object self_test {
 }
 '''
 
-    def create_files(self, problem_id, source_code):
-        source_code = deunicode(source_code)
-        class_name = problem_id
-        self._code = self._file('%s.scala' % class_name)
-        try:
-            with open(self._code, 'wb') as fo:
-                fo.write(source_code)
-        except IOError as e:
-            if e.errno in (errno.ENAMETOOLONG, errno.ENOENT):
-                raise CompileError('Why do you need a class name so long? '
-                                   'As a judge, I sentence your code to death.')
-            raise
-        self._class_name = class_name
+    def create_files(self, problem_id, source_code, *args, **kwargs):
+        super(Executor, self).create_files(problem_id, source_code, *args, **kwargs)
+        self._class_name = problem_id
+
+    def get_cmdline(self):
+        res = super(Executor, self).get_cmdline()
+        res[-2:-1] = self.args
+        return res
 
     def get_compile_args(self):
         return [self.get_compiler(), self._code]
+
 
 initialize = Executor.initialize

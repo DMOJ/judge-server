35cef469747129f3578447d2efb6b3d112328678
Quantum <quantum2048@gmail.com>
2016-05-01 19:11:48 -0400

    Actually pass generator input file.

--- dmoj/config.py
+++ dmoj/config.py
@@ -136,7 +136,16 @@ class TestCase(object):
 
         executor = self.problem.generator_manager.get_generator(filename, flags)
         proc = executor.launch_unsafe(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        self._generated = map(self._normalize, proc.communicate())
+
+        input_file = self.config['in']
+        input = None
+        if input_file is not None:
+            try:
+                with open(input_file) as file:
+                    input = file.read()
+            except IOError:
+                pass
+        self._generated = map(self._normalize, proc.communicate(input))
 
     def input_data(self):
         gen = self.config.generator

ccf695dbd9ca99033b49a572a30e6264baec5c25
Quantum <quantum2048@gmail.com>
2016-05-20 23:13:36 -0400

    Convert COBOL executor to #105

--- dmoj/executors/CBL.py
+++ dmoj/executors/CBL.py
@@ -1,46 +1,32 @@
 import subprocess
 
-from dmoj.cptbox import CHROOTSecurity, SecurePopen
-from dmoj.error import CompileError
-from .utils import test_executor
-from dmoj.executors.resource_proxy import ResourceProxy
+from .base_executor import CompiledExecutor
 from dmoj.judgeenv import env
 
-CBL_FS = ['.*\.so']
 
-
-class Executor(ResourceProxy):
-    def __init__(self, problem_id, source_code):
-        super(Executor, self).__init__()
-        source_code_file = self._file('%s.cbl' % problem_id)
-        with open(source_code_file, 'wb') as fo:
-            fo.write(source_code)
-        self._executable = self._file(str(problem_id))
-        cbl_args = [env['runtime']['cobc'], '-x', source_code_file]
-        cbl_process = subprocess.Popen(cbl_args, stdout=subprocess.PIPE, cwd=self._dir)
-        compile_error, _ = cbl_process.communicate()
-        if cbl_process.returncode != 0:
-            raise CompileError(compile_error)
-        self.name = problem_id
-        self.warning = compile_error if 'Warning:' in compile_error or 'Note:' in compile_error else None
-
-    def launch(self, *args, **kwargs):
-        return SecurePopen([self.name] + list(args),
-                           executable=self._executable,
-                           security=CHROOTSecurity(CBL_FS),
-                           address_grace=131072,
-                           time=kwargs.get('time'),
-                           memory=kwargs.get('memory'),
-                           env={}, cwd=self._dir)
-
-
-def initialize(sandbox=True):
-    if 'cobc' not in env['runtime']:
-        return False
-    return test_executor('CBL', Executor, '''\
+class Executor(CompiledExecutor):
+    ext = '.cbl'
+    name = 'CBL'
+    command = env['runtime'].get('cobc')
+    fs = ['.*\.so']
+    address_grace = 131072
+    test_program = '''\
 	IDENTIFICATION DIVISION.
 	PROGRAM-ID. HELLO-WORLD.
 	PROCEDURE DIVISION.
-		DISPLAY 'Hello, World!'.
+		DISPLAY 'echo: Hello, World!'.
 		STOP RUN.
-''')
+'''
+
+    def get_compile_args(self):
+        return [self.get_command(), '-x', self._code]
+
+    def get_compile_popen_kwargs(self):
+        return {'stdout': subprocess.PIPE, 'stderr': None}
+
+    def get_compile_output(self, process):
+        output = process.communicate()[0]
+        return output if 'Error:' in output or 'Note:' in output or 'Warning:' in output else None
+
+
+initialize = Executor.initialize

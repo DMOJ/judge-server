036dfcae6bf1b148c5420ec6801c46543db85a6d
Quantum <quantum2048@gmail.com>
2016-05-28 22:38:09 -0400

    I am rather tired of this annoying output.

--- dmoj/executors/PAS.py
+++ dmoj/executors/PAS.py
@@ -1,3 +1,5 @@
+import os
+
 from dmoj.executors.base_executor import CompiledExecutor
 from dmoj.judgeenv import env
 
@@ -14,6 +16,18 @@ begin
     writeln(line);
 end.
 '''
+
+    def __init__(self, *args, **kwargs):
+        self.devnull = open(os.devnull, 'w')
+        super(Executor, self).__init__(*args, **kwargs)
+
+    def cleanup(self):
+        if hasattr(self, 'devnull'):
+            self.devnull.close()
+        super(Executor, self).cleanup()
+
+    def get_compile_popen_kwargs(self):
+        return {'stdout': self.devnull}
 
     def get_compile_args(self):
         return [env['runtime']['fpc'], '-Fe/dev/stderr', '-So', '-O2', self._code]

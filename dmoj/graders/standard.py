11a0b6c1f4bdb6a2036b4292751ed7b4c8c48ff8
Quantum <quantum2048@gmail.com>
2016-06-05 13:15:31 -0400

    Split up standard grader into overridable pieces.

--- dmoj/graders/standard.py
+++ dmoj/graders/standard.py
@@ -31,20 +31,43 @@ class StandardGrader(BaseGrader):
         result.r_execution_time = process.r_execution_time or 0.0
 
         # Translate status codes/process results into Result object for status codes
+        self.set_result_flag(process, result)
 
-        if process.returncode > 0:
-            # print>> sys.stderr, 'Exited with error: %d' % process.returncode
-            result.result_flag |= Result.IR
-        if process.returncode < 0:
-            # None < 0 == True
-            # if process.returncode is not None:
-            # print>> sys.stderr, 'Killed by signal %d' % -process.returncode
-            result.result_flag |= Result.RTE  # Killed by signal
-        if process.tle:
-            result.result_flag |= Result.TLE
-        if process.mle:
-            result.result_flag |= Result.MLE
+        check = self.check_result(case, result)
 
+        # checkers must either return a boolean (True: full points, False: 0 points)
+        # or a CheckerResult, so convert to CheckerResult if it returned bool
+        if not isinstance(check, CheckerResult):
+            check = CheckerResult(check, case.points if check else 0.0)
+
+        result.result_flag |= [Result.WA, Result.AC][check.passed]
+        result.points = check.points
+
+        self.update_feedback(check, error, process, result)
+
+        return result
+
+    def update_feedback(self, check, error, process, result):
+        result.feedback = (check.feedback or
+                           (process.feedback if hasattr(process, 'feedback') else
+                            getattr(self.binary, 'update_feedback', lambda x, y, z: '')(error, result, process)))
+        if not result.feedback and hasattr(process, 'signal') and process.signal and result.get_main_code() in [
+            Result.IR, Result.RTE]:
+            result.feedback = strsignal(process.signal)
+
+        # On Linux we can provide better help messages
+        if hasattr(process, 'protection_fault') and process.protection_fault:
+            sigid, callname = process.protection_fault
+            callname = callname.replace('sys_', '', 1)
+            message = {
+                'open': 'opening files is not allowed',
+                'socketcall': 'accessing the network is not allowed',
+                'socket': 'accessing the network is not allowed',
+                'clone': 'threading is not allowed'
+            }.get(callname, '%s syscall disallowed' % callname)
+            result.feedback = message
+
+    def check_result(self, case, result):
         # If the submission didn't crash and didn't time out, there's a chance it might be AC
         # We shouldn't run checkers if the submission is already known to be incorrect, because some checkers
         # might be very computationally expensive.
@@ -61,35 +84,21 @@ class StandardGrader(BaseGrader):
             # Solution is guaranteed to receive 0 points
             check = False
 
-        # checkers must either return a boolean (True: full points, False: 0 points)
-        # or a CheckerResult, so convert to CheckerResult if it returned bool
-        if not isinstance(check, CheckerResult):
-            check = CheckerResult(check, case.points if check else 0.0)
+        return check
 
-        result.result_flag |= [Result.WA, Result.AC][check.passed]
-
-        result.points = check.points
-        result.feedback = (check.feedback or
-                           (process.feedback if hasattr(process, 'feedback') else
-                            getattr(self.binary, 'get_feedback', lambda x, y, z: '')(error, result, process)))
-
-        if not result.feedback and hasattr(process, 'signal') and process.signal and result.get_main_code() in [
-            Result.IR, Result.RTE]:
-            result.feedback = strsignal(process.signal)
-
-        # On Linux we can provide better help messages
-        if hasattr(process, 'protection_fault') and process.protection_fault:
-            sigid, callname = process.protection_fault
-            callname = callname.replace('sys_', '', 1)
-            message = {
-                'open':         'opening files is not allowed',
-                'socketcall':   'accessing the network is not allowed',
-                'socket':       'accessing the network is not allowed',
-                'clone':        'threading is not allowed'
-            }.get(callname,     '%s syscall disallowed' % callname)
-            result.feedback = message
-
-        return result
+    def set_result_flag(self, process, result):
+        if process.returncode > 0:
+            # print>> sys.stderr, 'Exited with error: %d' % process.returncode
+            result.result_flag |= Result.IR
+        if process.returncode < 0:
+            # None < 0 == True
+            # if process.returncode is not None:
+            # print>> sys.stderr, 'Killed by signal %d' % -process.returncode
+            result.result_flag |= Result.RTE  # Killed by signal
+        if process.tle:
+            result.result_flag |= Result.TLE
+        if process.mle:
+            result.result_flag |= Result.MLE
 
     def _interact_with_process(self, case, result, input):
         process = self._current_proc

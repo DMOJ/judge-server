65dcf3f7b57d59113f3866455b1920abb7d171cf
Quantum <quantum2048@gmail.com>
2016-06-05 13:24:22 -0400

    Custom grader doesn't actually work.

--- dmoj/graders/custom.py
+++ dmoj/graders/custom.py
@@ -1,5 +1,4 @@
 import os
-import traceback
 
 from dmoj.judgeenv import get_problem_root
 from dmoj.utils.module import load_module_from_file
@@ -9,16 +8,19 @@ class CustomGrader(object):
     def __init__(self, judge, problem, language, source):
         self.judge = judge
         mod = load_module_from_file(os.path.join(get_problem_root(problem.id), problem.config['custom_judge']))
-        self.custom_judge = mod.Grader(judge, problem, language, source)
+        self._grader = mod.Grader(judge, problem, language, source)
 
     def grade(self, case):
         try:
-            return self.custom_judge.grade(case)
+            return self._grader.grade(case)
         except:
             self.judge.internal_error()
 
     def terminate_grading(self):
         try:
-            return self.custom_judge.terminate_grading()
+            return self._grader.terminate_grading()
         except:
             self.judge.internal_error()
+
+    def __getattr__(self, item):
+        return getattr(self._grader, item)

da74faca8649bc7aa0673002b45a889a43ed66c8
Tudor Brindus <tbrindus@gmail.com>
2016-06-26 18:31:22 -0400

    Verify tokens per line for floats checker; #182

--- dmoj/checkers/floats.py
+++ dmoj/checkers/floats.py
@@ -14,6 +14,10 @@ def check(process_output, judge_output, precision, **kwargs):
         for process_line, judge_line in izip(process_lines, judge_lines):
             process_tokens = process_line.split()
             judge_tokens = judge_line.split()
+            
+            if len(process_tokens) != len(judge_tokens):
+                return False
+            
             for process_token, judge_token in izip(process_tokens, judge_tokens):
                 # Allow mixed tokens, for lines like "abc 0.68 def 0.70"
                 try:

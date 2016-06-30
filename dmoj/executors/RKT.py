30f6e6bb1019561abc663ab4587a361dde6720d0
Quantum <quantum2048@gmail.com>
2016-06-29 22:59:04 -0400

    Compile racket to bytecode.

--- dmoj/executors/RKT.py
+++ dmoj/executors/RKT.py
@@ -1,15 +1,18 @@
 from dmoj.cptbox.handlers import ACCESS_DENIED
-from dmoj.executors.base_executor import ScriptExecutor
+from dmoj.executors.base_executor import CompiledExecutor
 from dmoj.judgeenv import env
 
 
-class Executor(ScriptExecutor):
+class Executor(CompiledExecutor):
     ext = '.rkt'
     name = 'RKT'
-    fs = ['.*\.(?:so|rkt?$)', '/dev/tty$', '/proc/meminfo$', '.*racket.*', '/proc/stat$',
+    fs = ['.*\.(?:so|rkt?$|zo$)', '/dev/tty$', '/proc/meminfo$', '.*racket.*', '/proc/stat$',
           '/proc/self/maps$', '/usr/lib', '/etc/nsswitch.conf$',
           '/etc/passwd$', '/dev/null$', '/sys/devices/system/cpu/online$']
+
+    raco = env['runtime'].get('raco')
     command = env['runtime'].get('racket')
+
     syscalls = ['epoll_create', 'sigprocmask', 'rt_sigreturn', 'epoll_wait', 'poll',
                 ('socketcall', ACCESS_DENIED), ('socket', ACCESS_DENIED),
                 ('prctl', lambda debugger: debugger.arg0 in (15,))]
@@ -19,5 +22,21 @@ class Executor(ScriptExecutor):
 #lang racket
 (displayln (read-line))
 '''
+
+    def get_compile_args(self):
+        return [self.raco, 'make', self._code]
+
+    def get_cmdline(self):
+        return [self.get_command(), self._code]
+
+    def get_executable(self):
+        return self.get_command()
+
+    @classmethod
+    def initialize(cls, sandbox=True):
+        if cls.raco is None:
+            return False
+        return super(Executor, cls).initialize(sandbox)
+
 
 initialize = Executor.initialize

b9c69c5d2bf7824813f5b4fb9c9d8f236c89844a
Quantum <quantum2048@gmail.com>
2016-07-01 18:18:32 -0400

    F# autoconfig; #105

--- dmoj/executors/FS.py
+++ dmoj/executors/FS.py
@@ -9,8 +9,17 @@ class Executor(CLRExecutor):
     compile_args = ['--nologo', '--out:{exe}', '{source}']
     compiler_time_limit = 20
 
+    command_paths = [
+        r'C:\Program Files (x86)\Microsoft SDKs\F#\4.0\Framework\v4.0\Fsc.exe',
+        r'C:\Program Files\Microsoft SDKs\F#\4.0\Framework\v4.0\Fsc.exe',
+    ]
+
     test_program = '''\
 open System
 let input = System.Console.ReadLine()
 Console.WriteLine(input)
 '''
+
+    @classmethod
+    def get_find_first_mapping(cls):
+        return {cls.compiler: cls.command_paths}

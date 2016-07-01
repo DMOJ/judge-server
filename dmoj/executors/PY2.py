f822cd99b6b5cbb24245e4e7e64695526a46d7aa
Quantum <quantum2048@gmail.com>
2016-06-30 22:44:20 -0400

    Attempt to define autoconfig for PY2; #105

--- dmoj/executors/PY2.py
+++ dmoj/executors/PY2.py
@@ -4,6 +4,7 @@ from .python_executor import PythonExecutor
 
 class Executor(PythonExecutor):
     command = 'python'
+    command_paths = ['/usr/bin/python2.7', '/usr/bin/python2', '/usr/bin/python']
     test_program = "print __import__('sys').stdin.read()"
     name = 'PY2'
     fs = ['.*\.(?:py[co]?$)', '.*/lib(?:32|64)?/python[\d.]+/.*', '.*/lib/locale/']

259b6b7d50a3e209e43c3abfbf71b5354e0ea4c9
Quantum <quantum2048@gmail.com>
2016-06-30 22:02:59 -0400

    Need to load environment; #105

--- dmoj/executors/__main__.py
+++ dmoj/executors/__main__.py
@@ -1,5 +1,10 @@
 from dmoj.executors import load_executors
+from dmoj.judgeenv import load_env
 
+
+def main():
+    load_env(cli=True)
+    load_executors()
 
 if __name__ == '__main__':
-    load_executors()
+    main()

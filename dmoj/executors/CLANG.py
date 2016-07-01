6a8cdb5237f77dfa2adcbba536fd2b4efbd2ee91
Quantum <quantum2048@gmail.com>
2016-07-01 00:25:25 -0400

    Add clang commands; #105

--- dmoj/executors/CLANG.py
+++ dmoj/executors/CLANG.py
@@ -1,5 +1,7 @@
 from dmoj.executors.C import Executor as CExecutor
 
+
 class Executor(CExecutor):
     command = 'clang'
+    command_paths = ['clang-%s' % i for i in ['3.9', '3.8', '3.7', '3.6', '3.5']] + ['clang']
     name = 'CLANG'

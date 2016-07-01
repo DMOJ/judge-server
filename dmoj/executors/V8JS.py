e1dfc4e4f83ac895c1adfbd581dd9e2b12a2bccf
Tudor <tbrindus@gmail.com>
2016-06-30 22:38:19 -0400

    Typo for .*; #77

--- dmoj/executors/V8JS.py
+++ dmoj/executors/V8JS.py
@@ -7,4 +7,4 @@ class Executor(ScriptExecutor):
     command = 'v8dmoj'
     test_program = 'print(gets());'
     address_grace = 786432
-    fs = ['*.\.js']  # v8dmoj binaries are distributed statically linked
+    fs = ['.*\.js']  # v8dmoj binaries are distributed statically linked

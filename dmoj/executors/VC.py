7fdccb8d2654c488db9c92750fd3fc5e092c62d0
Quantum <quantum2048@gmail.com>
2016-06-30 21:23:51 -0400

    Successfully turned VC executor, closing #49

--- dmoj/executors/VC.py
+++ dmoj/executors/VC.py
@@ -1,11 +1,7 @@
 import os
-import subprocess
 
-from dmoj.wbox import WBoxPopen
-from dmoj.error import CompileError
-from .utils import test_executor
-from dmoj.executors.resource_proxy import ResourceProxy
 from dmoj.judgeenv import env
+from .base_executor import CompiledExecutor
 
 VC_ENV = env['runtime'].get('vc_env', {})
 VC_COMPILE = os.environ.copy()
@@ -15,53 +11,51 @@ VC_COMPILE.update((k.encode('mbcs'), v.encode('mbcs')) for k, v in
 VC_ENV = dict((k.encode('mbcs'), v.encode('mbcs')) for k, v in VC_ENV.iteritems())
 
 
-class Executor(ResourceProxy):
-    def __init__(self, problem_id, main_source, aux_sources=None, **kwargs):
-        super(Executor, self).__init__()
+class Executor(CompiledExecutor):
+    name = 'VC'
+    ext = '.cpp'
+    command = env['runtime'].get('cl.exe')
 
-        if not aux_sources:
-            aux_sources = {}
-        aux_sources[problem_id + '.cpp'] = main_source
-        sources = []
-        for name, source in aux_sources.iteritems():
-            if '.' not in name:
-                name += '.cpp'
-            with open(self._file(name), 'wb') as fo:
-                fo.write(source)
-            sources.append(name)
-        output_file = self._file('%s.exe' % problem_id)
-
-        cl_args = ['cl', '-nologo'] + sources + ['-W4', '-DONLINE_JUDGE', '-DWIN32', '-D_CRT_SECURE_NO_WARNINGS',
-                                                 '-EHsc', '-Ox', '-Fe%s' % output_file, '-link', '-stack:67108864']
-        cl_process = subprocess.Popen(cl_args, stderr=subprocess.PIPE, executable=env['runtime']['cl.exe'],
-                                      cwd=self._dir, env=VC_COMPILE)
-
-        _, compile_error = cl_process.communicate()
-        if cl_process.returncode != 0:
-            raise CompileError(compile_error)
-        self._executable = output_file
-        self.name = problem_id
-        self.warning = compile_error
-
-    def launch(self, *args, **kwargs):
-        return WBoxPopen([self.name] + list(args), executable=self._executable,
-                         time=kwargs.get('time'), memory=kwargs.get('memory'),
-                         cwd=self._dir, env=VC_ENV or None, network_block=True)
-
-    def launch_unsafe(self, *args, **kwargs):
-        return subprocess.Popen([self.name] + list(args), executable=self._executable,
-                                env=VC_ENV or None, cwd=self._dir, **kwargs)
-
-
-def initialize(sandbox=True):
-    if 'cl.exe' not in env['runtime'] or not os.path.isfile(env['runtime']['cl.exe']):
-        return False
-    return test_executor('VC', Executor, r'''
+    test_program = r'''
 #include <iostream>
 
 int main() {
-    auto message = "Hello, World!\n";
+    auto message = std::cin.rdbuf();
     std::cout << message;
     return 0;
 }
-''', sandbox=sandbox)
+'''
+
+    def create_files(self, problem_id, main_source, aux_sources=None, *args, **kwargs):
+        if not aux_sources:
+            aux_sources = {}
+        aux_sources[problem_id + self.ext] = main_source
+        sources = []
+        for name, source in aux_sources.iteritems():
+            if '.' not in name:
+                name += self.ext
+            with open(self._file(name), 'wb') as fo:
+                fo.write(source)
+            sources.append(name)
+        self.sources = sources
+        self.devnull = open(os.devnull, 'w')
+
+    def cleanup(self):
+        self.devnull.close()
+        super(Executor, self).cleanup()
+
+    def get_compile_args(self):
+        return ['cl', '-nologo'] + self.sources + ['-W4', '-DONLINE_JUDGE', '-DWIN32', '-D_CRT_SECURE_NO_WARNINGS',
+                                                   '-EHsc', '-Ox', '-Fe%s' % self.get_compiled_file(),
+                                                   '-link', '-stack:67108864']
+
+    def get_compile_popen_kwargs(self):
+        return {'executable': self.get_command(), 'stdout': self.devnull}
+
+    def get_compile_env(self):
+        return VC_COMPILE
+
+    def get_env(self):
+        return VC_ENV
+
+initialize = Executor.initialize

a7133530ceca4e1ce1b5219abf3c8185b3596e11
Tudor <tbrindus@gmail.com>
2016-06-30 22:58:11 -0400

    Use new #105 features for TCL

--- dmoj/executors/TCL.py
+++ dmoj/executors/TCL.py
@@ -1,6 +1,3 @@
-import errno
-
-from dmoj.cptbox.syscalls import *
 from dmoj.executors.base_executor import ScriptExecutor
 
 
@@ -9,29 +6,11 @@ class Executor(ScriptExecutor):
     name = 'TCL'
     address_grace = 131072
     command = 'tclsh'
+    syscalls = ['connect', 'access', 'getsockname',
+                # TCL uses some handles internally
+                ('write', lambda debugger: debugger.arg0 <= 4)]
     fs = ['.*\.tcl', '/etc/nsswitch\.conf$', '/etc/passwd$']
     test_program = '''\
 gets stdin input
 puts $input
 '''
-
-    def get_security(self, **kwargs):
-        security = super(Executor, self).get_security(**kwargs)
-
-        def handle_socket(debugger):
-            def socket_return():
-                debugger.result = -errno.EACCES
-
-            debugger.syscall = debugger.getpid_syscall
-            debugger.on_return(socket_return)
-            return True
-
-        security[sys_tgkill] = True
-        security[sys_write] = lambda debugger: debugger.arg0 <= 4  # TCL uses some handles internally
-        security[sys_socket] = handle_socket
-        security[sys_socketcall] = handle_socket
-        security[sys_connect] = True
-        security[sys_access] = True
-        security[sys_getsockname] = True
-
-        return security

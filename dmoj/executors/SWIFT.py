a0a1cb02cb9b6dfd33477a9cfed05dd4836c5090
Tudor <tbrindus@gmail.com>
2016-07-02 15:24:27 -0400

    Pass in pty for swiftc's stderr; #119

--- dmoj/executors/SWIFT.py
+++ dmoj/executors/SWIFT.py
@@ -1,3 +1,6 @@
+import os
+import pty
+
 from .base_executor import CompiledExecutor
 
 
@@ -9,3 +12,28 @@ class Executor(CompiledExecutor):
 
     def get_compile_args(self):
         return [self.get_command(), self._code]
+
+    def get_compile_popen_kwargs(self):
+        return {'stderr': self._slave, 'stdout': self._slave, 'stdin': self._slave}
+
+    def get_compile_process(self):
+        self._master, self._slave = pty.openpty()
+        proc = super(Executor, self).get_compile_process()
+
+        class io_error_wrapper(object):
+            def __init__(self, fd):
+                self.fd = fd
+
+            def read(self, *args, **kwargs):
+                try:
+                    return self.fd.read(*args, **kwargs)
+                except (IOError, OSError):
+                    return ''
+
+            def __getattr__(self, attr):
+                return getattr(self.fd, attr)
+
+        proc.stderr = io_error_wrapper(os.fdopen(self._master, 'r'))
+
+        os.close(self._slave)
+        return proc

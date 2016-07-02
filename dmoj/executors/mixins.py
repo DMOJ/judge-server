94c6a26c1e4d0f91155b1b59f371fbe13dda823e
Tudor <tbrindus@gmail.com>
2016-07-02 15:53:02 -0400

    Thoroughly comment the hacks from #119

--- dmoj/executors/mixins.py
+++ dmoj/executors/mixins.py
@@ -19,20 +19,20 @@ class NullStdoutMixin(object):
 
 
 class EmulateTerminalMixin(object):
+    """
+    Some languages may insist on providing certain functionality (e.g. colored highlighting of errors) if they feel
+    they are connected to a terminal. Some are more persistent than others in enforcing this, so this mixin aims
+    to provide a convincing-enough lie to the runtime so that it starts singing in color.
+    """
     if os.name != 'nt':
-        def get_compile_popen_kwargs(self):
-            return {'stderr': self._slave, 'stdout': self._slave, 'stdin': self._slave}
-
-        def get_compile_env(self):
-            env = super(EmulateTerminalMixin, self).get_compile_env() or os.environ.copy()
-            env['TERM'] = 'xterm'
-            return env
-
         def get_compile_process(self):
-            self._master, self._slave = pty.openpty()
             proc = super(EmulateTerminalMixin, self).get_compile_process()
 
             class io_error_wrapper(object):
+                """
+                Wrap pty-related IO errors so that we don't crash Popen.communicate()
+                """
+
                 def __init__(self, fd):
                     self.fd = fd
 
@@ -45,7 +45,24 @@ class EmulateTerminalMixin(object):
                 def __getattr__(self, attr):
                     return getattr(self.fd, attr)
 
+            self._master, self._slave = pty.openpty()
+            # Since stderr and stdout are connected to the same slave pty, proc.stderr will contain the merged stdout
+            # of the process as well.
             proc.stderr = io_error_wrapper(os.fdopen(self._master, 'r'))
 
             os.close(self._slave)
             return proc
+
+        def get_compile_popen_kwargs(self):
+            """
+            Emulate the streams of a process connected to a terminal: stdin, stdout, and stderr are all ptys.
+            """
+            return {'stdin': self._slave, 'stdout': self._slave, 'stderr': self._slave}
+
+        def get_compile_env(self):
+            """
+            Some runtimes *cough cough* Swift *cough cough* actually check the environment variables too.
+            """
+            env = super(EmulateTerminalMixin, self).get_compile_env() or os.environ.copy()
+            env['TERM'] = 'xterm'
+            return env

9d13d9496cd319ffbc6404aa16a0393a78d43317
Tudor <tbrindus@gmail.com>
2016-07-05 00:07:07 -0400

    Give TCL all the threads it wants
    Fix `Tcl_InitNotifier: unable to start notifier thread`

--- dmoj/executors/TCL.py
+++ dmoj/executors/TCL.py
@@ -4,6 +4,7 @@ from dmoj.executors.base_executor import ScriptExecutor
 class Executor(ScriptExecutor):
     ext = '.tcl'
     name = 'TCL'
+    nproc = -1  # TCL uses a bunch of threads internally
     address_grace = 131072
     command = 'tclsh'
     syscalls = ['connect', 'access', 'getsockname',

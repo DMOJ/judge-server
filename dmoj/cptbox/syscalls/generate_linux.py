ea89d2f876846b6df5040678271301d82205423e
Quantum <quantum2048@gmail.com>
2016-07-07 00:36:52 -0400

    Attempt to add new syscalls.py; #189

---
+++ dmoj/cptbox/syscalls/generate_linux.py
@@ -0,0 +1,49 @@
+import re
+from urllib2 import urlopen
+from contextlib import closing
+
+from syscall_tables import *
+
+func_to_name = {}
+
+with open('linux-x86.tbl', 'w') as x86, closing(urlopen(LINUX_SYSCALLS_32)) as data:
+    for line in data:
+        if line.startswith('#') or line.isspace():
+            continue
+        syscall = line.split()
+        name = syscall[2].strip('_')
+        if len(syscall) > 3:
+            func = syscall[3]
+            func_to_name[func] = name
+        print>>x86, '%d\t%s' % (int(syscall[0]), name)
+
+with open('linux-x64.tbl', 'w') as x64, open('linux-x32.tbl', 'w') as x32, closing(urlopen(LINUX_SYSCALLS_64)) as data:
+    for line in data:
+        if line.startswith('#') or line.isspace():
+            continue
+        syscall = line.split()
+        id = int(syscall[0])
+        arch = syscall[1]
+        name = syscall[2].strip('_')
+        if arch in ('common', '64'):
+            print>>x64, '%d\t%s' % (id, name)
+        if arch in ('common', 'x32'):
+            print>>x32, '%d\t%s' % (id, name)
+
+rewas = re.compile(r'/\* was (sys_[a-z0-9_]+) \*/')
+with open('linux-arm.tbl', 'w') as arm, closing(urlopen(LINUX_SYSCALLS_ARM)) as data:
+    id = 0
+    for line in data:
+        if 'CALL' in line:
+            match = rewas.search(line)
+            if match is None:
+                func = line[line.rfind('(')+1:line.find(',') if ',' in line else line.find(')')]
+            else:
+                func = match.group(1)
+            if func in func_to_name:
+                name = func_to_name[func]
+            else:
+                name = func.replace('sys_', '').replace('_wrapper', '')
+            if name != 'ni_syscall':
+                print>>arm, '%d\t%s' % (id, name)
+            id += 1

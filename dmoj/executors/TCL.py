a6677622a067fa6581f7f402a0c97631d77eaf2c
Tudor <tbrindus@gmail.com>
2016-07-08 00:54:58 -0400

    Deny write access for fd > 3; #192

--- dmoj/executors/TCL.py
+++ dmoj/executors/TCL.py
@@ -1,4 +1,12 @@
 from dmoj.executors.base_executor import ScriptExecutor
+from dmoj.cptbox.handlers import ACCESS_DENIED
+
+
+def do_write(debugger):
+    if debugger.arg0 <= 2:
+        return True
+    # TCL doesn't seem to care if anything past 2 fails
+    return ACCESS_DENIED(debugger)
 
 
 class Executor(ScriptExecutor):
@@ -9,7 +17,7 @@ class Executor(ScriptExecutor):
     command = 'tclsh'
     syscalls = ['connect', 'access', 'getsockname', 'select',
                 # TCL uses some handles internally
-                ('write', lambda debugger: debugger.arg0 <= 7)]
+                ('write', do_write)]
     fs = ['.*\.tcl', '/etc/nsswitch\.conf$', '/etc/passwd$']
     test_program = '''\
 gets stdin input

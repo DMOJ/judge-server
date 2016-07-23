4ca70e7d8e5504aaa038e9ebfeb8df91f40b8002
Quantum <quantum2048@gmail.com>
2016-07-23 15:42:08 -0400

    Make #203 compilable on Debian GNU/kFreeBSD.

--- dmoj/cptbox/ext_freebsd.h
+++ dmoj/cptbox/ext_freebsd.h
@@ -5,6 +5,11 @@ inline long ptrace_traceme() {
     return ptrace(PT_TRACE_ME, 0, NULL, 0);
 }
 
+// Debian GNU/kFreeBSD neglected to define this in their libc.
+#if defined(__FreeBSD_kernel__) && !defined(PT_FOLLOW_FORK)
+#   define PT_FOLLOW_FORK 23
+#endif
+
 // Constant for wait4
 #define __WALL 0
 

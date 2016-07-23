5a5d9460bfb36abb9dca325837f28222bee6a0e1
Tudor Brindus <tbrindus@gmail.com>
2016-07-23 03:13:32 -0400

    Include runtime when kompiling Kotlin; #201

--- dmoj/executors/KOTLIN.py
+++ dmoj/executors/KOTLIN.py
@@ -4,7 +4,7 @@ import subprocess
 
 from dmoj.executors.java_executor import JavaExecutor
 
-with open(os.path.join(os.path.dirname(__file__), 'kotlin-security.policy')) as policy_file:
+with open(os.path.join(os.path.dirname(__file__), 'java-security.policy')) as policy_file:
     policy = policy_file.read()
 
 
@@ -24,14 +24,12 @@ fun main(args: Array<String>) {
 
     def create_files(self, problem_id, source_code, *args, **kwargs):
         super(Executor, self).create_files(problem_id, source_code, *args, **kwargs)
-        self._class_name = '%s%s%sKt' % (['', '_'][problem_id[0] in '0123456789'], problem_id[0].upper(), problem_id[1:])
+        self._jar_name = '%s.jar' % problem_id
 
     def get_cmdline(self):
         res = super(Executor, self).get_cmdline()
-
-        res[-2:-1] = ['-Dsubmission.file=%s' % self._class_name, '-Dsubmission.dir=%s' % os.path.dirname(self._code)] + self.runtime_dict.get('kotlin_args')
-
+        res[-2:] = ['-jar', self._jar_name]
         return res
 
     def get_compile_args(self):
-        return [self.get_compiler(), self._code]
+        return [self.get_compiler(), '-include-runtime', '-d', self._jar_name, self._code]

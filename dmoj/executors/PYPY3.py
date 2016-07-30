4429034669e3c4fd8ec843ee9667e9e30b088de2
Quantum <quantum2048@gmail.com>
2016-07-30 01:45:29 -0400

    What the hell? @Xyene

--- dmoj/executors/PYPY3.py
+++ dmoj/executors/PYPY3.py
@@ -1,8 +1,8 @@
 from dmoj.cptbox.handlers import ACCESS_DENIED
-from dmoj.executors import PYPY
+from dmoj.executors.PYPY import Executor as PYPYExecutor
 
 
-class Executor(PYPY):
+class Executor(PYPYExecutor):
     command = 'pypy3'
     test_program = "print(__import__('sys').stdin.read(), end='')"
     name = 'PYPY3'

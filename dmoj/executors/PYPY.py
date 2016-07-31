bf39dee5cd5d93d2d5dfc9af09d350bbd57f303d
Tudor <tbrindus@gmail.com>
2016-07-31 16:28:33 -0400

    Report implemented Python version for PyPy; #197

--- dmoj/executors/PYPY.py
+++ dmoj/executors/PYPY.py
@@ -15,6 +15,16 @@ class Executor(PythonExecutor):
     @classmethod
     def parse_version(cls, command, output):
         try:
-            return map(int, reversion.findall(output)[1].split('.'))  # Prints implemented Python version first
+            cls._pypy_versions = [tuple(map(int, version.split('.'))) for version in reversion.findall(output)]
+            return cls._pypy_versions[1]
         except:
             return None
+
+    @classmethod
+    def get_runtime_versions(cls):
+        if cls._command_versions:
+            return cls._command_versions
+        # A little hack to report implemented Python version too
+        cls._command_versions = tuple(list(super(Executor, cls).get_runtime_versions()) +
+                                      [('implementing python', cls._pypy_versions[0])])
+        return cls._command_versions

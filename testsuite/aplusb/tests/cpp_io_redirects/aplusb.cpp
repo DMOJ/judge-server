a3d1c85651cdfdba8dbcf51f0de802b0296a8cfa
Tudor <tbrindus@gmail.com>
2016-08-13 12:48:39 -0400

    Add C++ IO redirect test

---
+++ testsuite/aplusb/tests/cpp_io_redirects/aplusb.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    freopen("DATA.out", "w", stdout);
+    freopen("DATA.in", "r", stdin);
+
+    int N;
+    scanf("%d", &N);
+
+    while(N--) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a + b);
+    }
+    return 0;
+}

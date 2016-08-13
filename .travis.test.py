4ca047738e039f756a0f9a477f1971dde5d9021e
Tudor <tbrindus@gmail.com>
2016-08-13 19:08:55 -0400

    Run Groovy on Travis; #208

--- .travis.test.py
+++ .travis.test.py
@@ -19,7 +19,7 @@ def find_directory(parent, expr):
             return os.path.join(parent, dir)
 
 TEST_ON_TRAVIS = ['ADA', 'AWK', 'BF', 'C', 'CBL', 'D', 'CPP03', 'CPP11', 'CLANG', 'CLANGX',
-                  'COFFEE', 'GO', 'HASK', 'JAVA7', 'JAVA8', 'JAVA9', 'MONOCS',
+                  'COFFEE', 'GO', 'GROOVY', 'HASK', 'JAVA7', 'JAVA8', 'JAVA9', 'MONOCS',
                   'MONOFS', 'MONOVB', 'PAS', 'GAS32', 'GAS64', 'NASM', 'NASM64',
                   'PERL', 'PHP', 'PY2', 'PY3', 'PYPY', 'PYPY3',
                   'RUBY19', 'RUBY21', 'RUST', 'SCALA', 'SWIFT', 'TEXT']

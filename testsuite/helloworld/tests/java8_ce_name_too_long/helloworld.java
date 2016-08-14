d389af6db4a976ade30451382b69f1c461bd24da
Tudor <tbrindus@gmail.com>
2016-08-13 20:17:09 -0400

    Add test for Java classname too long

---
+++ testsuite/helloworld/tests/java8_ce_name_too_long/helloworld.java
@@ -0,0 +1,5 @@
+public class aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa {
+    public static void main(String[] argv) {
+        throw new RuntimeException();
+    }
+}

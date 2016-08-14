7060b113aa1f4e5ee12d1844817a133c4c39cb6b
Tudor Brindus <tbrindus@gmail.com>
2016-08-14 17:32:05 -0400

    Make Java sandbox batch build script well-behaved

--- build_java_executor.bat
+++ build_java_executor.bat
@@ -1,9 +1,12 @@
+@echo off
+pushd %cd%
 cd %~dp0java_sandbox
-del /s /q objs
-rmdir objs
+del /f /s /q objs > nul
+rmdir /s /q objs
 md objs
 cd src
 javac -source 1.7 -target 1.7 -d ..\objs ca\dmoj\java\*.java
 cd ..\objs
 del ..\..\dmoj\executors\java-sandbox.jar
 jar cmf ..\src\META-INF\MANIFEST.MF ..\..\dmoj\executors\java-sandbox.jar ca\dmoj\java\*.class
+popd

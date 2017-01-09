526c6f1bb2d12504be53909276466c3bd09f527c
BinAccel <BinAccel@users.noreply.github.com>
2017-01-09 09:40:20 -0500

    I knew I'd fuck up (#11)
    I knew I'd mess up

--- testsuite/helloworld/tests/java_final_strictfp_class/final_strictfp_class.java
+++ testsuite/helloworld/tests/java_final_strictfp_class/final_strictfp_class.java
@@ -1,5 +1,5 @@
 public final strictfp class final_strictfp_class {
   public static void main(String[] args) {
-    System.out.println("Hello, World");
+    System.out.println("Hello, World!");
   }
 }

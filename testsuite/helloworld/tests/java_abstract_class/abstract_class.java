5862c2bf210378a23c3b02bd0d06ddd3f2d9f94a
BinAccel <BinAccel@users.noreply.github.com>
2017-01-09 09:35:04 -0500

    Java Class Test (#9)
    * Begin creating tests for the new class regex
    
    * I forgot that we need folders.
    
    * Create abstract_class.java
    
    * Create strictfp_class.java
    
    * Create final_strictfp_class.java
    
    * Better folder name
    
    * Create strictfp_abstract_class.java
    
    * Create test.yml
    
    * Create test.yml
    
    * Create test.yml
    
    * Create test.yml
    
    * Create test.yml
    
    * It's all Victor's fault
    
    * I blame you Victor
    
    * Update test.yml
    
    * Typo

---
+++ testsuite/helloworld/tests/java_abstract_class/abstract_class.java
@@ -0,0 +1,5 @@
+public abstract class abstract_class {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
---
+++ testsuite/helloworld/tests/java_final_class/final_class.java
@@ -0,0 +1,5 @@
+public final class final_class {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}

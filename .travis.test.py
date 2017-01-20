01ef5b6eb9fa86c20bef349ec977cffcd6bef9b8
BinAccel <BinAccel@users.noreply.github.com>
2017-01-20 17:31:14 -0500

    Py3

--- .travis.test.py
+++ .travis.test.py
@@ -10,6 +10,7 @@ from dmoj.executors import executors
 from dmoj.testsuite import Tester
 from dmoj.utils.ansi import ansi_style
 
+from __future__ import print_function
 
 def find_directory(parent, expr):
     regex = re.compile(expr)
@@ -53,45 +54,45 @@ def main():
 
     failed = False
 
-    print 'Available JVMs:'
+    print('Available JVMs:')
     for jvm in get_dirs(JVM_DIR):
-        print '  -', jvm
-    print
+        print('  -', jvm)
+    print()
 
-    print 'Available Pythons:'
+    print('Available Pythons:')
     for python in get_dirs(PYENV_DIR):
-        print '  -', python
+        print('  -', python)
     print
 
-    print 'Available Rubies:'
+    print('Available Rubies:')
     for ruby in get_dirs(RVM_DIR):
-        print '  -', ruby
-    print
+        print('  -', ruby)
+    print()
 
-    print 'Using extra allowed filesystems:'
+    print('Using extra allowed filesystems:')
     for lang, fs in judgeenv.env['extra_fs'].iteritems():
-        print '%-6s: %s' % (lang, '|'.join(fs))
-    print
+        print('%-6s: %s' % (lang, '|'.join(fs)))
+    print()
 
-    print 'Testing executors...'
+    print('Testing executors...')
 
     for name in TEST_ON_TRAVIS:
         executor = import_module('dmoj.executors.' + name)
 
-        print ansi_style('%-34s%s' % ('Testing #ansi[%s](|underline):' % name, '')),
+        print(ansi_style('%-34s%s' % ('Testing #ansi[%s](|underline):' % name, '')), end='')
 
         if not hasattr(executor, 'Executor'):
             failed = True
-            print ansi_style('#ansi[Does not export](red|bold) #ansi[Executor](red|underline)')
+            print(ansi_style('#ansi[Does not export](red|bold) #ansi[Executor](red|underline)'))
             continue
 
         if not hasattr(executor.Executor, 'autoconfig'):
-            print ansi_style('#ansi[Could not autoconfig](magenta|bold)')
+            print(ansi_style('#ansi[Could not autoconfig](magenta|bold)'))
             continue
 
         try:
             if name in OVERRIDES:
-                print ansi_style('#ansi[(manual config)](yellow)'),
+                print(ansi_style('#ansi[(manual config)](yellow)'), end='')
                 data = executor.Executor.autoconfig_run_test(OVERRIDES[name])
             else:
                 data = executor.Executor.autoconfig()
@@ -101,43 +102,43 @@ def main():
             errors = '' if len(data) < 4 else data[3]
         except Exception:
             failed = True
-            print ansi_style('#ansi[Autoconfig broken](red|bold)')
+            print(ansi_style('#ansi[Autoconfig broken](red|bold)'))
             traceback.print_exc()
         else:
-            print ansi_style(['#ansi[%s](red|bold)', '#ansi[%s](green|bold)'][success] %
-                             (feedback or ['Failed', 'Success'][success]))
+            print(ansi_style(['#ansi[%s](red|bold)', '#ansi[%s](green|bold)'][success] %
+                             (feedback or ['Failed', 'Success'][success])))
 
             if success:
                 result.update(config)
                 executor.Executor.runtime_dict = config
                 executors[name] = executor
                 for runtime, ver in executor.Executor.get_runtime_versions():
-                    print ansi_style('  #ansi[%s](cyan): %s' % (runtime, '.'.join(map(str, ver)) if ver else 'unknown'))
+                    print(ansi_style('  #ansi[%s](cyan): %s' % (runtime, '.'.join(map(str, ver)) if ver else 'unknown')))
             else:
                 if config:
-                    print '  Attempted:'
-                    print '   ', yaml.dump(config, default_flow_style=False).rstrip().replace('\n', '\n' + ' ' * 4)
+                    print('  Attempted:')
+                    print('   ', yaml.dump(config, default_flow_style=False).rstrip().replace('\n', '\n' + ' ' * 4))
 
                 if errors:
-                    print '  Errors:'
-                    print '   ', errors.replace('\n', '\n' + ' ' * 4)
+                    print('  Errors:')
+                    print('   ', errors.replace('\n', '\n' + ' ' * 4))
                 failed = True
 
-    print
-    print ansi_style('#ansi[Configuration result](green|bold|underline):')
-    print yaml.dump({'runtime': result}, default_flow_style=False).rstrip()
-    print
-    print
-    print 'Running test cases...'
+    print()
+    print(ansi_style('#ansi[Configuration result](green|bold|underline):'))
+    print(yaml.dump({'runtime': result}, default_flow_style=False).rstrip())
+    print()
+    print()
+    print('Running test cases...')
     judgeenv.problem_dirs = [os.path.join(os.path.dirname(__file__), 'testsuite')]
     tester = Tester()
     fails = tester.test_all()
-    print
-    print 'Test complete'
+    print()
+    print('Test complete')
     if fails:
-        print ansi_style('#ansi[A total of %d case(s) failed](red|bold).') % fails
+        print(ansi_style('#ansi[A total of %d case(s) failed](red|bold).') % fails)
     else:
-        print ansi_style('#ansi[All cases passed.](green|bold)')
+        print(ansi_style('#ansi[All cases passed.](green|bold)'))
     failed |= fails != 0
     raise SystemExit(int(failed))
 

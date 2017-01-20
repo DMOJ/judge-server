5138b44f398fe6e3827dd637a9d4bb09f8aec080
BinAccel <BinAccel@users.noreply.github.com>
2017-01-20 17:17:52 -0500

    Let Tudor be happier

--- dmoj/cli.py
+++ dmoj/cli.py
@@ -12,9 +12,9 @@ from dmoj.judge import Judge
 from dmoj.utils.ansi import ansi_style
 from __future__ import print_function
 
-if sys.version_info.major == 2:
-    input = raw_input
-    range = xrange
+if sys.version_info.major == 3:
+    raw_input = input
+    xrange = range
 
 class LocalPacketManager(object):
     def __init__(self, judge):
@@ -135,7 +135,7 @@ class ListProblemsCommand(Command):
             problems = iter(map(itemgetter(0), all_problems))
             max_len = max(len(p[0]) for p in all_problems)
             for row in izip_longest(*[problems] * 4, fillvalue=''):
-                print(' '.join(('%*s' % (-max_len, row[i])) for i in range(4)))
+                print(' '.join(('%*s' % (-max_len, row[i])) for i in xrange(4)))
         else:
             print(ansi_style("#ansi[No problems matching filter found.](red|bold)"))
         print

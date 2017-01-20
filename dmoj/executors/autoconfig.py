2da8f28f39318c384264f5f1370d67ac611c302e
BinAccel <BinAccel@users.noreply.github.com>
2017-01-20 17:34:11 -0500

    Py3

--- dmoj/executors/autoconfig.py
+++ dmoj/executors/autoconfig.py
@@ -8,6 +8,7 @@ from dmoj import judgeenv
 from dmoj.executors import get_available, load_executor
 from dmoj.utils.ansi import ansi_style
 
+from __future__ import print_function
 
 def main():
     result = {}
@@ -30,7 +31,7 @@ def main():
             continue
 
         if hasattr(executor.Executor, 'autoconfig'):
-            print ansi_style('%-43s%s' % ('Auto-configuring #ansi[%s](|underline):' % name, '')),
+            print(ansi_style('%-43s%s' % ('Auto-configuring #ansi[%s](|underline):' % name, '')), end='')
             try:
                 data = executor.Executor.autoconfig()
                 config = data[0]
@@ -38,27 +39,27 @@ def main():
                 feedback = data[2]
                 errors = '' if len(data) < 4 else data[3]
             except Exception:
-                print ansi_style('#ansi[Not supported](red|bold)')
+                print(ansi_style('#ansi[Not supported](red|bold)'))
                 traceback.print_exc()
             else:
-                print ansi_style(['#ansi[%s](red|bold)', '#ansi[%s](green|bold)'][success] %
-                                 (feedback or ['Failed', 'Success'][success]))
+                print(ansi_style(['#ansi[%s](red|bold)', '#ansi[%s](green|bold)'][success] %
+                                 (feedback or ['Failed', 'Success'][success])))
 
                 if not success:
                     if config:
-                        print '  Attempted:'
-                        print '   ', yaml.dump(config, default_flow_style=False).rstrip().replace('\n', '\n' + ' ' * 4)
+                        print('  Attempted:')
+                        print('   ', yaml.dump(config, default_flow_style=False).rstrip().replace('\n', '\n' + ' ' * 4))
 
                     if errors:
-                        print '  Errors:'
-                        print '   ', errors.replace('\n', '\n' + ' ' * 4)
+                        print('  Errors:')
+                        print('   ', errors.replace('\n', '\n' + ' ' * 4))
 
                 if success:
                     result.update(config)
 
-    print
-    print ansi_style('#ansi[Configuration result](green|bold|underline):')
-    print yaml.dump({'runtime': result}, default_flow_style=False).rstrip()
+    print()
+    print(ansi_style('#ansi[Configuration result](green|bold|underline):'))
+    print(yaml.dump({'runtime': result}, default_flow_style=False).rstrip())
 
 if __name__ == '__main__':
     main()

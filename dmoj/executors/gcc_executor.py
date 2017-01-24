967348728ff15ab311196303c9941ba39de47f2d
BinAccel <BinAccel@users.noreply.github.com>
2017-01-24 13:44:07 -0500

    Um...?

--- dmoj/executors/gcc_executor.py
+++ dmoj/executors/gcc_executor.py
@@ -13,7 +13,7 @@ IS_ARM = hasattr(os, 'uname') and os.uname()[4].startswith('arm')
 if os.name == 'nt':
     GCC_COMPILE.update((k.encode('mbcs'), v.encode('mbcs')) for k, v in
                        (env.runtime.gcc_compile or GCC_ENV).iteritems())
-    GCC_ENV = dict((k.encode('mbcs'), v.encode('mbcs')) for k, v in GCC_ENV.items())
+    GCC_ENV = dict((k.encode('mbcs'), v.encode('mbcs')) for k, v in GCC_ENV.iteritems())
 else:
     GCC_COMPILE.update(env.runtime.gcc_compile or {})
 

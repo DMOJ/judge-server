14cd3e0c94cb349893a6beb59d5ca9faffda804e
BinAccel <BinAccel@users.noreply.github.com>
2017-02-04 16:12:28 -0500

    Allow for silent mode
    `-s` or `--silent`

--- dmoj/executors/autoconfig.py
+++ dmoj/executors/autoconfig.py
@@ -4,12 +4,14 @@ import traceback
 import yaml
 import yaml.representer
 
+import argparse
+
 from dmoj import judgeenv
 from dmoj.executors import get_available, load_executor
 from dmoj.utils.ansi import ansi_style
 
 
-def main():
+def main(silent=False):
     result = {}
 
     if os.name == 'nt':
@@ -30,7 +32,8 @@ def main():
             continue
 
         if hasattr(executor.Executor, 'autoconfig'):
-            print ansi_style('%-43s%s' % ('Auto-configuring #ansi[%s](|underline):' % name, '')),
+            if not silent:
+                print ansi_style('%-43s%s' % ('Auto-configuring #ansi[%s](|underline):' % name, '')),
             try:
                 data = executor.Executor.autoconfig()
                 config = data[0]
@@ -38,27 +41,34 @@ def main():
                 feedback = data[2]
                 errors = '' if len(data) < 4 else data[3]
             except Exception:
-                print ansi_style('#ansi[Not supported](red|bold)')
-                traceback.print_exc()
+                if not silent:
+                    print ansi_style('#ansi[Not supported](red|bold)')
+                    traceback.print_exc()
             else:
-                print ansi_style(['#ansi[%s](red|bold)', '#ansi[%s](green|bold)'][success] %
+                if not silent:
+                    print ansi_style(['#ansi[%s](red|bold)', '#ansi[%s](green|bold)'][success] %
                                  (feedback or ['Failed', 'Success'][success]))
 
                 if not success:
-                    if config:
-                        print '  Attempted:'
-                        print '   ', yaml.dump(config, default_flow_style=False).rstrip().replace('\n', '\n' + ' ' * 4)
+                    if not silent:
+                        if config:
+                            print '  Attempted:'
+                            print '   ', yaml.dump(config, default_flow_style=False).rstrip().replace('\n', '\n' + ' ' * 4)
 
-                    if errors:
-                        print '  Errors:'
-                        print '   ', errors.replace('\n', '\n' + ' ' * 4)
+                        if errors:
+                            print '  Errors:'
+                            print '   ', errors.replace('\n', '\n' + ' ' * 4)
 
                 if success:
                     result.update(config)
 
-    print
-    print ansi_style('#ansi[Configuration result](green|bold|underline):')
+    if not silent:
+        print
+        print ansi_style('#ansi[Configuration result](green|bold|underline):')
     print yaml.dump({'runtime': result}, default_flow_style=False).rstrip()
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Automatically configures runtimes')
+    parser.add_argument('-s', '--silent', nargs='?', help='silent mode', dest='silent', const=True, default=False)
+    args = parser.parse_args()
+    main(bool(args.silent))

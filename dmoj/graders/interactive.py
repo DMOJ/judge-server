91cad4e1558ebe8569befc2a9f05114e8acc6d04
BinAccel <BinAccel@users.noreply.github.com>
2017-03-10 19:15:03 -0500

    Fixes

--- dmoj/graders/interactive.py
+++ dmoj/graders/interactive.py
@@ -3,38 +3,39 @@ from dmoj.graders.standard import StandardGrader
 class Interactor(object):
     def __init__(self, process):
         self.process = process
+        self.closed = False
 
     def read(self):
         return self.process.stdout.read()
 
-    def readline(self):
+    def readln(self):
         return self.process.stdout.readline()
 
     def write(self, str):
         self.process.stdin.write(str)
         self.process.stdin.flush()
-        
+
     def writeln(self, str):
         write(str + '\n')
-        
+
     def close(self):
-        for stream in [self.process.stdin, self.process.stdout, self.process.stderr]:
-            stream.close()
+        if not closed:
+            for stream in [self.process.stdin, self.process.stdout, self.process.stderr]:
+                stream.close()
+            closed = True
 
 class InteractiveGrader(StandardGrader):
     def _interact_with_process(self, case, result, input):
         interactor = Interactor(self._current_proc)
+        self.check = False
         try:
             self.check = self.interact(interactor)
             interactor.close()
-        except NotImplementedError as e:
-            # it is not the user's fault that the problemsetter was incompetent
-            raise e
         except:
-            self.check = False
+            pass
 
-        self.process.wait()
-        
-    def interact():
-        raise NotImplementedError
-        
+        self._current_proc.wait()
+
+    def check_result(self, case, result):
+        return self.check
+

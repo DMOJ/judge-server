7bb81b1c1aa6d41f5c1adcc848c29ba26191db8a
BinAccel <BinAccel@users.noreply.github.com>
2017-03-13 00:43:14 -0400

    Add new interactive grader base (#244)
    * Create Interactive Grader
    
    Git CLI is being a jerk
    
    * Add Interactive Grader to __init__
    
    * Spelling is a pain
    
    * Fixes
    
    * self.fix_errors
    
    * Misc. Fixes
    
    * More fixes
    
    * Error-based fixes
    
    * Closed intervals and optional newline strip
    
    is `strip_newline` the best name?
    
    * Dupe code is bad code
    
    * Remove Interactor from __init__
    
    * Just eat IOError
    
    * Should've been a tuple
    
    * Fixes; DMOJ/judge-testsuite#16
    
    * Whoops; DMOJ/judge-testsuite#16

--- dmoj/graders/__init__.py
+++ dmoj/graders/__init__.py
@@ -1,3 +1,4 @@
 from dmoj.graders.standard import StandardGrader
 from dmoj.graders.signature import SignatureGrader
 from dmoj.graders.custom import CustomGrader
+from dmoj.graders.interactive import InteractiveGrader
---
+++ dmoj/graders/interactive.py
@@ -0,0 +1,75 @@
+from dmoj.graders.standard import StandardGrader
+
+class WrongAnswer(BaseException):
+    pass
+
+class Interactor(object):
+    def __init__(self, process):
+        self.process = process
+        self._tokens = None
+
+    def read(self):
+        return self.process.stdout.read()
+
+    def readln(self, strip_newline=True):
+        ret = self.process.stdout.readline()
+        if strip_newline:
+            return ret.rstrip()
+        return ret
+
+    def readtoken(self, delim=None):
+        if not self._tokens:
+            self._tokens = self.readln()
+        try:
+            ret, self._tokens = self._tokens.split(delim, 1)
+        except ValueError:
+            ret = self._tokens
+            self._tokens = None
+        return ret
+
+    def readint(self, lo=float('-inf'), hi=float('inf'), delim=None):
+        try:
+            ret = int(self.readtoken(delim))
+        except ValueError:
+            raise WrongAnswer
+        if not lo <= ret <= hi:
+            raise WrongAnswer
+        return ret
+
+    def readfloat(self, lo=float('-inf'), hi=float('inf'), delim=None):
+        try:
+            ret = float(self.readtoken(delim))
+        except ValueError:
+            raise WrongAnswer
+        if not lo <= ret <= hi:
+            raise WrongAnswer
+        return ret
+
+    def write(self, val):
+        self.process.stdin.write(str(val))
+        self.process.stdin.flush()
+
+    def writeln(self, val):
+        self.write(str(val) + '\n')
+
+    def close(self):
+        for stream in [self.process.stdin, self.process.stdout, self.process.stderr]:
+            stream.close()
+
+class InteractiveGrader(StandardGrader):
+    def _interact_with_process(self, case, result, input):
+        interactor = Interactor(self._current_proc)
+        self.check = False
+        try:
+            self.check = self.interact(case, interactor)
+            interactor.close()
+        except (WrongAnswer, IOError):
+            pass
+
+        self._current_proc.wait()
+
+    def check_result(self, case, result):
+        return self.check
+
+    def interact(self, case, interactor):
+        raise NotImplementedError

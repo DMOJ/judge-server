ebc4f68e5c6b1ac4aba3e3e6d44b3be54d72b795
BinAccel <BinAccel@users.noreply.github.com>
2017-03-12 22:20:58 -0400

    Remove Interactor from __init__

--- dmoj/graders/__init__.py
+++ dmoj/graders/__init__.py
@@ -1,4 +1,4 @@
 from dmoj.graders.standard import StandardGrader
 from dmoj.graders.signature import SignatureGrader
 from dmoj.graders.custom import CustomGrader
-from dmoj.graders.interactive import InteractiveGrader, Interactor
+from dmoj.graders.interactive import InteractiveGrader

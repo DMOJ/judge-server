e34a9d640bfb341368e8dffa2801f8a8551e5b4c
BinAccel <BinAccel@users.noreply.github.com>
2017-03-12 21:51:28 -0400

    Misc. Fixes

--- dmoj/graders/interactive.py
+++ dmoj/graders/interactive.py
@@ -1,9 +1,13 @@
 from dmoj.graders.standard import StandardGrader
 
+class WrongAnswer(BaseException):
+    pass
+
 class Interactor(object):
     def __init__(self, process):
         self.process = process
         self._is_closed = False
+        self._tokens = None
 
     def read(self):
         return self.process.stdout.read()
@@ -11,6 +15,33 @@ class Interactor(object):
     def readln(self):
         return self.process.stdout.readline()
 
+    def readtoken(self, str=None):
+        if not self._tokens:
+            self._tokens = self.readln()
+        try:
+            ret, self._tokens = self._tokens.split(str, 1)
+        except ValueError:
+            ret = self._tokens
+            self._tokens = None
+        return ret
+
+    def readint(self, lo=float('-inf'), hi=float('inf'), str=None):
+        try:
+            ret = int(self.readtoken(str))
+        except ValueError:
+            raise WrongAnswer
+        if lo > ret or ret > hi:
+            raise WrongAnswer
+        return ret
+
+    def readfloat(self, lo=float('-inf'), hi=float('inf'), str=None):
+        try:
+            ret = float(self.readtoken(str))
+        except ValueError:
+            raise WrongAnswer
+        if lo > ret or ret > hi:
+            raise WrongAnswer
+        return ret
     def write(self, str):
         self.process.stdin.write(str)
         self.process.stdin.flush()
@@ -31,7 +62,7 @@ class InteractiveGrader(StandardGrader):
         try:
             self.check = self.interact(interactor)
             interactor.close()
-        except:
+        except WrongAnswer:
             pass
 
         self._current_proc.wait()
@@ -39,3 +70,5 @@ class InteractiveGrader(StandardGrader):
     def check_result(self, case, result):
         return self.check
 
+    def interact(self, interactor):
+        raise NotImplementedError

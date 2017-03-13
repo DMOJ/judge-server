27ed8085d957e15859608c4d23cdd3d851edf7bf
BinAccel <BinAccel@users.noreply.github.com>
2017-03-12 22:07:19 -0400

    Error-based fixes

--- dmoj/graders/interactive.py
+++ dmoj/graders/interactive.py
@@ -6,14 +6,13 @@ class WrongAnswer(BaseException):
 class Interactor(object):
     def __init__(self, process):
         self.process = process
-        self._is_closed = False
         self._tokens = None
 
     def read(self):
         return self.process.stdout.read()
 
     def readln(self):
-        return self.process.stdout.readline()
+        return self.process.stdout.readline().rstrip()
 
     def readtoken(self, delim=None):
         if not self._tokens:
@@ -51,10 +50,11 @@ class Interactor(object):
         self.write(str + '\n')
 
     def close(self):
-        if not self._is_closed:
-            for stream in [self.process.stdin, self.process.stdout, self.process.stderr]:
+        for stream in [self.process.stdin, self.process.stdout, self.process.stderr]:
+            try:
                 stream.close()
-            self._is_closed = True
+            except IOError:
+                pass
 
 class InteractiveGrader(StandardGrader):
     def _interact_with_process(self, case, result, input):

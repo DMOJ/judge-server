241e7c9c9c245d5808689401c25f4e86ad29c2f3
Phoenix <Phoenix1369@users.noreply.github.com>
2017-05-07 14:45:32 -0400

    Add checker which counts correct lines
    Optionally provides additional feedback on which lines were incorrect

---
+++ dmoj/checkers/linecount.py
@@ -0,0 +1,16 @@
+from dmoj.result import CheckerResult
+verdict = "XO"
+
+def check(process_output, judge_output, point_value, feedback=False, **kwargs):
+    process_lines = filter(None, process_output.strip().split("\n"))
+    judge_lines = filter(None, judge_output.strip().split("\n"))
+
+    cases = [verdict[0]] * len(judge_lines)
+    count = 0
+    for i, (process_line, judge_line) in enumerate(zip(process_lines, judge_lines)):
+        if process_line == judge_line:
+            cases[i] = verdict[1]
+            count += 1
+    if not count:
+        return CheckerResult(False, 0)
+    return CheckerResult(True, point_value * (1.0 * count / len(judge_lines)), ''.join(cases) if feedback else "")

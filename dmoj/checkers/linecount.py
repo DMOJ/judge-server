e7e8592eb7fe40291535bb91c6ab5bed998bef39
Phoenix <Phoenix1369@users.noreply.github.com>
2017-05-07 15:10:22 -0400

    Punish users for extraneous output

--- dmoj/checkers/linecount.py
+++ dmoj/checkers/linecount.py
@@ -7,10 +7,14 @@ def check(process_output, judge_output, point_value, feedback=False, **kwargs):
 
     cases = [verdict[0]] * len(judge_lines)
     count = 0
+
     for i, (process_line, judge_line) in enumerate(zip(process_lines, judge_lines)):
         if process_line == judge_line:
             cases[i] = verdict[1]
             count += 1
+
     if not count:
         return CheckerResult(False, 0)
+    if len(process_lines) > len(judge_lines):
+        return CheckerResult(False, 0, 'Incorrect Output Format')
     return CheckerResult(True, point_value * (1.0 * count / len(judge_lines)), ''.join(cases) if feedback else "")

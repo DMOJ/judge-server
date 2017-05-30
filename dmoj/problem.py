cae519a4e8c587818899b675c890fbe2567d2f28
Ripple <ripple-495@outlook.com>
2017-05-30 13:55:02 -0400

    - logging for schema + pygments

--- dmoj/problem.py
+++ dmoj/problem.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import zipfile
+import logging
 from functools import partial
 from pykwalify.core import Core
 
@@ -34,8 +35,10 @@ class Problem(object):
             doc = yaml.safe_load(self.problem_data['init.yml'])
             if not doc:
                 raise InvalidInitException('I find your lack of content disturbing.')
+            logging.disable(logging.CRITICAL)  # disable logging
             init_check = Core(source_data=doc, schema_files=[os.path.join(os.path.dirname(__file__), 'problem_schema.yml')])
             init_check.validate(raise_exception=True)
+            logging.disable(logging.NOTSET)  # enable logging
             self.config = ConfigNode(doc, defaults={
                 'wall_time_factor': 3,
                 'output_prefix_length': 64,

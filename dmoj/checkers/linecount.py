7e03fe5bd480f9287dfe0dff2b72a2a61af7de3c
Phoenix <Phoenix1369@users.noreply.github.com>
2017-06-18 17:41:16 -0400

    Add generalized parameter `match`
    - Fix pretest handling

--- dmoj/checkers/linecount.py
+++ dmoj/checkers/linecount.py
@@ -2,18 +2,25 @@ from dmoj.result import CheckerResult
 
 verdict = u"\u2717\u2713"
 
-def check(process_output, judge_output, point_value, feedback=False, **kwargs):
+def check(process_output, judge_output, point_value,
+        feedback=False, match=lambda x,y: x.strip()==y.strip(), **kwargs):
+
     process_lines = filter(None, process_output.strip().split("\n"))
     judge_lines = filter(None, judge_output.strip().split("\n"))
+
+    if len(process_lines) > len(judge_lines):
+        return CheckerResult(False, 0)
 
     cases = [verdict[0]] * len(judge_lines)
     count = 0
 
     for i, (process_line, judge_line) in enumerate(zip(process_lines, judge_lines)):
-        if process_line == judge_line:
+        if match(process_line, judge_line):
             cases[i] = verdict[1]
             count += 1
 
-    if not count or len(process_lines) > len(judge_lines):
+    if not count:
         return CheckerResult(False, 0)
+    if not point_value and count != len(judge_lines):
+        return CheckerResult(False, 0, ''.join(cases) if feedback else "")
     return CheckerResult(True, point_value * (1.0 * count / len(judge_lines)), ''.join(cases) if feedback else "")

f27321194ebe67c1f595363e28e4553f084fb4b3
Phoenix <Phoenix1369@users.noreply.github.com>
2017-06-25 21:08:04 -0400

    Generalize line comparison of linecount.py (#266)
    * Add generalized parameter `match`
    
    - Fix pretest handling
    
    * Update match parameter names
    
    * Fix spacing of lambda
    
    * Align function parameters
    
    * Actually evaluate the string lambda
    
    - Change verdict from `_AC` to `WA`
    
    * Add default comparison
    
    * Skip string check
    
    * Move empty check up
    
    * Legitimize checker
    
    * Use default arguments and string check
    
    - Instead of previous insanity
    
    * Revert to basestring

--- dmoj/checkers/linecount.py
+++ dmoj/checkers/linecount.py
@@ -2,18 +2,27 @@ from dmoj.result import CheckerResult
 
 verdict = u"\u2717\u2713"
 
-def check(process_output, judge_output, point_value, feedback=False, **kwargs):
+def check(process_output, judge_output, point_value, feedback=False,
+          match=lambda p, j: p.strip() == j.strip(), **kwargs):
+
     process_lines = filter(None, process_output.strip().split("\n"))
     judge_lines = filter(None, judge_output.strip().split("\n"))
+
+    if len(process_lines) > len(judge_lines):
+        return False
+
+    if not judge_lines:
+        return True
+
+    if isinstance(match, basestring):
+        match = eval(match)
 
     cases = [verdict[0]] * len(judge_lines)
     count = 0
 
     for i, (process_line, judge_line) in enumerate(zip(process_lines, judge_lines)):
-        if process_line == judge_line:
+        if match(process_line, judge_line):
             cases[i] = verdict[1]
             count += 1
 
-    if not count or len(process_lines) > len(judge_lines):
-        return CheckerResult(False, 0)
-    return CheckerResult(True, point_value * (1.0 * count / len(judge_lines)), ''.join(cases) if feedback else "")
+    return CheckerResult(count == len(judge_lines), point_value * (1.0 * count / len(judge_lines)), ''.join(cases) if feedback else "")

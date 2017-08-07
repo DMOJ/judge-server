8b8953011e7989e728eef4509f2073e6b1914eab
Quantum <quantum2048@gmail.com>
2017-08-07 19:17:49 -0400

    Failed to test code...

--- dmoj/executors/RUBY2.py
+++ dmoj/executors/RUBY2.py
@@ -5,7 +5,7 @@ from .ruby_executor import RubyExecutor
 
 class Executor(RubyExecutor):
     name = 'RUBY2'
-    command_paths = ['ruby2.%d' for i in reversed(xrange(0, 5))]
+    command_paths = ['ruby2.%d' % i for i in reversed(xrange(0, 5))]
     syscalls = ['pipe2', 'poll', ('write', lambda debugger: debugger.arg0 in (1, 2, 4))]
 
     def get_nproc(self):

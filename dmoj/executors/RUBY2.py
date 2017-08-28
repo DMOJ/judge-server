7241ca5ac890a70f2492222feaba3d6338cec857
Quantum <quantum2048@gmail.com>
2017-08-28 17:50:55 -0400

    Ruby wants /proc/self/loginuid

--- dmoj/executors/RUBY2.py
+++ dmoj/executors/RUBY2.py
@@ -7,6 +7,7 @@ class Executor(RubyExecutor):
     name = 'RUBY2'
     command_paths = ['ruby2.%d' % i for i in reversed(xrange(0, 5))]
     syscalls = ['pipe2', 'poll', ('write', lambda debugger: debugger.arg0 in (1, 2, 4))]
+    fs = ['/proc/self/loginuid$']
 
     def get_nproc(self):
         return [-1, 1][os.name == 'nt']

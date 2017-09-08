cb75af0e9844e703c38850991118439148a78843
Quantum <quantum2048@gmail.com>
2017-09-08 19:29:53 -0400

    Python 3.6 is now a thing.

--- dmoj/executors/PY3.py
+++ dmoj/executors/PY3.py
@@ -3,6 +3,6 @@ from dmoj.executors.python_executor import PythonExecutor
 
 class Executor(PythonExecutor):
     command = 'python3'
-    command_paths = ['python%s' % i for i in ['3.5', '3.4', '3.3', '3.2', '3.1', '3']]
+    command_paths = ['python%s' % i for i in ['3.6', '3.5', '3.4', '3.3', '3.2', '3.1', '3']]
     test_program = "print(__import__('sys').stdin.read(), end='')"
     name = 'PY3'

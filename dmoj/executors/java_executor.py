b361312480ec94dd0c8f0d31ebccdbf611223aa9
Tudor Brindus <tbrindus@gmail.com>
2017-10-06 23:54:34 -0400

    "Dumping more address grace shall surely fix this"

--- dmoj/executors/java_executor.py
+++ dmoj/executors/java_executor.py
@@ -49,7 +49,7 @@ class JavaExecutor(CompiledExecutor):
     vm = None
     compiler = None
     nproc = -1
-    address_grace = 262144
+    address_grace = 786432
 
     jvm_regex = None
     security_policy = policy

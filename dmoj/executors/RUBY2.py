c4810ed89dcb851489c4a13f421e3688074a6ce9
Quantum <quantum2048@gmail.com>
2017-11-15 14:21:18 -0500

    FreeBSD-specific calls for Ruby...

--- dmoj/executors/RUBY2.py
+++ dmoj/executors/RUBY2.py
@@ -7,7 +7,7 @@ class Executor(RubyExecutor):
     name = 'RUBY2'
     command_paths = (['ruby2.%d' % i for i in reversed(xrange(0, 5))] +
                      ['ruby2%d' % i for i in reversed(xrange(0, 5))])
-    syscalls = ['pipe2', 'poll', ('write', lambda debugger: debugger.arg0 in (1, 2, 4))]
+    syscalls = ['pipe2', 'poll', ('write', lambda debugger: debugger.arg0 in (1, 2, 4)), 'thr_set_name']
     fs = ['/proc/self/loginuid$', '/etc/nsswitch.conf$', '/etc/passwd$']
 
     def get_nproc(self):

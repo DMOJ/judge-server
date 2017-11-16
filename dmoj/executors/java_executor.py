2dc306c78a73cab245484ba8d7f089546c167647
Tudor Brindus <xyene@dmoj.ca>
2017-11-16 13:03:59 -0500

    `br` = `rb` but works on Python 2

--- dmoj/executors/java_executor.py
+++ dmoj/executors/java_executor.py
@@ -17,7 +17,7 @@ restring = re.compile(r''''(?:\\.|[^'\\])'|"(?:\\.|[^"\\])*"''', re.DOTALL)
 reinline_comment = re.compile(r'//.*?(?=[\r\n])')
 reclass = re.compile(r'\bpublic\s+(?:strictfp\s+)?(?:(?:abstract|final)\s+)?(?:strictfp\s+)?class\s+([_a-zA-Z\$][_0-9a-zA-z\$]*?)\b')
 repackage = re.compile(r'\bpackage\s+([^.;]+(?:\.[^.;]+)*?);')
-redeunicode = re.compile(rb'\\u([0-9a-f]{4})', re.I)
+redeunicode = re.compile(br'\\u([0-9a-f]{4})', re.I)
 deunicode = lambda x: redeunicode.sub(lambda a: unichr(int(a.group(1), 16)), x)
 
 

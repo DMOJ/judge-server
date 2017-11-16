29a57097fb903f2849fe21647dd99e06509c364a
Tudor Brindus <xyene@dmoj.ca>
2017-11-16 14:03:04 -0500

    Stop maintaining old code paths

--- dmoj/utils/ansi.py
+++ dmoj/utils/ansi.py
@@ -1,6 +1,7 @@
-from collections import OrderedDict
-from termcolor import colored
 import re
+
+from termcolor import colored
+import ansi2html
 
 
 def strip_ansi(s):
@@ -8,22 +9,9 @@ def strip_ansi(s):
     return re.sub(r'\x1b\[([0-9,A-Z]{1,2}(;[0-9]{1,2})?(;[0-9]{3})?)?[m|K]?', '', s)
 
 
-try:
-    import ansi2html
-
-
-    def format_ansi(s):
-        return ansi2html.Ansi2HTMLConverter(inline=True).convert(s, full=False)
-except ImportError:
-    def format_ansi(s):
-        escape = OrderedDict([
-            ('&', '&amp;'),
-            ('<', '&lt;'),
-            ('>', '&gt;'),
-        ])
-        for a, b in escape.items():
-            s = s.replace(a, b)
-        return strip_ansi(s)
+def format_ansi(s):
+    # TODO: supposedly, the decode isn't necessary https://github.com/ralphbean/ansi2html/issues/60
+    return ansi2html.Ansi2HTMLConverter(inline=True).convert(s.decode('utf-8'), full=False)
 
 
 def ansi_style(text):

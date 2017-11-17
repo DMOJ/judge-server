e01dd1f403ee95af2380c6af176575fc70ce2a56
Quantum <quantum2048@gmail.com>
2017-11-17 00:33:39 -0500

    Screw range.

--- dmoj/utils/winutils.py
+++ dmoj/utils/winutils.py
@@ -1,11 +1,7 @@
 import os
 import signal
-import sys
 from ctypes import windll, c_size_t, Structure, sizeof, byref, c_uint64
 from ctypes.wintypes import DWORD, HANDLE, POINTER, BOOL, WinError
-
-if sys.version_info.major == 3:
-    xrange = range
 
 if os.name != 'nt':
     raise ImportError('No module named winutils')
@@ -42,7 +38,7 @@ def max_memory(handle):
 
 
 def execution_time(handle):
-    times = [c_uint64() for i in xrange(4)]
+    times = [c_uint64() for i in 'a' * 4]
     GetProcessTimes(handle, *map(byref, times))
     return (times[2].value + times[3].value) / 10000000.
 

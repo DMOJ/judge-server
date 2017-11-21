912c249ea5f8f75a822e160b8ddf45f6081f609c
Kirito Feng <kiritofeng@users.noreply.github.com>
2017-11-21 14:36:48 -0500

    Make it work...

--- dmoj/checkers/sorted.py
+++ dmoj/checkers/sorted.py
@@ -9,8 +9,8 @@ def check(process_output, judge_output, **kwargs):
     if len(process_lines) != len(judge_lines):
         return False
 
-    process_lines = map(six.text_type.split, process_lines)
-    judge_lines = map(six.text_type.split, judge_lines)
+    process_lines = map(six.binary_type.split, process_lines)
+    judge_lines = map(six.binary_type.split, judge_lines)
     process_lines.sort()
     judge_lines.sort()
 

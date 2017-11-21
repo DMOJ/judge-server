f025ca254261c4a44d99b13c40776f1e959d8b11
Tudor Brindus <me@tbrindus.ca>
2017-11-21 07:48:03 -0500

    Add C++17 executor

---
+++ dmoj/executors/CPP17.py
@@ -0,0 +1,18 @@
+from dmoj.executors.CPP03 import Executor as CPPExecutor
+
+
+class Executor(CPPExecutor):
+    command = 'g++17'
+    command_paths = ['g++-7', 'g++']
+    std = 'c++17'
+    name = 'CPP17'
+    test_program = '''
+#include <iostream>
+
+int main() {
+    float literal = 0x3.ABCp-10;
+    auto input = std::cin.rdbuf();
+    std::cout << input;
+    return 0;
+}
+'''

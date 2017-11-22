087cb7516382027f188450a795835f0a35dfe736
Tudor Brindus <me@tbrindus.ca>
2017-11-22 15:50:59 -0500

    Checkers: 50 shades of broken

--- dmoj/checkers/easy.py
+++ dmoj/checkers/easy.py
@@ -1,6 +1,9 @@
+from dmoj.utils.unicode import utf8bytes
+
+
 def check(process_output, judge_output, **kwargs):
     import re
     from collections import Counter
-    process_all = re.sub(br'\s+', '', process_output)
-    judge_all = re.sub(br'\s+', '', judge_output)
+    process_all = re.sub(br'\s+', '', utf8bytes(process_output))
+    judge_all = re.sub(br'\s+', '', utf8bytes(judge_output))
     return Counter(process_all.lower()) == Counter(judge_all.lower())

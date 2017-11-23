106197baf738a1202faaca1e07c4a19678b60c02
Quantum <quantum2048@gmail.com>
2017-11-23 00:33:41 -0500

    Add real unit tests.

---
+++ dmoj/tests/test_checker.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class CheckerTest(unittest.TestCase):
+    def test_standard(self):
+        from dmoj.checkers.standard import check
+
+        assert check(b'a', b'a')
+        assert check(b'a b', b'a  b')
+        assert check(b'a b   \n', b'a b')
+        assert check(b'\n\na b \n    ', b'a b')
+        assert check(b'a\n\n\nb', b'a\nb')
+        assert check(b'  a   \n\n', b'\n\n\n  a   \n')
+        assert check(b'a ' * 1000, b' a' * 1000)
+        assert check(b'a\n' * 1000, b'\n\n\na' * 1000)
+
+        assert not check(b'a', b'b')
+        assert not check(b'\n\n\na \n b\n', b'a b')
+        assert not check(b'a\n\n\nb', b'a b')

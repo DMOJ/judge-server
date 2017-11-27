768b87be289a37e8170013ec1f6055c440b0fba0
Tudor Brindus <me@tbrindus.ca>
2017-11-27 18:24:51 -0500

    gcc11 is not a real command on most systems; #276

--- dmoj/executors/C11.py
+++ dmoj/executors/C11.py
@@ -4,6 +4,7 @@ from .gcc_executor import GCCExecutor
 class Executor(GCCExecutor):
     command = 'gcc11'
     flags = ['-std=c11']
+    command_paths = ['gcc']
     ext = '.c'
     name = 'C11'
     test_program = '''

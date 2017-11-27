97781668a6ae80d4fa6330ccd70a6d01fe9a44a1
Tudor Brindus <me@tbrindus.ca>
2017-11-27 18:20:17 -0500

    Add C11 executor support; #276

---
+++ dmoj/executors/C11.py
@@ -0,0 +1,18 @@
+from .gcc_executor import GCCExecutor
+
+
+class Executor(GCCExecutor):
+    command = 'gcc11'
+    flags = ['-std=c11']
+    ext = '.c'
+    name = 'C11'
+    test_program = '''
+#include <stdio.h>
+
+int main() {
+    int ch;
+    while ((ch = getchar()) != EOF)
+        putchar(ch);
+    return 0;
+}
+'''

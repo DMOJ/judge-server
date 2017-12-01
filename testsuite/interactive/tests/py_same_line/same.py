e13ab2570236fd771cc142fbfab316d5fe98792d
kiritofeng <rogerjfu@hotmail.com>
2017-12-01 02:32:36 +0000

    Remove inconsistent tests

--- testsuite/interactive/tests/py_same_line/same.py
+++
@@ -1,5 +0,0 @@
-from sys import stdout
-print 0,
-stdout.flush()
-print input(),
-stdout.flush()
--- testsuite/interactive/tests/py_stdin_readline/readline.py
+++
@@ -1,5 +0,0 @@
-from sys import stdin, stdout
-print 0
-stdout.flush()
-print stdin.readline()
-stdout.flush()

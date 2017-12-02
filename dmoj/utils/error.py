01cc1645c37ba33dd9c9d88264dccd8679959417
Tudor Brindus <me@tbrindus.ca>
2017-12-02 01:13:55 -0500

    Everything sets up a logger nowadays

--- dmoj/utils/error.py
+++ dmoj/utils/error.py
@@ -9,7 +9,5 @@ log = logging.getLogger('dmoj.cptbox')
 def print_protection_fault(fault, stream=sys.stderr):
     syscall, callname, args = fault
     log.warning('Protection fault on: %d (%s)', syscall, callname)
-    print('Protection fault on: %d (%s)' % (syscall, callname), file=stream)
     for i, arg in enumerate(args):
         log.info('Arg%d: 0x%016x', i, arg)
-        print('Arg%d: 0x%016x' % (i, arg), file=stream)

f9b83bff9860ce49eb52f1bc27bda50fc5d231eb
Quantum <quantum2048@gmail.com>
2017-12-02 00:27:08 -0500

    Fix #286

--- dmoj/error.py
+++ dmoj/error.py
@@ -3,7 +3,7 @@ from dmoj.utils.unicode import utf8text
 
 class CompileError(Exception):
     def __init__(self, message):
-        super(CompileError, self).__init__(utf8text(message))
+        super(CompileError, self).__init__(utf8text(message, 'replace'))
 
 
 class InternalError(Exception):

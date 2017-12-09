cb2c9f9755103a4e672f05bf481059877cf021be
Tudor Brindus <xyene@dmoj.ca>
2017-12-09 03:50:01 -0500

    Drop dual PHP/PHP7 executors as per #117

--- dmoj/executors/PHP.py
+++ dmoj/executors/PHP.py
@@ -1,7 +1,15 @@
-from .php_executor import PHPExecutor
+from dmoj.executors.base_executor import ScriptExecutor
 
 
-class Executor(PHPExecutor):
+class Executor(ScriptExecutor):
     name = 'PHP'
+    ext = '.php'
     command = 'php'
-    command_paths = ['php5', 'php']
+    command_paths = ['php7', 'php5', 'php']
+
+    fs = ['.*/php[\w-]*\.ini$', '.*/conf.d/.*\.ini$']
+
+    test_program = '<?php while($f = fgets(STDIN)) echo $f;'
+
+    def get_cmdline(self):
+        return ['php', self._code]
--- dmoj/executors/PHP7.py
+++
@@ -1,7 +0,0 @@
-from .php_executor import PHPExecutor
-
-
-class Executor(PHPExecutor):
-    name = 'PHP7'
-    command = 'php7'
-    fs = ['.*\.ini$']

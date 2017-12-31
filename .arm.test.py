8aacd37375e9f6881b35bcf6506c203031dbb201
Xyene <xyene@dmoj.ca>
2017-12-31 11:29:28 -0500

    Add ARM-specific CI test; #300

---
+++ .arm.test.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+
+import logging
+
+from dmoj import judgeenv
+from dmoj.citest import ci_test
+
+EXECUTORS = ['AWK', 'BF', 'C', 'CPP03', 'CPP11', 'CPP14', 'CLANG', 'CLANGX',
+             'JAVA8', 'MONOCS', 'MONOFS', 'MONOVB', 'PAS', 'PERL',
+             'PY2', 'PY3', 'RUBY2', 'SED', 'TEXT']
+OVERRIDES = {}
+
+
+def main():
+    judgeenv.env['runtime'] = {
+        'clang_target_arch': None,  # Clang can't autodetect march=native
+    }
+    judgeenv.env['extra_fs'] = {};
+
+    logging.basicConfig(level=logging.INFO)
+
+    print('Using extra allowed filesystems:')
+    for lang, fs in judgeenv.env['extra_fs'].iteritems():
+        print('%-6s: %s' % (lang, '|'.join(fs)))
+    print()
+
+    print('Testing executors...')
+    ci_test(EXECUTORS, OVERRIDES)
+
+
+if __name__ == '__main__':
+    main()

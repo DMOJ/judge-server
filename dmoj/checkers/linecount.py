3b062bb6939696af6a729cc9cf6ed736ce296b88
kiritofeng <rogerjfu@hotmail.com>
2017-12-31 20:48:33 +0000

    Remove `strip()` call

--- dmoj/checkers/linecount.py
+++ dmoj/checkers/linecount.py
@@ -11,8 +11,8 @@ verdict = u"\u2717\u2713"
 
 def check(process_output, judge_output, point_value, feedback=False,
           match=lambda p, j: p.strip() == j.strip(), **kwargs):
-    process_lines = list(filter(None, resplit(b'[\r\n]', utf8bytes(process_output.strip()))))
-    judge_lines = list(filter(None, resplit(b'[\r\n]', utf8bytes(judge_output.strip()))))
+    process_lines = list(filter(None, resplit(b'[\r\n]', utf8bytes(process_output))))
+    judge_lines = list(filter(None, resplit(b'[\r\n]', utf8bytes(judge_output))))
 
     if len(process_lines) > len(judge_lines):
         return False

c1c34252322864d424961d7bd3d4b1d18ed776f6
Xyene <xyene@dmoj.ca>
2017-12-31 11:26:05 -0500

    Add Clang-specific arch options; #303

--- dmoj/executors/CLANG.py
+++ dmoj/executors/CLANG.py
@@ -5,3 +5,4 @@ class Executor(CExecutor):
     command = 'clang'
     command_paths = ['clang-%s' % i for i in ['3.9', '3.8', '3.7', '3.6', '3.5']] + ['clang']
     name = 'CLANG'
+    arch = 'clang_target_arch'

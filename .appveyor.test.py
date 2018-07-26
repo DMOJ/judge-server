578c1bbf552ae2c1c587c6b8bf244c6d7fb766d9
kiritofeng <rogerjfu@hotmail.com>
2018-07-26 03:52:48 +0000

    Do not fix Windows by breaking Linux...

--- .appveyor.test.py
+++ .appveyor.test.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+import codecs
 import logging
 import os
 import sys
@@ -16,9 +17,8 @@ required_executors = ['AWK', 'BF', 'C', 'CPP03', 'CPP11', 'CS', 'PERL', 'PY2', '
 def main():
     logging.basicConfig(level=logging.INFO)
 
-    sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
-    sys.stderr = os.fdopen(sys.stderr.fileno(), 'w', 0)
-    unicode_stdout_stderr()
+    sys.stdout = codecs.getwriter('utf-8')(os.fdopen(sys.stdout.fileno(), 'w', 0))
+    sys.stderr = codecs.getwriter('utf-8')(os.fdopen(sys.stderr.fileno(), 'w', 0))
 
     judgeenv.load_env(cli=True, testsuite=True)
     executors.load_executors()

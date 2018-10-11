5e0d460de24cdf3dc88496427e86cf3d54bbd724
xiaowuc1 <huaiyu.wu2010@gmail.com>
2018-10-10 17:58:10 -0700

    Add presentation error feedback to identical checker (#342)

--- dmoj/checkers/identical.py
+++ dmoj/checkers/identical.py
@@ -1,2 +1,12 @@
-def check(process_output, judge_output, **kwargs):
-    return judge_output == process_output
+from dmoj.checkers._checker import standard
+from dmoj.result import CheckerResult
+from dmoj.utils.unicode import utf8bytes
+
+def check(process_output, judge_output, pe_allowed=True, **kwargs):
+    if judge_output == process_output:
+        return True
+    feedback = None
+    if pe_allowed and standard(utf8bytes(judge_output), utf8bytes(process_output)):
+        # in the event the standard checker would have passed the problem, raise a presentation error
+        feedback = "Presentation Error"
+    return CheckerResult(False, 0, feedback=feedback)

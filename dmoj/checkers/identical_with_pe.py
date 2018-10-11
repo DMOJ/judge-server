931779f29de8584737dbc61d9eeb9ca987a81a67
xiaowuc1 <xiaowuc1@gmail.com>
2018-10-10 17:17:33 -0700

    add identical_with_pe checker

---
+++ dmoj/checkers/identical_with_pe.py
@@ -0,0 +1,13 @@
+from dmoj.checkers._checker import standard
+from dmoj.result import CheckerResult
+from dmoj.utils.unicode import utf8bytes
+
+def check(process_output, judge_output, **kwargs):
+    # return AC iff the output matches exactly
+    if judge_output == process_output:
+        return True
+    feedback = None
+    if standard(utf8bytes(judge_output), utf8bytes(process_output)):
+        # in the event the standard checker would have passed the problem, raise a presentation error
+        feedback = "Presentation Error"
+    return CheckerResult(False, 0, feedback=feedback)

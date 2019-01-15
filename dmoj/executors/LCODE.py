299c3ca93f335f99cc3602e7612f6b0125785318
Evan <evanzhang1028@hotmail.com>
2019-01-14 22:25:28 -0500

    Add LOLCODE executor

---
+++ dmoj/executors/LCODE.py
@@ -0,0 +1,20 @@
+from dmoj.executors.script_executor import ScriptExecutor
+
+
+class Executor(ScriptExecutor):
+    name = 'LOLCODE'
+    ext = '.lic'
+    command = 'lci'
+    command_paths = ['lci']
+
+    test_program = '''\
+HAI 1.2
+    CAN HAS STDIO?
+    I HAS A INPUT
+    GIMMEH INPUT
+    VISIBLE INPUT
+KTHXBYE
+'''
+
+    def get_cmdline(self):
+        return ['lci', self._code]

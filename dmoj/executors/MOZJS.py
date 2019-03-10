57379d8b058a1eed55e3dafdbd8937120a9a2b46
ehhthing <ehhthing@users.noreply.github.com>
2019-03-10 15:23:27 -0400

    Install SpiderMonkey from Mozilla instead of apt
    Using apt-get/apt is not suitable because the latest version for Ubuntu 14 is js24, which lacks many features (notably the --version flag) and is outdated.

--- dmoj/executors/MOZJS.py
+++ dmoj/executors/MOZJS.py
@@ -3,11 +3,6 @@ from .base_executor import ScriptExecutor
 class Executor(ScriptExecutor):
     ext = '.js'
     name = 'MOZJS'
-    command_paths = ['js', 'js24', 'js52']
+    command_paths = ['js', 'js52']
     test_program = 'print(readline());'
     nproc = -1
- 
-    @classmethod
-    def get_version_flags(cls, command):
-    	# In js24, there is no version flag, so we must parse the output from the help flag.
-        return ['--help'] 

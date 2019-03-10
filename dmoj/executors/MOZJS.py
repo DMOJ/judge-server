21827c4fd4c253a9965422e6d8d3af66288c3a19
ehhthing <ehhthing@users.noreply.github.com>
2019-03-10 13:59:21 -0400

    Misc Changes (see extended desc.)
    - Change indentation to spaces
    - Downgrade libmoz to 24 (only version supported on ubuntu 14-16)
    - Parse version from help flag because js24 doesn't have a version flag.

--- dmoj/executors/MOZJS.py
+++ dmoj/executors/MOZJS.py
@@ -1,12 +1,13 @@
 from .base_executor import ScriptExecutor
 
 class Executor(ScriptExecutor):
-	ext = '.js'
-	name = 'MOZJS'
-	command_paths = ['js', 'js24', 'js52']
-	test_program = 'print(readline());'
-	nproc = -1
-
-	@classmethod
-	def get_version_flags(cls, command):
-		return ['-v']
+    ext = '.js'
+    name = 'MOZJS'
+    command_paths = ['js', 'js24', 'js52']
+    test_program = 'print(readline());'
+    nproc = -1
+ 
+    @classmethod
+    def get_version_flags(cls, command):
+    	# In js24, there is no version flag, so we must parse the output from the help flag.
+        return ['--help'] 

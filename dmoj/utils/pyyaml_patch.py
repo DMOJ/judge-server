b2e73d975e02617858ef7116f51a248be84721bf
Quantum <quantum2048@gmail.com>
2019-03-29 19:21:12 -0400

    Patch PyYAML to support direct emoji input

---
+++ dmoj/utils/pyyaml_patch.py
@@ -0,0 +1,8 @@
+import yaml.reader
+import re
+import sys
+
+
+if yaml.reader.Reader.NON_PRINTABLE.pattern == u'[^\t\n\r -~\x85\xa0-\ud7ff\ue000-\ufffd]' and sys.maxunicode > 65535:
+    yaml.reader.Reader.NON_PRINTABLE = re.compile(
+        u'[^\x09\x0A\x0D\x20-\x7E\x85\xA0-\uD7FF\uE000-\uFFFD\U00010000-\U0010FFFF]')

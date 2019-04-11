c266e15465b19b57764367f41b62cce2e010a597
kiritofeng <rogerjfu@hotmail.com>
2019-04-11 03:18:30 +0000

    Make DART executor sane
    Compiling the file appears to reduce memory usage

--- dmoj/executors/DART.py
+++ dmoj/executors/DART.py
@@ -1,7 +1,10 @@
-from .base_executor import ScriptExecutor
+from .base_executor import CompiledExecutor
+from dmoj.executors.mixins import ScriptDirectoryMixin
 
 
-class Executor(ScriptExecutor):
+# Running DART normally results in unholy memory usage
+# Thankfully compiling it results in something...far more sane
+class Executor(ScriptDirectoryMixin, CompiledExecutor):
     ext = '.dart'
     name = 'DART'
     nproc = -1  # Dart uses a really, really large number of threads
@@ -11,9 +14,17 @@ void main() {
     print("echo: Hello, World!");
 }
 '''
-    address_grace = 786432
-    data_grace = 65536
+    address_grace = 128 * 1024
 
     syscalls = ['epoll_create', 'epoll_ctl', 'epoll_wait', 'timerfd_settime', 'pipe2']
 
     fs = ['.*/vm-service$']
+
+    def get_compile_args(self):
+        return [self.get_command(), '--snapshot=%s' % self.get_compiled_file(), self._code]
+
+    def get_cmdline(self):
+        return [self.get_command(), self.get_compiled_file()]
+
+    def get_executable(self):
+        return self.get_command()

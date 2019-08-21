a3cc45756f0f6f1247041a054c7b133df9a8bd32
Tudor Brindus <me@tbrindus.ca>
2019-08-20 20:49:06 -0400

    Stupid hack

--- dmoj/executors/PY3.py
+++ dmoj/executors/PY3.py
@@ -1,3 +1,5 @@
+import os
+
 from dmoj.executors.python_executor import PythonExecutor
 
 
@@ -6,3 +8,8 @@ class Executor(PythonExecutor):
     command_paths = ['python%s' % i for i in ['3.6', '3.5', '3.4', '3.3', '3.2', '3.1', '3']]
     test_program = "print(__import__('sys').stdin.read(), end='')"
     name = 'PY3'
+
+    def get_env(self):
+        env = super(Executor, self).get_env()
+        env['PYTHONHOME'] = os.path.dirname(self.get_executable())
+        return env

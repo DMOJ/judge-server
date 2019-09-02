c824d9ec83c952748ea03a9ac3e8a48818dd5694
kiritofeng <rogerjfu@hotmail.com>
2019-09-02 08:47:21 -0400

    Use `Decimal` in floats checker; #456

--- dmoj/checkers/floats.py
+++ dmoj/checkers/floats.py
@@ -1,17 +1,21 @@
+from decimal import Decimal
 from re import split as resplit
+from typing import Union
 
 from dmoj.error import InternalError
 from dmoj.utils.unicode import utf8bytes
 
 
-def verify_absolute(process_float: float, judge_float: float, epsilon: float) -> bool:
+def verify_absolute(process_float: Union[float, Decimal], judge_float: Union[float, Decimal],
+                    epsilon: Union[float, Decimal]) -> bool:
     # Since process_float can be NaN, this is NOT equivalent to
     # (process_float - judge_float) > epsilon;  the code below will always
     # reject NaN, even if judge_float is NaN
     return abs(process_float - judge_float) <= epsilon
 
 
-def verify_relative(process_float: float, judge_float: float, epsilon: float) -> bool:
+def verify_relative(process_float: Union[float, Decimal], judge_float: Union[float, Decimal],
+                    epsilon: Union[float, Decimal]) -> bool:
     p1 = min(judge_float * (1 - epsilon), judge_float * (1 + epsilon))
     p2 = max(judge_float * (1 - epsilon), judge_float * (1 + epsilon))
     # Since process_float can be NaN, this is NOT equivalent to
@@ -19,7 +23,8 @@ def verify_relative(process_float: float, judge_float: float, epsilon: float) ->
     return p1 <= process_float <= p2
 
 
-def verify_default(process_float: float, judge_float: float, epsilon: float) -> bool:
+def verify_default(process_float: Union[float, Decimal], judge_float: Union[float, Decimal],
+                   epsilon: Union[float, Decimal]) -> bool:
     # process_float can be NaN
     # in this case, we reject NaN as a possible answer, even if judge_float is NaN
     return (abs(process_float - judge_float) <= epsilon or
@@ -28,7 +33,12 @@ def verify_default(process_float: float, judge_float: float, epsilon: float) -> 
 
 
 def check(process_output: bytes, judge_output: bytes, precision: int = 6,
-          error_mode: str = 'default', **kwargs) -> bool:
+          high_precision: bool = False, error_mode: str = 'default', **kwargs) -> bool:
+    if high_precision:
+        float_datatype = Decimal
+    else:
+        float_datatype = float
+
     # Discount empty lines
     process_lines = list(filter(None, resplit(b'[\r\n]', utf8bytes(process_output))))
     judge_lines = list(filter(None, resplit(b'[\r\n]', utf8bytes(judge_output))))
@@ -45,7 +55,7 @@ def check(process_output: bytes, judge_output: bytes, precision: int = 6,
     if not verify_float:
         raise InternalError('invalid `error_mode` value')
 
-    epsilon = 10 ** -int(precision)
+    epsilon = float_datatype(10) ** -int(precision)
 
     try:
         for process_line, judge_line in zip(process_lines, judge_lines):
@@ -58,13 +68,13 @@ def check(process_output: bytes, judge_output: bytes, precision: int = 6,
             for process_token, judge_token in zip(process_tokens, judge_tokens):
                 # Allow mixed tokens, for lines like "abc 0.68 def 0.70"
                 try:
-                    judge_float = float(judge_token)
+                    judge_float = float_datatype(judge_token)
                 except ValueError:
                     # If it's not a float the token must match exactly
                     if process_token != judge_token:
                         return False
                 else:
-                    process_float = float(process_token)
+                    process_float = float_datatype(process_token)
 
                     if not verify_float(process_float, judge_float, epsilon):
                         return False

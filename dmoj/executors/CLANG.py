9776a887aa25996f811f1f0c608146c5381e8caf
Guanzhong Chen <quantum2048@gmail.com>
2019-09-14 15:38:01 -0400

    Correctly display clang version (#477)
    clang -dumpversion always returns 4.2.1 for compatibility reasons.

--- dmoj/executors/CLANG.py
+++ dmoj/executors/CLANG.py
@@ -1,8 +1,8 @@
 from dmoj.executors.C import Executor as CExecutor
+from dmoj.executors.clang_executor import ClangExecutor
 
 
-class Executor(CExecutor):
+class Executor(ClangExecutor, CExecutor):
     command = 'clang'
     command_paths = ['clang-%s' % i for i in ['3.9', '3.8', '3.7', '3.6', '3.5']] + ['clang']
     name = 'CLANG'
-    arch = 'clang_target_arch'

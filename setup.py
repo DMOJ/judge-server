4413fa5becfa54a37c3c9575128ae5c6a9a42c1f
Quantum <quantum2048@gmail.com>
2019-09-15 13:12:14 -0400

    Release 1.4.0

--- setup.py
+++ setup.py
@@ -151,7 +151,7 @@ with io.open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf
 
 setup(
     name='dmoj',
-    version='1.3.0',
+    version='1.4.0',
     packages=find_packages(),
     package_data={
         'dmoj.cptbox': ['syscalls/aliases.list', 'syscalls/*.tbl'],

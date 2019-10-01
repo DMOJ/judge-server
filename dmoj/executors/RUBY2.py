538734aadda3ea4c80f70e80c99c7f2cabb72b5e
Evan <evanzhang1028@hotmail.com>
2019-10-01 10:14:40 -0400

    Bump up ruby command path to support Ruby 2.6

--- dmoj/executors/RUBY2.py
+++ dmoj/executors/RUBY2.py
@@ -4,7 +4,7 @@ from .ruby_executor import RubyExecutor
 class Executor(RubyExecutor):
     name = 'RUBY2'
     nproc = -1
-    command_paths = (['ruby2.%d' % i for i in reversed(range(0, 5))] +
-                     ['ruby2%d' % i for i in reversed(range(0, 5))])
+    command_paths = (['ruby2.%d' % i for i in reversed(range(0, 7))] +
+                     ['ruby2%d' % i for i in reversed(range(0, 7))])
     syscalls = ['poll', 'thr_set_name']
     fs = ['/proc/self/loginuid$']

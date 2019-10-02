ba7b0b477cdaa5ea7b04a884f345f98c7332884e
Michael Gray <mike@graywind.org>
2019-10-02 16:45:18 -0500

    Change from kiritofeng

--- dmoj/checkers/linecount.py
+++ dmoj/checkers/linecount.py
@@ -8,7 +8,8 @@ verdict = u"\u2717\u2713"
 
 
 def check(process_output: str, judge_output: str, point_value: float, feedback: Optional[bool] = True,
-          match: Optional[callable] = lambda p, j: p.strip() == j.strip(), **kwargs) -> Union[CheckerResult, bool]:
+          match: Optional[Union[callable, str]] = lambda p, j: p.strip() == j.strip(),
+          **kwargs) -> Union[CheckerResult, bool]:
     process_lines = list(filter(None, resplit(b'[\r\n]', utf8bytes(process_output))))
     judge_lines = list(filter(None, resplit(b'[\r\n]', utf8bytes(judge_output))))
 

f7246c478d07d05d4b3ec34ce8e327507fd61bf4
xiaowuc1 <xiaowuc1@gmail.com>
2019-10-02 22:53:48 -0700

    Add mypy types to utils/unicode.py
    mypy considers the original return statements to be
    errors since they omit the implicit None.

--- dmoj/utils/unicode.py
+++ dmoj/utils/unicode.py
@@ -1,18 +1,19 @@
 import codecs
 import sys
+from typing import Optional
 
 
-def utf8bytes(maybe_text):
+def utf8bytes(maybe_text) -> Optional[bytes]:
     if maybe_text is None:
-        return
+        return None
     if isinstance(maybe_text, bytes):
         return maybe_text
     return maybe_text.encode('utf-8')
 
 
-def utf8text(maybe_bytes, errors='strict'):
+def utf8text(maybe_bytes, errors='strict') -> Optional[str]:
     if maybe_bytes is None:
-        return
+        return None
     if isinstance(maybe_bytes, str):
         return maybe_bytes
     return maybe_bytes.decode('utf-8', errors)

3ca408ddd926ca423b8facfb950e9c3817cf4b9d
Tudor Brindus <xyene@dmoj.ca>
2019-10-06 17:59:14 -0400

    Don't try to populate process output on OLE (#532)

--- dmoj/graders/standard.py
+++ dmoj/graders/standard.py
@@ -117,9 +117,7 @@ class StandardGrader(BaseGrader):
         try:
             result.proc_output, error = process.communicate(input, outlimit=case.config.output_limit_length,
                                                             errlimit=1048576)
-        except OutputLimitExceeded as ole:
-            stream, result.proc_output, error = ole.args
-            log.warning('OLE on stream: %s', stream)
+        except OutputLimitExceeded:
             result.result_flag |= Result.OLE
             try:
                 process.kill()

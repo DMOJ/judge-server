DMOJ/judge-server

commit bd6c8e66d5f9ee406bb1d55cbe524ecf26d8391c

Make base judge pass mypy checks (#531)

dmoj/cli.py
@@ -1,7 +1,9 @@
 import shlex
 import sys
 from collections import OrderedDict
+from typing import Dict
 
+from dmoj.commands import Command
 from dmoj.judge import Judge
 from dmoj.utils.ansi import ansi_style, print_ansi
 
@@ -69,7 +71,7 @@ class InvalidCommandException(Exception):
         self.message = message
 
 
-commands = OrderedDict()
+commands: Dict[str, Command] = OrderedDict()
 
 
 def register(command):

dmoj/commands/__init__.py
@@ -1,3 +1,6 @@
+from typing import List, Type
+
+from dmoj.commands.base_command import Command
 from dmoj.commands.diff import DifferenceCommand
 from dmoj.commands.help import HelpCommand
 from dmoj.commands.problems import ListProblemsCommand
@@ -8,5 +11,7 @@ from dmoj.commands.show import ShowCommand
 from dmoj.commands.submissions import ListSubmissionsCommand
 from dmoj.commands.submit import SubmitCommand
 
-all_commands = [ListProblemsCommand, ListSubmissionsCommand, SubmitCommand, ResubmitCommand, RejudgeCommand,
-                DifferenceCommand, ShowCommand, HelpCommand, QuitCommand]
+all_commands: List[Type[Command]] = [
+    ListProblemsCommand, ListSubmissionsCommand, SubmitCommand, ResubmitCommand, RejudgeCommand, DifferenceCommand,
+    ShowCommand, HelpCommand, QuitCommand,
+]

dmoj/judgeenv.py
@@ -1,5 +1,7 @@
 import argparse
 import os
+import ssl
+from typing import List, Set
 
 import yaml
 
@@ -7,11 +9,6 @@ from dmoj.config import ConfigNode
 # noinspection PyUnresolvedReferences
 from dmoj.utils import pyyaml_patch  # noqa: F401, imported for side effect
 from dmoj.utils.unicode import utf8text
-
-try:
-    import ssl
-except ImportError:
-    ssl = None
 
 problem_dirs = ()
 problem_watches = ()
@@ -43,11 +40,11 @@ log_file = server_host = server_port = no_ansi = no_watchdog = problem_regex = c
 secure = no_cert_check = False
 cert_store = api_listen = None
 
-startup_warnings = []
-cli_command = []
+startup_warnings: List[str] = []
+cli_command: List[str] = []
 
-only_executors = set()
-exclude_executors = set()
+only_executors: Set[str] = set()
+exclude_executors: Set[str] = set()
 
 
 def load_env(cli=False, testsuite=False):  # pragma: no cover
@@ -85,15 +82,12 @@ def load_env(cli=False, testsuite=False):  # pragma: no cover
         parser.add_argument('-A', '--api-host', default='127.0.0.1',
                             help='IPv4 address to listen for judge API')
 
-        if ssl:
-            parser.add_argument('-s', '--secure', action='store_true',
-                                help='connect to server via TLS')
-            parser.add_argument('-k', '--no-certificate-check', action='store_true',
-                                help='do not check TLS certificate')
-            parser.add_argument('-T', '--trusted-certificates', default=None,
-                                help='use trusted certificate file instead of system')
-        else:
-            parser.set_defaults(secure=False, no_certificate_check=False, trusted_certificates=None)
+        parser.add_argument('-s', '--secure', action='store_true',
+                            help='connect to server via TLS')
+        parser.add_argument('-k', '--no-certificate-check', action='store_true',
+                            help='do not check TLS certificate')
+        parser.add_argument('-T', '--trusted-certificates', default=None,
+                            help='use trusted certificate file instead of system')
 
     _group = parser.add_mutually_exclusive_group()
     _group.add_argument('-e', '--only-executors',

dmoj/packet.py
@@ -2,22 +2,21 @@ import errno
 import json
 import logging
 import socket
+import ssl
 import struct
 import sys
 import threading
 import time
 import traceback
 import zlib
-from typing import List, Optional, Tuple
+from typing import List, Optional, TYPE_CHECKING, Tuple
 
 from dmoj import sysinfo
 from dmoj.judgeenv import get_runtime_versions, get_supported_problems
 from dmoj.utils.unicode import utf8bytes, utf8text
 
-try:
-    import ssl
-except ImportError:
-    ssl = None
+if TYPE_CHECKING:
+    from dmoj.judge import Judge
 
 log = logging.getLogger(__name__)
 
@@ -29,7 +28,10 @@ class JudgeAuthenticationFailed(Exception):
 class PacketManager:
     SIZE_PACK = struct.Struct('!I')
 
-    def __init__(self, host: str, port: int, judge: object, name: str, key: str,
+    ssl_context: Optional[ssl.SSLContext]
+    judge: 'Judge'
+
+    def __init__(self, host: str, port: int, judge: 'Judge', name: str, key: str,
                  secure: bool = False, no_cert_check: bool = False,
                  cert_store: Optional[str] = None):
         self.host = host
@@ -40,7 +42,7 @@ class PacketManager:
         self._closed = False
 
         log.info('Preparing to connect to [%s]:%s as: %s', host, port, name)
-        if secure and ssl:
+        if secure:
             self.ssl_context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
             self.ssl_context.options |= ssl.OP_NO_SSLv2
             self.ssl_context.options |= ssl.OP_NO_SSLv3

dmoj/sysinfo.py
@@ -1,7 +1,7 @@
 import os
-from multiprocessing import cpu_count
+from multiprocessing import cpu_count as _get_cpu_count
 
-_cpu_count = cpu_count()
+_cpu_count = _get_cpu_count()
 
 
 if hasattr(os, 'getloadavg'):

commit d9b998a1f0f1df9b3ce31c6bcaeae00566427af9

Fix circular imports caused by bd6c8e66d5f9ee406bb1d55cbe524ecf26d8391c

dmoj/cli.py
@@ -1,9 +1,7 @@
 import shlex
 import sys
-from collections import OrderedDict
-from typing import Dict
 
-from dmoj.commands import Command
+from dmoj.errors import InvalidCommandException
 from dmoj.judge import Judge
 from dmoj.utils.ansi import ansi_style, print_ansi
 
@@ -66,24 +64,11 @@ class LocalJudge(Judge):
         self.graded_submissions = []
 
 
-class InvalidCommandException(Exception):
-    def __init__(self, message=None):
-        self.message = message
-
-
-commands: Dict[str, Command] = OrderedDict()
-
-
-def register(command):
-    commands[command.name] = command
-
-
 def main():
     sys.exit(cli_main())
 
 
 def cli_main():
-    global commands
     import logging
     from dmoj import judgeenv, executors
 
@@ -102,9 +87,9 @@ def cli_main():
     del judgeenv.startup_warnings
     print()
 
-    from dmoj.commands import all_commands
+    from dmoj.commands import all_commands, commands, register_command
     for command in all_commands:
-        register(command(judge))
+        register_command(command(judge))
 
     def run_command(line):
         if not line:

dmoj/commands/__init__.py
@@ -1,6 +1,6 @@
 from typing import List, Type
 
-from dmoj.commands.base_command import Command
+from dmoj.commands.base_command import Command, commands, register_command
 from dmoj.commands.diff import DifferenceCommand
 from dmoj.commands.help import HelpCommand
 from dmoj.commands.problems import ListProblemsCommand

dmoj/commands/base_command.py
@@ -3,8 +3,10 @@ import os
 import subprocess
 import sys
 import tempfile
+from collections import OrderedDict
+from typing import Dict
 
-from dmoj.cli import InvalidCommandException
+from dmoj.error import InvalidCommandException
 from dmoj.executors import executors
 from dmoj.utils.ansi import print_ansi
 
@@ -76,3 +78,10 @@ class Command:
 
     def execute(self, line):
         raise NotImplementedError
+
+
+commands: Dict[str, Command] = OrderedDict()
+
+
+def register_command(command):
+    commands[command.name] = command

dmoj/commands/help.py
@@ -1,5 +1,4 @@
-from dmoj.cli import commands
-from dmoj.commands.base_command import Command
+from dmoj.commands.base_command import Command, commands
 
 
 class HelpCommand(Command):

dmoj/commands/problems.py
@@ -3,8 +3,8 @@ from itertools import zip_longest
 from operator import itemgetter
 
 from dmoj import judgeenv
-from dmoj.cli import InvalidCommandException
 from dmoj.commands.base_command import Command
+from dmoj.error import InvalidCommandException
 
 
 class ListProblemsCommand(Command):

dmoj/commands/resubmit.py
@@ -1,8 +1,8 @@
 from operator import itemgetter
 
 from dmoj import judgeenv
-from dmoj.cli import InvalidCommandException
 from dmoj.commands.base_command import Command
+from dmoj.error import InvalidCommandException
 from dmoj.executors import executors
 
 

dmoj/commands/submissions.py
@@ -1,5 +1,5 @@
-from dmoj.cli import InvalidCommandException
 from dmoj.commands.base_command import Command
+from dmoj.error import InvalidCommandException
 from dmoj.utils.ansi import print_ansi
 
 

dmoj/commands/submit.py
@@ -1,8 +1,8 @@
 from operator import itemgetter
 
 from dmoj import judgeenv
-from dmoj.cli import InvalidCommandException
 from dmoj.commands.base_command import Command
+from dmoj.error import InvalidCommandException
 from dmoj.executors import executors
 
 

dmoj/error.py
@@ -13,3 +13,8 @@ class InternalError(Exception):
 class OutputLimitExceeded(Exception):
     def __init__(self, stream, limit):
         super().__init__("exceeded %d-byte limit on %s stream" % (limit, stream))
+
+
+class InvalidCommandException(Exception):
+    def __init__(self, message=None):
+        self.message = message
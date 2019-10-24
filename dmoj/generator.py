7aa325c062d9934f40aa78fbffdafbfe09aec67b
Xyene <xyene@dmoj.ca>
2019-10-24 14:14:55 -0400

    Fix up generator TL; #563

--- dmoj/generator.py
+++ dmoj/generator.py
@@ -2,7 +2,6 @@ import os
 import traceback
 
 from dmoj.error import InternalError
-from dmoj.judgeenv import env
 
 
 class GeneratorManager:
@@ -39,7 +38,6 @@ class GeneratorManager:
         clazz = clazz.Executor
 
         if issubclass(clazz, CompiledExecutor):
-            compiler_time_limit = compiler_time_limit or env.generator_compiler_time_limit
             clazz = type('Executor', (clazz,), {'compiler_time_limit': compiler_time_limit})
 
         # Optimize the common case.

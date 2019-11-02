05624defe2b9f65b7571fab5b445588281b374ab
Tudor Brindus <xyene@dmoj.ca>
2019-11-02 00:49:52 -0400

    Test ARM64 judge on Travis in Docker (#573)

--- .docker.test.py
+++ .docker.test.py
@@ -1,10 +1,22 @@
 import logging
+import os
+import platform
 
 from dmoj import judgeenv
 from dmoj.citest import ci_test
 from dmoj.executors import get_available
 
-ALLOW_FAIL = {'GASARM', 'JAVA9', 'JAVA10', 'OBJC'}
+arch = platform.machine()
+if arch == 'x86_64':
+    ALLOW_FAIL = {'GASARM', 'JAVA9', 'JAVA10', 'OBJC'}
+    EXECUTORS = get_available()
+elif arch == 'aarch64':
+    EXECUTORS = {'AWK', 'BF', 'C', 'C11', 'CPP03', 'CPP11', 'CPP14', 'CPP17',
+                 'JAVA8', 'PAS', 'PERL', 'PY2', 'PY3', 'SED', 'TEXT'}
+    ALLOW_FAIL = set()
+else:
+    raise AssertionError('invalid architecture')
+
 OVERRIDES = {}
 
 
@@ -16,7 +28,7 @@ def main():
 
     print('Testing executors...')
 
-    ci_test(get_available(), OVERRIDES, ALLOW_FAIL)
+    ci_test(EXECUTORS, OVERRIDES, ALLOW_FAIL)
 
 
 if __name__ == '__main__':

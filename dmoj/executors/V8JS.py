381adeeec0fd1d65372d7003183d4b1ec8f2cfbf
Larry <ehhthing@users.noreply.github.com>
2019-11-09 23:15:35 -0500

    Increase V8JS Stack Limit (#584)

--- dmoj/executors/V8JS.py
+++ dmoj/executors/V8JS.py
@@ -12,3 +12,6 @@ class Executor(ScriptExecutor):
     @classmethod
     def get_version_flags(cls, command):
         return [('-e', 'print(version())')]
+    
+    def get_cmdline(self):
+        return [self.get_command(), '--stack-size=131072', self._code] # 128MB Stack Limit

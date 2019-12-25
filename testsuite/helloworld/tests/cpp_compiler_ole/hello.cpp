754be304dd0cf1925d9a28187dec650ae4f6ef22
kiritofeng <rogerjfu@hotmail.com>
2019-12-25 17:19:27 -0500

    Make compiler OLE test actually OLE compiler; #44

--- testsuite/helloworld/tests/cpp_compiler_ole/hello.cpp
+++ testsuite/helloworld/tests/cpp_compiler_ole/hello.cpp
@@ -1,1 +1,14 @@
-template<class T>class a{a<T*>operator->();};a<int>i=i->b;
+#define A(x) x##x##x##x
+#define B(x) A(x##x##x##x)
+#define C(x) B(x##x##x##x)
+#define D(x) C(x##x##x##x)
+#define E(x) D(x##x##x##x)
+#define F(x) E(x##x##x##x)
+#define G(x) F(x##x##x##x)
+#define H(x) G(x##x##x##x)
+#define I(x) H(x##x##x##x)
+#define J(x) I(x##x##x##x)
+#define K(x) J(x##x##x##x)
+int main() {
+    int x = K(a);
+}

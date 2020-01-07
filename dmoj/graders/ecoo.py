b276e2525cd88a3d23a693887d0c88d2b1a25470
Roger Fu <kiritofeng@users.noreply.github.com>
2020-01-07 14:26:31 -0500

    Add ECOO Grader

---
+++ dmoj/graders/ecoo.py
@@ -0,0 +1,19 @@
+from dmoj.graders.standard import StandardGrader
+from dmoj.result import CheckerResult
+from dmoj.utils.unicode import utf8text
+
+class ECOOGrader(StandardGrader):
+    def check_result(self, case, result):
+        check = super().check_result(case, result)
+        if not isinstance(check, CheckerResult):
+            check = CheckerResult(check, case.points if check else 0.0)
+        if not check.passed:
+            check.extended_feedback = utf8text(case.input_data())
+        return check
+
+    def cases(self):
+        cfg = self.problem.config['pretest_test_cases' if self.is_pretested else 'test_cases']
+        if self.meta.contest_no is not None:
+            return self._resolve_testcases(cfg.get(self.meta.attempt_no, 'default'))
+        else:
+            return self._resolve_testcases(cfg['default'])

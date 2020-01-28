562a85efa59051c89d5d45800a8c0a475e2c025e
Tudor Brindus <xyene@dmoj.ca>
2020-01-28 17:39:53 -0500

    Update custom.py

--- dmoj/graders/custom.py
+++ dmoj/graders/custom.py
@@ -5,10 +5,10 @@ from dmoj.utils.module import load_module_from_file
 
 
 class CustomGrader:
-    def __init__(self, judge, problem, language, source, meta):
+    def __init__(self, judge, problem, language, source):
         self.judge = judge
         self.mod = load_module_from_file(os.path.join(get_problem_root(problem.id), problem.config['custom_judge']))
-        self._grader = self.mod.Grader(judge, problem, language, source, meta)
+        self._grader = self.mod.Grader(judge, problem, language, source)
 
     def __getattr__(self, item):
         return getattr(self._grader, item)

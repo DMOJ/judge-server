8c06fef01ea1924ca504da8ffe9718f14f68836e
Tudor Brindus <xyene@dmoj.ca>
2020-04-09 00:00:59 +0000

    OCAML: compile with JSC Base/Core/Stdio and Zarith

--- dmoj/executors/OCAML.py
+++ dmoj/executors/OCAML.py
@@ -1,16 +1,43 @@
 from dmoj.executors.compiled_executor import CompiledExecutor
 
 
+# We default to a "more-than-vanilla" OCaml. In particular, we compile with
+# Jane Street Base, Core, and Stdio packages, as well as Zarith. These need a
+# functioning opam + ocamlfind installation to work.
 class Executor(CompiledExecutor):
     ext = 'ml'
     name = 'OCAML'
-    command = 'ocaml'
-    command_paths = ['ocamlopt']
-    test_program = 'print_endline (input_line stdin)'
+    command = 'ocamlfind'
+    test_program = '''
+open! Base
+open! Core
+open! Stdio
+
+let () = (In_channel.iter_lines Stdio.stdin ~f:print_endline)
+'''
 
     def get_compile_args(self):
-        return [self.get_command(), 'str.cmxa', 'nums.cmxa', self._code, '-o', self.problem]
+        # fmt: off
+        return [
+            self.runtime_dict['ocamlfind'],
+            'opt',
+            '-package', 'str',
+            '-package', 'base',
+            '-package', 'core',
+            '-package', 'stdio',
+            '-package', 'zarith',
+            '-thread',
+            '-linkpkg',
+            self._code,
+            '-o',
+            self.problem,
+        ]
+        # fmt: on
 
     @classmethod
     def get_version_flags(cls, command):
-        return ['-version']
+        return ['opt', '-version']
+
+    @classmethod
+    def get_versionable_commands(cls):
+        return [('ocaml', cls.get_command())]

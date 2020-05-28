93c226d73497e54fb3c8d9db8f586f917fc26e3f
Tudor Brindus <me@tbrindus.ca>
2020-05-27 20:58:54 -0400

    Allow Groovy test failure

--- .docker.test.py
+++ .docker.test.py
@@ -7,7 +7,7 @@ from dmoj.executors import get_available
 
 arch = platform.machine()
 if arch == 'x86_64':
-    ALLOW_FAIL = {'GASARM', 'JAVA9', 'JAVA10', 'OBJC'}
+    ALLOW_FAIL = {'GASARM', 'JAVA9', 'JAVA10', 'OBJC', 'GROOVY'}
     EXECUTORS = get_available()
 elif arch == 'aarch64':
     EXECUTORS = {

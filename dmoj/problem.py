adafdfaf7dfcef765b79757847bbe5930365abd2
Tudor Brindus <xyene@dmoj.ca>
2020-08-26 00:30:05 -0400

    problem: use global points entry as default

--- dmoj/problem.py
+++ dmoj/problem.py
@@ -150,7 +150,7 @@ class Problem:
             self._problem_file_list(),
             re.compile(get_with_default('input_format', DEFAULT_TEST_CASE_INPUT_PATTERN), re.IGNORECASE),
             re.compile(get_with_default('output_format', DEFAULT_TEST_CASE_OUTPUT_PATTERN), re.IGNORECASE),
-            iter(get_with_default('case_points', itertools.repeat(1))),
+            iter(get_with_default('case_points', itertools.repeat(test_cases.points))),
         )
 
     def load_checker(self, name):

72dc72548bcd7b3c476d59fc214081dd44189b5c
Tudor Brindus <me@tbrindus.ca>
2020-10-02 22:11:51 -0400

    Remove `NullTracer` implementation
    It is no longer being used.

--- dmoj/cptbox/__init__.py
+++ dmoj/cptbox/__init__.py
@@ -1,12 +1,4 @@
-from collections import defaultdict
-
 from dmoj.cptbox.handlers import ALLOW, DISALLOW
 from dmoj.cptbox.isolate import IsolateTracer
 from dmoj.cptbox.syscalls import SYSCALL_COUNT
 from dmoj.cptbox.tracer import PIPE, TracedPopen
-
-
-class NullTracer(defaultdict):
-    def __init__(self):
-        for i in range(SYSCALL_COUNT):
-            self[i] = ALLOW

9707f2fad433a1507b9ed691c8554861fec7cdb4
Quantum <quantum2048@gmail.com>
2020-12-27 03:28:10 -0500

    Make patched_int hashable and enums work

--- dmoj/utils/builtin_int_patch.py
+++ dmoj/utils/builtin_int_patch.py
@@ -19,12 +19,15 @@ class patched_int_meta(type):
     def __eq__(self, other):
         return self is other or other is int_
 
+    def __hash__(self):
+        return hash(int_)
+
 
 class patched_int(int_, metaclass=patched_int_meta):
     def __new__(cls, s, *args, **kwargs):
         if isinstance(s, str) and len(s) > INT_MAX_NUMBER_DIGITS:
             raise ValueError('integer is too long')
-        return super().__new__(cls, s, *args, **kwargs)
+        return int_.__new__(cls, s, *args, **kwargs)
 
 
 def apply():

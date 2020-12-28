13371403203cddf68890abd9cbc66089ad23849e
Quantum <quantum2048@gmail.com>
2020-12-27 20:26:01 -0500

    Remove dmoj.utils.os_ext.ARCH_* constants
    Fixes #800

--- dmoj/cptbox/__init__.py
+++ dmoj/cptbox/__init__.py
@@ -1,4 +1,5 @@
+from dmoj.cptbox._cptbox import PTBOX_ABI_ARM, PTBOX_ABI_ARM64, PTBOX_ABI_X32, PTBOX_ABI_X64, PTBOX_ABI_X86
 from dmoj.cptbox.handlers import ALLOW, DISALLOW
 from dmoj.cptbox.isolate import IsolateTracer
 from dmoj.cptbox.syscalls import SYSCALL_COUNT
-from dmoj.cptbox.tracer import PIPE, TracedPopen
+from dmoj.cptbox.tracer import PIPE, TracedPopen, can_debug

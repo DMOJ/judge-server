781f6d2756d1ce4092bfcb691d627d1ffc01f201
Quantum <quantum2048@gmail.com>
2021-01-01 06:48:24 +0000

    Make generator_test_sandbox generator compile on FreeBSD

--- testsuite/generator_test_sandbox/generator.cpp
+++ testsuite/generator_test_sandbox/generator.cpp
@@ -4,7 +4,12 @@
 #include <sys/ptrace.h>
 
 int main() {
-    if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) == -1) {
+#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
+    if (ptrace(PT_TRACE_ME, 0, NULL, 0) == -1)
+#else
+    if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) == -1)
+#endif
+    {
         perror("ptrace");
     }
     return 0;

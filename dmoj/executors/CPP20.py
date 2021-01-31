214e8ef56d919413311a1053602ae7ecd968a9ba
Evan <evanzhang1028@hotmail.com>
2021-01-30 21:54:53 -0500

    Add support for C++20

---
+++ dmoj/executors/CPP20.py
@@ -0,0 +1,18 @@
+from dmoj.executors.CPP03 import Executor as CPPExecutor
+
+
+class Executor(CPPExecutor):
+    command = 'g++20'
+    command_paths = ['g++-11', 'g++']
+    std = 'c++20'
+    name = 'CPP20'
+    test_program = '''
+#include <iostream>
+
+int main() {
+    std::strong_ordering comparison = 1 <=> 2;
+    auto input = std::cin.rdbuf();
+    std::cout << input;
+    return 0;
+}
+'''

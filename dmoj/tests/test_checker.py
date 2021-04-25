9a825b8b7ee7f9a4aef0cf928fdf82ef4fc63e9a
Quantum <quantum2048@gmail.com>
2021-04-24 21:47:25 -0400

    Correctly uses unittest assert methods in checker tests

--- dmoj/tests/test_checker.py
+++ dmoj/tests/test_checker.py
@@ -4,7 +4,7 @@ from dmoj.result import CheckerResult
 
 
 class CheckerTest(unittest.TestCase):
-    def assert_pass(self, check, str1, str2, expect=True):
+    def assert_standard_pass(self, check, str1, str2, expect=True):
         if expect:
             message = 'expecting %r to equal %r'
         else:
@@ -12,71 +12,79 @@ class CheckerTest(unittest.TestCase):
         self.assertEqual(bool(check(str1, str2)), expect, message % (str1, str2))
         self.assertEqual(bool(check(str2, str1)), expect, message % (str2, str1))
 
-    def assert_fail(self, check, str1, str2):
-        self.assert_pass(check, str1, str2, expect=False)
+    def assert_standard_fail(self, check, str1, str2):
+        self.assert_standard_pass(check, str1, str2, expect=False)
 
     def test_standard(self):
         from dmoj.checkers.standard import check
 
-        self.assert_pass(check, b'a', b'a')
-        self.assert_pass(check, b'a b', b'a  b')
-        self.assert_pass(check, b'a b   \n', b'a b')
-        self.assert_pass(check, b'\n\na b \n    ', b'a b')
-        self.assert_pass(check, b'a\n\n\nb', b'a\nb')
-        self.assert_pass(check, b'  a   \n\n', b'\n\n\n  a   \n')
-        self.assert_pass(check, b'a ' * 1000, b' a' * 1000)
-        self.assert_pass(check, b'a\n' * 1000, b'\n\n\na' * 1000)
+        self.assert_standard_pass(check, b'a', b'a')
+        self.assert_standard_pass(check, b'a b', b'a  b')
+        self.assert_standard_pass(check, b'a b   \n', b'a b')
+        self.assert_standard_pass(check, b'\n\na b \n    ', b'a b')
+        self.assert_standard_pass(check, b'a\n\n\nb', b'a\nb')
+        self.assert_standard_pass(check, b'  a   \n\n', b'\n\n\n  a   \n')
+        self.assert_standard_pass(check, b'a ' * 1000, b' a' * 1000)
+        self.assert_standard_pass(check, b'a\n' * 1000, b'\n\n\na' * 1000)
 
-        self.assert_fail(check, b'a', b'b')
-        self.assert_fail(check, b'\n\n\na \n b\n', b'a b')
-        self.assert_fail(check, b'a\n\n\nb', b'a b')
-        self.assert_fail(check, b'ab', b'a b')
+        self.assert_standard_fail(check, b'a', b'b')
+        self.assert_standard_fail(check, b'\n\n\na \n b\n', b'a b')
+        self.assert_standard_fail(check, b'a\n\n\nb', b'a b')
+        self.assert_standard_fail(check, b'ab', b'a b')
 
         # Checkers should handle mixed bytes/str
-        self.assert_pass(check, b'a', u'a')
-        self.assert_fail(check, b'a', u'b')
+        self.assert_standard_pass(check, b'a', u'a')
+        self.assert_standard_fail(check, b'a', u'b')
+
+    def assert_wa_feedback(self, result, feedback):
+        self.assertIsInstance(result, CheckerResult)
+        self.assertFalse(result.passed)
+        self.assertEqual(result.feedback, feedback)
+
+    def assert_identical_pe(self, result):
+        self.assert_wa_feedback(result, feedback='Presentation Error, check your whitespace')
 
     def test_identical(self):
         from dmoj.checkers.identical import check
 
-        def is_pe(res, feedback='Presentation Error, check your whitespace'):
-            return res is not True and not res.passed and res.feedback == feedback
-
-        assert check(b'a\nb\nc', b'a\nb\nc')
-        assert check(b'a\nb\nc', b'a\nb\nc')
-        assert is_pe(check(b'a \nb\nc', b'a\nb\nc'))
-        assert is_pe(check(b'a\nb\nc', b'a\nb\nc\n'))
-        assert is_pe(check(b'a\nb\nc', b'a\nb\nc\n', pe_allowed=False), feedback=None)
+        self.assertTrue(check(b'a\nb\nc', b'a\nb\nc'))
+        self.assertTrue(check(b'a\nb\nc\n', b'a\nb\nc\n'))
+        self.assert_identical_pe(check(b'a \nb\nc', b'a\nb\nc'))
+        self.assert_identical_pe(check(b'a\nb\nc', b'a\nb\nc\n'))
+        self.assert_wa_feedback(check(b'a\nb\nc', b'a\nb\nc\n', pe_allowed=False), feedback=None)
 
     def test_sorted(self):
         from dmoj.checkers.sorted import check
 
-        assert not check(b'1 2 3', b'3 2 1')
-        assert check(b'1 2 3', b'3 2 1', split_on='whitespace')
-        assert not check(b'1 2 3', b'3 2 1', split_on='lines')
-        assert check(b'1 2 3', b'1 2 3')
-        assert not check(b'1 2 2', b'1 2 3')
-        assert not check(b'1 2', b'1')
-        assert not check(b'1\n2', b'1')
-        assert not check(b'12', b'1 2')
+        self.assertFalse(check(b'1 2 3', b'3 2 1'))
+        self.assertTrue(check(b'1 2 3', b'3 2 1', split_on='whitespace'))
+        self.assertFalse(check(b'1 2 3', b'3 2 1', split_on='lines'))
+        self.assertTrue(check(b'1 2 3', b'1 2 3'))
+        self.assertFalse(check(b'1 2 2', b'1 2 3'))
+        self.assertFalse(check(b'1 2', b'1'))
+        self.assertFalse(check(b'1\n2', b'1'))
+        self.assertFalse(check(b'12', b'1 2'))
 
-        assert check(b'1 2\n3', b'3\n1 2')
-        assert not check(b'1 2\n3', b'3\n2 1')
-        assert check(b'1 2\n3', b'3\n2 1', split_on='whitespace')
+        self.assertTrue(check(b'1 2\n3', b'3\n1 2'))
+        self.assertFalse(check(b'1 2\n3', b'3\n2 1'))
+        self.assertTrue(check(b'1 2\n3', b'3\n2 1', split_on='whitespace'))
+
+    def assert_partial(self, result, expected_points, passed):
+        self.assertIsInstance(result, CheckerResult)
+        self.assertEqual(result.points, expected_points)
+        self.assertEqual(result.passed, passed)
 
     def test_linematches(self):
         from dmoj.checkers.linematches import check
 
-        def validate(res, expected_points, passed):
-            return isinstance(res, CheckerResult) and res.points == expected_points and res.passed == passed
+        self.assertFalse(check(b'1', b'1\n2', point_distribution=[1, 1]))
+        self.assertFalse(check(b'1\n2', b'1'))
 
-        assert not check(b'1', b'1\n2', point_distribution=[1, 1])
-        assert not check(b'1\n2', b'1')
+        self.assert_partial(check(b'1', b'1\n2', filler_lines_required=False, point_distribution=[4, 6]),
+                            expected_points=0.4, passed=True)
+        self.assert_partial(check(b'1\n3', b'1\n2', point_distribution=[4, 6]), expected_points=0.4, passed=True)
 
-        assert validate(check(b'1', b'1\n2', filler_lines_required=False, point_distribution=[4, 6]), 0.4, True)
-        assert validate(check(b'1\n3', b'1\n2', point_distribution=[4, 6]), 0.4, True)
+        self.assert_partial(check(b'3\n2', b'1\n2', point_distribution=[4, 6]), expected_points=0.6, passed=True)
+        self.assert_partial(check(b'1\n2', b'1\n2', point_distribution=[4, 6]), expected_points=1, passed=True)
 
-        assert validate(check(b'3\n2', b'1\n2', point_distribution=[4, 6]), 0.6, True)
-        assert validate(check(b'1\n2', b'1\n2', point_distribution=[4, 6]), 1, True)
-
-        assert validate(check(b'1', b'2', point_distribution=[1]), 0, False)
+        self.assert_partial(check(b'1', b'2', point_distribution=[1]), expected_points=0, passed=False)

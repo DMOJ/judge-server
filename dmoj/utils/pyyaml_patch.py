25050e69ebdf3d3f55a6ff0fa3baa91b577a824c
Quantum <quantum2048@gmail.com>
2021-09-07 05:18:41 -0400

    utils: sys.maxunicode > 65535 is always true on Python 3.3+

--- dmoj/utils/pyyaml_patch.py
+++ dmoj/utils/pyyaml_patch.py
@@ -1,9 +1,8 @@
 import re
-import sys
 
 import yaml.reader
 
-if yaml.reader.Reader.NON_PRINTABLE.pattern == '[^\t\n\r -~\x85\xa0-\ud7ff\ue000-\ufffd]' and sys.maxunicode > 65535:
+if yaml.reader.Reader.NON_PRINTABLE.pattern == '[^\t\n\r -~\x85\xa0-\ud7ff\ue000-\ufffd]':
     yaml.reader.Reader.NON_PRINTABLE = re.compile(
         '[^\x09\x0A\x0D\x20-\x7E\x85\xA0-\uD7FF\uE000-\uFFFD\U00010000-\U0010FFFF]'
     )

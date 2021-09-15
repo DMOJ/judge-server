45fe294d35056596f0abee850b7c1d731d245ab4
Quantum <quantum2048@gmail.com>
2021-09-15 01:29:03 -0400

    executors: make Zig executor work with 0.8.1

--- dmoj/executors/ZIG.py
+++ dmoj/executors/ZIG.py
@@ -11,13 +11,13 @@ const std = @import("std");
 
 pub fn main() !void {
     const io = std.io;
-    const stdin = std.io.getStdIn().inStream();
-    const stdout = std.io.getStdOut().outStream();
+    const stdin = std.io.getStdIn().reader();
+    const stdout = std.io.getStdOut().writer();
 
     var line_buf: [50]u8 = undefined;
     while (try stdin.readUntilDelimiterOrEof(&line_buf, '\n')) |line| {
         if (line.len == 0) break;
-        try stdout.print("{}", .{line});
+        try stdout.print("{s}", .{line});
     }
 }"""
 
@@ -28,7 +28,7 @@ pub fn main() !void {
         super().create_files(problem_id, source_code, *args, **kwargs)
 
     def get_compile_args(self):
-        return [self.get_command(), 'build-exe', self._code, '--release-safe', '--name', self.problem]
+        return [self.get_command(), 'build-exe', self._code, '-O', 'ReleaseSafe', '--name', self.problem]
 
     @classmethod
     def get_version_flags(cls, command):

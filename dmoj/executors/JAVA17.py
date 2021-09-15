6dd3f9517a41571eb3340f5959cce2bd82cc4d10
Tudor Brindus <me@tbrindus.ca>
2021-09-14 21:26:50 -0400

    executors: add support for Java 17

---
+++ dmoj/executors/JAVA17.py
@@ -0,0 +1,37 @@
+from dmoj.executors.java_executor import JavacExecutor
+
+
+class Executor(JavacExecutor):
+    compiler = 'javac17'
+    vm = 'java17'
+    name = 'JAVA17'
+    jvm_regex = r'java-17-|openjdk17'
+
+    test_program = '''\
+import java.io.IOException;
+
+sealed interface IORunnableInterface permits IORunnable {
+    public void run() throws IOException;
+}
+
+final class IORunnable implements IORunnableInterface {
+    public void run() throws IOException {
+        System.out.print("""
+
+""".strip());
+
+        var buffer = new byte[4096];
+        int read;
+        while ((read = System.in.read(buffer)) >= 0)
+            System.out.write(buffer, 0, read);
+    }
+}
+
+public class self_test {
+    public static void main(String[] args) throws IOException {
+        new IORunnable().run();
+    }
+}'''
+
+    def get_compile_args(self):
+        return [self.get_compiler(), '-encoding', 'UTF-8', self._code]

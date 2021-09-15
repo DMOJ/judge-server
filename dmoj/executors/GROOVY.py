1fcf0343c8630c59824ca850358978a07ea04bfc
Tudor Brindus <me@tbrindus.ca>
2021-09-14 21:16:09 -0400

    executors: drop security policies for JVM runtimes
    They are deprecated as of Java 17.
    
    Ref #912.

--- dmoj/executors/GROOVY.py
+++ dmoj/executors/GROOVY.py
@@ -4,9 +4,6 @@ import subprocess
 from dmoj.executors.java_executor import JavaExecutor
 from dmoj.utils.unicode import utf8text
 
-with open(os.path.join(os.path.dirname(__file__), 'groovy-security.policy')) as policy_file:
-    policy = policy_file.read()
-
 
 class Executor(JavaExecutor):
     name = 'GROOVY'
@@ -14,7 +11,6 @@ class Executor(JavaExecutor):
 
     compiler = 'groovyc'
     vm = 'groovy_vm'
-    security_policy = policy
 
     test_program = """\
 println System.in.newReader().readLine()

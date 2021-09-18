8a9819b34ad12874163d0ac8748943ffaf32a300
Quantum <quantum2048@gmail.com>
2021-09-18 18:44:13 -0400

    executors: add types to ClangExecutor

--- dmoj/executors/clang_executor.py
+++ dmoj/executors/clang_executor.py
@@ -1,12 +1,14 @@
+from typing import List
+
 from .gcc_executor import GCCExecutor, MAX_ERRORS
 
 
 class ClangExecutor(GCCExecutor):
     arch = 'clang_target_arch'
 
-    def get_flags(self):
-        return self.flags + ['-ferror-limit=%d' % MAX_ERRORS]
+    def get_flags(self) -> List[str]:
+        return self.flags + [f'-ferror-limit={MAX_ERRORS}']
 
     @classmethod
-    def get_version_flags(cls, command):
+    def get_version_flags(cls, command: str) -> List[str]:
         return ['--version']

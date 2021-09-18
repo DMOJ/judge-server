02eab9baab9ae4932d9ba0d8376834d079e88ad2
Quantum <quantum2048@gmail.com>
2021-09-18 17:03:30 -0400

    utils: use signal.strsignal over ctypes when possible

--- dmoj/utils/os_ext.py
+++ dmoj/utils/os_ext.py
@@ -18,20 +18,30 @@ def oom_score_adj(score: int, to: Optional[int] = None) -> None:
         f.write(utf8bytes(str(score)))
 
 
-def strsignal(signo: int) -> str:
-    # in large part from http://code.activestate.com/recipes/578899-strsignal/
-    libc = ctypes.CDLL(ctypes.util.find_library('c'))
-    strsignal_c = ctypes.CFUNCTYPE(ctypes.c_char_p, ctypes.c_int)(('strsignal', libc), ((1,),))
-    NSIG = signal.NSIG
+try:
+    from signal import strsignal as _strsignal
+except ImportError:  # before Python 3.8
 
-    # The behavior of the C library strsignal() is unspecified if
-    # called with an out-of-range argument.  Range-check on entry
-    # _and_ NULL-check on exit.
-    if 0 <= signo < NSIG:
-        s = strsignal_c(signo)
-        if s:
-            return s.decode('utf-8')
-    return 'Unknown signal %d' % signo
+    def strsignal(signo: int) -> str:
+        # in large part from http://code.activestate.com/recipes/578899-strsignal/
+        libc = ctypes.CDLL(ctypes.util.find_library('c'))
+        strsignal_c = ctypes.CFUNCTYPE(ctypes.c_char_p, ctypes.c_int)(('strsignal', libc), ((1,),))
+        NSIG = signal.NSIG
+
+        # The behavior of the C library strsignal() is unspecified if
+        # called with an out-of-range argument.  Range-check on entry
+        # _and_ NULL-check on exit.
+        if 0 <= signo < NSIG:
+            s = strsignal_c(signo)
+            if s:
+                return s.decode('utf-8')
+        return f'Unknown signal {signo}'
+
+
+else:
+
+    def strsignal(signo: int) -> str:
+        return _strsignal(signo) or f'Unknown signal {signo}'
 
 
 def bool_env(name: str) -> bool:

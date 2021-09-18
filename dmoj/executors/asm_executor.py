a2f1f36523c9b49c58f795d367febdce3cd431b3
Quantum <quantum2048@gmail.com>
2021-09-18 18:33:02 -0400

    executors: fix types for assembly executor classes

--- dmoj/executors/asm_executor.py
+++ dmoj/executors/asm_executor.py
@@ -1,11 +1,12 @@
 import os
 import re
-from typing import List, Optional
+from typing import Any, Dict, List, Optional, Set, Tuple
 
 from dmoj.cptbox import PTBOX_ABI_X64, PTBOX_ABI_X86, can_debug
+from dmoj.cptbox.filesystem_policies import ExactFile, FilesystemAccessRule, RecursiveDir
 from dmoj.executors.compiled_executor import CompiledExecutor
 from dmoj.judgeenv import env, skip_self_test
-from dmoj.utils.unicode import utf8text
+from dmoj.utils.unicode import utf8bytes, utf8text
 
 refeatures = re.compile(r'^[#;@|!]\s*features:\s*([\w\s,]+)', re.M)
 feature_split = re.compile(r'[\s,]+').split
@@ -17,7 +18,7 @@ class ASMExecutor(CompiledExecutor):
     as_name: str
     ld_name: str
     qemu_path: Optional[str] = None
-    dynamic_linker: Optional[str] = None
+    dynamic_linker: str
     crt_pre: List[str]
     crt_post: List[str]
     platform_prefixes: Optional[List[str]]
@@ -25,36 +26,36 @@ class ASMExecutor(CompiledExecutor):
     name = 'ASM'
     ext = 'asm'
 
-    def __init__(self, problem_id, source_code, *args, **kwargs):
+    def __init__(self, problem_id: str, source_code: bytes, *args, **kwargs) -> None:
         self.use_qemu = self.qemu_path is not None and os.path.isfile(self.qemu_path)
         self.features = self.find_features(source_code)
 
         super().__init__(problem_id, source_code + b'\n', *args, **kwargs)
 
-    def find_features(self, source_code):
+    def find_features(self, source_code: bytes) -> Set[str]:
         features = refeatures.search(utf8text(source_code))
         if features is not None:
             return set(filter(None, feature_split(features.group(1))))
         return set()
 
     @classmethod
-    def get_as_path(cls):
+    def get_as_path(cls) -> str:
         return cls.runtime_dict.get(cls.as_name)
 
     @classmethod
-    def get_ld_path(cls):
+    def get_ld_path(cls) -> str:
         return cls.runtime_dict.get(cls.ld_name)
 
-    def get_as_args(self, object):
+    def get_as_args(self, obj_file: str) -> List[str]:
         raise NotImplementedError()
 
-    def assemble(self):
-        object = self._file('%s.o' % self.problem)
-        process = self.create_compile_process(self.get_as_args(object))
+    def assemble(self) -> Tuple[bytes, List[str]]:
+        obj_file = self._file(f'{self.problem}.o')
+        process = self.create_compile_process(self.get_as_args(obj_file))
         as_output = self.get_compile_output(process)
-        return as_output, [object]
+        return as_output, [obj_file]
 
-    def compile(self):
+    def compile(self) -> str:
         as_output, to_link = self.assemble()
 
         if 'libc' in self.features:
@@ -65,41 +66,49 @@ class ASMExecutor(CompiledExecutor):
         ld_output = self.get_compile_output(process)
 
         if as_output or ld_output:
-            self.warning = ('%s\n%s' % (utf8text(as_output), utf8text(ld_output))).strip()
+            self.warning = utf8bytes(f'{utf8text(as_output)}\n{utf8text(ld_output)}').strip()
         self._executable = executable
         return executable
 
-    def get_cmdline(self, **kwargs):
+    def get_cmdline(self, **kwargs) -> List[str]:
         if self.use_qemu:
+            assert self.qemu_path is not None
+            assert self._executable is not None
             return [self.qemu_path, self._executable]
         return super().get_cmdline()
 
-    def get_executable(self):
+    def get_executable(self) -> str:
         if self.use_qemu:
+            assert self.qemu_path is not None
             return self.qemu_path
         return super().get_executable()
 
-    def get_fs(self):
+    def get_fs(self) -> List[FilesystemAccessRule]:
         fs = super().get_fs()
         if self.use_qemu:
-            fs += ['/proc/sys/vm/mmap_min_addr$', '/etc/qemu-binfmt/', self._executable]
+            assert self._executable is not None
+            fs += [
+                ExactFile('/proc/sys/vm/mmap_min_addr'),
+                RecursiveDir('/etc/qemu-binfmt'),
+                ExactFile(self._executable),
+            ]
         return fs
 
-    def get_address_grace(self):
+    def get_address_grace(self) -> int:
         grace = super().get_address_grace()
         if self.use_qemu:
             grace += 65536
         return grace
 
     @classmethod
-    def initialize(cls):
+    def initialize(cls) -> bool:
         if cls.qemu_path is None and not can_debug(cls.abi):
             return False
         if any(
             i is None for i in (cls.get_as_path(), cls.get_ld_path(), cls.dynamic_linker, cls.crt_pre, cls.crt_post)
         ):
             return False
-        if any(not os.path.isfile(i) for i in (cls.get_as_path(), cls.get_ld_path(), cls.dynamic_linker)):
+        if any(not i or not os.path.isfile(i) for i in (cls.get_as_path(), cls.get_ld_path(), cls.dynamic_linker)):
             return False
         if any(not os.path.isfile(i) for i in cls.crt_pre) or any(not os.path.isfile(i) for i in cls.crt_post):
             return False
@@ -108,14 +117,13 @@ class ASMExecutor(CompiledExecutor):
         return skip_self_test or cls.run_self_test()
 
     @classmethod
-    def get_versionable_commands(cls):
-        for runtime in (cls.as_name, cls.ld_name):
-            yield runtime, cls.runtime_dict[runtime]
+    def get_versionable_commands(cls) -> List[Tuple[str, str]]:
+        return [(runtime, cls.runtime_dict[runtime]) for runtime in (cls.as_name, cls.ld_name)]
 
     @classmethod
-    def autoconfig(cls):
+    def autoconfig(cls) -> Tuple[Optional[Dict[str, Any]], bool, str, str]:
         if not can_debug(cls.abi):
-            return {}, False, 'Unable to natively debug'
+            return {}, False, 'Unable to natively debug', ''
         return super().autoconfig()
 
 
@@ -123,19 +131,20 @@ class GASExecutor(ASMExecutor):
     name = 'GAS'
     as_platform_flag: str
 
-    def get_as_args(self, object):
-        as_args = [self.get_as_path(), '-o', object, self._code]
+    def get_as_args(self, obj_file: str) -> List[str]:
+        assert self._code is not None
+        as_args = [self.get_as_path(), '-o', obj_file, self._code]
         if os.path.basename(self.get_as_path()) == 'as' and getattr(self, 'as_platform_flag', None):
             as_args += [self.as_platform_flag]
         return as_args
 
     @classmethod
-    def get_find_first_mapping(cls):
+    def get_find_first_mapping(cls) -> Optional[Dict[str, List[str]]]:
         if cls.platform_prefixes is None:
             return None
         return {
-            cls.as_name: ['%s-as' % i for i in cls.platform_prefixes] + ['as'],
-            cls.ld_name: ['%s-ld' % i for i in cls.platform_prefixes] + ['ld'],
+            cls.as_name: [f'{i}-as' for i in cls.platform_prefixes] + ['as'],
+            cls.ld_name: [f'{i}-ld' for i in cls.platform_prefixes] + ['ld'],
         }
 
 
@@ -144,24 +153,25 @@ class NASMExecutor(ASMExecutor):
     as_name = 'nasm'
     nasm_format: str
 
-    def find_features(self, source_code):
+    def find_features(self, source_code: bytes) -> Set[str]:
         features = super().find_features(source_code)
         if source_code.startswith(b'; libc'):
             features.add('libc')
         return features
 
-    def get_as_args(self, object):
-        return [self.get_as_path(), '-f', self.nasm_format, self._code, '-o', object]
+    def get_as_args(self, obj_file: str) -> List[str]:
+        assert self._code is not None
+        return [self.get_as_path(), '-f', self.nasm_format, self._code, '-o', obj_file]
 
     @classmethod
-    def get_version_flags(cls, command):
+    def get_version_flags(cls, command: str) -> List[str]:
         return ['-version'] if command == cls.as_name else super().get_version_flags(command)
 
     @classmethod
-    def get_find_first_mapping(cls):
+    def get_find_first_mapping(cls) -> Optional[Dict[str, List[str]]]:
         if cls.platform_prefixes is None:
             return None
-        return {cls.ld_name: ['%s-ld' % i for i in cls.platform_prefixes] + ['ld'], 'nasm': ['nasm']}
+        return {cls.ld_name: [f'{i}-ld' for i in cls.platform_prefixes] + ['ld'], 'nasm': ['nasm']}
 
 
 class PlatformX86Mixin(ASMExecutor):

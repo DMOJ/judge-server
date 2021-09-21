200f3eecbb3a0ccd8e42b60abc7f97ca60c18327
Quantum <quantum2048@gmail.com>
2021-09-21 05:41:18 -0400

    ci: expand set of ARM64 executors tested

--- .docker.test.py
+++ .docker.test.py
@@ -6,29 +6,12 @@ from dmoj.citest import ci_test
 from dmoj.executors import get_available
 
 arch = platform.machine()
-if arch == 'x86_64':
-    ALLOW_FAIL = {'GASARM', 'JAVA9', 'JAVA10', 'OBJC', 'GROOVY'}
-    EXECUTORS = get_available()
-elif arch == 'aarch64':
-    EXECUTORS = {
-        'AWK',
-        'BF',
-        'C',
-        'C11',
-        'CPP03',
-        'CPP11',
-        'CPP14',
-        'CPP17',
-        'JAVA8',
-        'PAS',
-        'PERL',
-        'PY2',
-        'PY3',
-        'SED',
-        'TEXT',
-    }
-    ALLOW_FAIL = set()
-else:
+ALLOW_FAIL = {'GASARM', 'JAVA9', 'JAVA10', 'OBJC', 'GROOVY'}
+EXECUTORS = get_available()
+
+if arch == 'aarch64':
+    ALLOW_FAIL |= {'D', 'GAS32', 'GAS64', 'NASM', 'NASM64', 'SWIFT', 'TUR'}
+elif arch != 'x86_64':
     raise AssertionError('invalid architecture')
 
 OVERRIDES = {}

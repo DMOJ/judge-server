744b0523d4989c9b708b73d6959ddef8f3221dbf
Quantum <quantum2048@gmail.com>
2021-09-22 03:55:39 -0400

    executors: add types to Turing executor

--- dmoj/executors/TUR.py
+++ dmoj/executors/TUR.py
@@ -1,6 +1,7 @@
 import os
+from typing import Dict, List
 
-from dmoj.cptbox.filesystem_policies import ExactFile
+from dmoj.cptbox.filesystem_policies import ExactFile, FilesystemAccessRule
 from dmoj.executors.compiled_executor import CompiledExecutor
 from dmoj.judgeenv import env
 
@@ -14,25 +15,30 @@ get echo : *
 put echo
 """
 
-    def get_fs(self):
+    def get_fs(self) -> List[FilesystemAccessRule]:
+        assert self._code is not None
         return super().get_fs() + [ExactFile(self._code + 'bc')]
 
-    def get_compile_args(self):
+    def get_compile_args(self) -> List[str]:
         tprologc = self.runtime_dict['tprologc']
+        assert self._code is not None
         return [tprologc, self._code, os.path.dirname(tprologc)]
 
-    def get_cmdline(self, **kwargs):
+    def get_cmdline(self, **kwargs) -> List[str]:
+        assert self._code is not None
         return [self.runtime_dict['tprolog'], self._code + 'bc']
 
-    def get_executable(self):
-        return self.get_command()
+    def get_executable(self) -> str:
+        command = self.get_command()
+        assert command is not None
+        return command
 
     @classmethod
-    def initialize(cls):
+    def initialize(cls) -> bool:
         if 'tprolog' not in env['runtime'] or 'tprologc' not in env['runtime']:
             return False
         return super().initialize()
 
     @classmethod
-    def get_find_first_mapping(cls):
+    def get_find_first_mapping(cls) -> Dict[str, List[str]]:
         return {'tprolog': ['tprolog'], 'tprologc': ['tprologc']}

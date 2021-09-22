8dbae08d3a5bd0cd528780c7c8c69c89bbc8e6eb
Quantum <quantum2048@gmail.com>
2021-09-22 03:28:45 -0400

    executors: add types to Objective C executor

--- dmoj/executors/OBJC.py
+++ dmoj/executors/OBJC.py
@@ -2,8 +2,9 @@ import os
 from subprocess import CalledProcessError, check_output
 from typing import List
 
-from dmoj.cptbox.filesystem_policies import ExactFile
+from dmoj.cptbox.filesystem_policies import ExactFile, FilesystemAccessRule
 from dmoj.judgeenv import env
+from dmoj.utils.unicode import utf8text
 from .gcc_executor import GCCExecutor
 
 
@@ -27,22 +28,22 @@ int main (int argc, const char * argv[]) {
 }
 """
 
-    def get_flags(self):
+    def get_flags(self) -> List[str]:
         return self.objc_flags + super().get_flags()
 
-    def get_ldflags(self):
+    def get_ldflags(self) -> List[str]:
         return self.objc_ldflags + super().get_ldflags()
 
-    def get_fs(self):
+    def get_fs(self) -> List[FilesystemAccessRule]:
         return super().get_fs() + [ExactFile('/proc/self/cmdline')]
 
     @classmethod
-    def initialize(cls):
+    def initialize(cls) -> bool:
         if 'gnustep-config' not in env['runtime'] or not os.path.isfile(env['runtime']['gnustep-config']):
             return False
         try:
-            cls.objc_flags = check_output([env['runtime']['gnustep-config'], '--objc-flags']).split()
-            cls.objc_ldflags = check_output([env['runtime']['gnustep-config'], '--base-libs']).split()
+            cls.objc_flags = utf8text(check_output([env['runtime']['gnustep-config'], '--objc-flags'])).split()
+            cls.objc_ldflags = utf8text(check_output([env['runtime']['gnustep-config'], '--base-libs'])).split()
         except CalledProcessError:
             return False
         return super().initialize()

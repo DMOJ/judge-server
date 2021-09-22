4b505a33e9c76c1ca23aec2ce76fae0765667ecc
Quantum <quantum2048@gmail.com>
2021-09-22 03:49:19 -0400

    executors: add types to Scheme executor

--- dmoj/executors/SCM.py
+++ dmoj/executors/SCM.py
@@ -1,3 +1,5 @@
+from typing import List, Tuple
+
 from dmoj.cptbox.filesystem_policies import RecursiveDir
 from dmoj.executors.compiled_executor import CompiledExecutor
 
@@ -12,13 +14,18 @@ class Executor(CompiledExecutor):
     ]
     test_program = '(import chicken.io) (map print (read-lines))'
 
-    def get_compile_args(self):
-        return [self.get_command(), self._code]
+    def get_compile_args(self) -> List[str]:
+        command = self.get_command()
+        assert command is not None
+        assert self._code is not None
+        return [command, self._code]
 
     @classmethod
-    def get_versionable_commands(cls):
-        return (('csc', cls.get_command()),)
+    def get_versionable_commands(cls) -> List[Tuple[str, str]]:
+        command = cls.get_command()
+        assert command is not None
+        return [('csc', command)]
 
     @classmethod
-    def get_version_flags(cls, command):
+    def get_version_flags(cls, command: str) -> List[str]:
         return ['-version']

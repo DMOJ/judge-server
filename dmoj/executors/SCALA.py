52f337d365930a3a13ea73af0e45a2567f2cc4cf
Quantum <quantum2048@gmail.com>
2021-09-22 03:47:01 -0400

    executors: add types to Scala executor

--- dmoj/executors/SCALA.py
+++ dmoj/executors/SCALA.py
@@ -1,7 +1,9 @@
 import os
 import subprocess
+from typing import List, Tuple
 
 from dmoj.cptbox.filesystem_policies import ExactFile, RecursiveDir
+from dmoj.executors.base_executor import AutoConfigOutput, AutoConfigResult
 from dmoj.executors.java_executor import JavaExecutor
 from dmoj.utils.unicode import utf8text
 
@@ -28,11 +30,11 @@ object self_test extends App {
 }
 """
 
-    def create_files(self, problem_id, source_code, *args, **kwargs):
+    def create_files(self, problem_id: str, source_code: bytes, *args, **kwargs) -> None:
         super().create_files(problem_id, source_code, *args, **kwargs)
         self._class_name = problem_id
 
-    def get_cmdline(self, **kwargs):
+    def get_cmdline(self, **kwargs) -> List[str]:
         res = super().get_cmdline(**kwargs)
 
         # Simply run bash -x $(which scala) and copy all arguments after -Xmx and -Xms
@@ -41,20 +43,27 @@ object self_test extends App {
         return res
 
     def get_compile_args(self):
-        return [self.get_compiler(), self._code]
+        compiler = self.get_compiler()
+        assert compiler is not None
+        assert self._code is not None
+        return [compiler, self._code]
 
     @classmethod
-    def get_versionable_commands(cls):
-        return [('scalac', cls.get_compiler()), ('java', cls.get_vm())]
+    def get_versionable_commands(cls) -> List[Tuple[str, str]]:
+        compiler = cls.get_compiler()
+        vm = cls.get_vm()
+        assert compiler is not None
+        assert vm is not None
+        return [('scalac', compiler), ('java', vm)]
 
     @classmethod
-    def autoconfig(cls):
-        result = {}
+    def autoconfig(cls) -> AutoConfigOutput:
+        result: AutoConfigResult = {}
 
         for key, files in {'scalac': ['scalac'], 'scala': ['scala']}.items():
             file = cls.find_command_from_list(files)
             if file is None:
-                return result, False, 'Failed to find "%s"' % key
+                return result, False, f'Failed to find "{key}"', ''
             result[key] = file
 
         scala = result.pop('scala')
@@ -66,13 +75,17 @@ object self_test extends App {
         log = [i for i in output.split('\n') if 'scala.tools.nsc.MainGenericRunner' in i]
 
         if not log:
-            return result, False, 'Failed to parse: %s' % scala
+            return result, False, f'Failed to parse: {scala}', ''
 
         cmdline = log[-1].lstrip('+ ').split()
-        result['scala_vm'] = cls.unravel_java(cls.find_command_from_list([cmdline[0]]))
+        vm = cls.find_command_from_list([cmdline[0]])
+        if not vm:
+            return result, False, f'Failed to find: {cmdline[0]}', ''
+
+        result['scala_vm'] = cls.unravel_java(vm)
         result['scala_args'] = [i for i in cmdline[1:-1] if not i.startswith(('-Xmx', '-Xms'))]
 
         data = cls.autoconfig_run_test(result)
         if data[1]:
-            data = data[:2] + ('Using %s' % scala,) + data[3:]
+            data = data[:2] + (f'Using {scala}',) + data[3:]
         return data

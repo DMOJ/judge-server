6254e0cdd9083833d50eb67f165f24cf6a236290
Quantum <quantum2048@gmail.com>
2021-09-22 04:00:45 -0400

    executors: detect newer versions of Python 3

--- dmoj/executors/PY3.py
+++ dmoj/executors/PY3.py
@@ -3,6 +3,6 @@ from dmoj.executors.python_executor import PythonExecutor
 
 class Executor(PythonExecutor):
     command = 'python3'
-    command_paths = ['python%s' % i for i in ['3.6', '3.5', '3.4', '3.3', '3.2', '3.1', '3']]
+    command_paths = [f'python3.{i}' for i in reversed(range(1, 10))] + ['python3']
     test_program = "print(__import__('sys').stdin.read(), end='')"
     name = 'PY3'

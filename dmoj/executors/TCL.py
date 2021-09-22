3f343ceeb7c6461dbe5e54d4f60129b7b4769157
Quantum <quantum2048@gmail.com>
2021-09-22 03:52:45 -0400

    executors: add types to Tcl executor

--- dmoj/executors/TCL.py
+++ dmoj/executors/TCL.py
@@ -1,5 +1,6 @@
 import subprocess
 
+from dmoj.executors.base_executor import RuntimeVersionList
 from dmoj.executors.script_executor import ScriptExecutor
 
 
@@ -15,14 +16,18 @@ puts $input
 """
 
     @classmethod
-    def get_runtime_versions(cls):
+    def get_runtime_versions(cls) -> RuntimeVersionList:
         # TCL is dangerous to fetch versions for, since some TCL versions ignore the --version flag and instead go
         # straight into the interpreter. Since version processes are ran without time limit, this is pretty bad since
         # it can hang the startup process. TCL versions without --version can't be reliably detected either, since
         # they also don't have --help.
         # Here, we just use subprocess to print the TCL version, and use that.
-        process = subprocess.Popen([cls.get_command()], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        command = cls.get_command()
+        assert command is not None
+        process = subprocess.Popen([command], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        assert process.stdin is not None
+        assert process.stdout is not None
         process.stdin.write(b'puts $tcl_version\n')
         process.stdin.close()
-        retcode = process.poll()
-        return (('tclsh', tuple(map(int, process.stdout.read().split(b'.'))) if not retcode else ()),)
+        retcode = process.wait()
+        return [('tclsh', tuple(map(int, process.stdout.read().split(b'.'))) if not retcode else ())]

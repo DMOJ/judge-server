2e8d45fb586a8ae7368fea39af1ff7e74a0e8a41
Quantum <quantum2048@gmail.com>
2021-09-22 18:56:41 -0400

    graders: add types to InteractiveGrader

--- dmoj/graders/interactive.py
+++ dmoj/graders/interactive.py
@@ -1,5 +1,10 @@
+from typing import Any, AnyStr, Optional
+
+from dmoj.checkers import CheckerOutput
+from dmoj.cptbox import TracedPopen
 from dmoj.graders.standard import StandardGrader
-from dmoj.result import CheckerResult
+from dmoj.problem import TestCase
+from dmoj.result import CheckerResult, Result
 from dmoj.utils.unicode import utf8bytes, utf8text
 
 
@@ -12,23 +17,27 @@ MAX_NUMBER_DIGITS = 10000
 
 
 class Interactor:
-    def __init__(self, process):
+    _tokens: Optional[bytes]
+
+    def __init__(self, process: TracedPopen) -> None:
         self.process = process
         self._tokens = None
 
-    def _abbreviate(self, s, n=5):
-        s = utf8text(s)
-        if len(s) > n:
-            return s[:n] + '...'
-        return s
+    def _abbreviate(self, s: AnyStr, n: int = 5) -> str:
+        text = utf8text(s)
+        if len(text) > n:
+            return text[:n] + '...'
+        return text
 
-    def read(self):
+    def read(self) -> bytes:
+        assert self.process.stdout is not None
         ret = self.process.stdout.read()
         if ret == EOF:
             raise IOError('child stream closed')
         return ret
 
-    def readln(self, strip_newline=True):
+    def readln(self, strip_newline: bool = True) -> bytes:
+        assert self.process.stdout is not None
         ret = self.process.stdout.readline()
         if ret == EOF:
             raise IOError('child stream closed')
@@ -36,7 +45,7 @@ class Interactor:
             ret = ret.rstrip()
         return ret
 
-    def readtoken(self, delim=None):
+    def readtoken(self, delim: Optional[bytes] = None) -> bytes:
         if not self._tokens:
             self._tokens = self.readln()
         try:
@@ -46,7 +55,7 @@ class Interactor:
             self._tokens = None
         return ret
 
-    def readint(self, lo=float('-inf'), hi=float('inf'), delim=None):
+    def readint(self, lo: float = float('-inf'), hi: float = float('inf'), delim: Optional[bytes] = None) -> int:
         token = self.readtoken(delim)
 
         if len(token) > MAX_NUMBER_DIGITS:
@@ -62,7 +71,7 @@ class Interactor:
 
         return ret
 
-    def readfloat(self, lo=float('-inf'), hi=float('inf'), delim=None):
+    def readfloat(self, lo: float = float('-inf'), hi: float = float('inf'), delim: Optional[bytes] = None) -> float:
         token = self.readtoken(delim)
 
         if len(token) > MAX_NUMBER_DIGITS:
@@ -78,20 +87,28 @@ class Interactor:
 
         return ret
 
-    def write(self, val):
+    def write(self, val: Any) -> None:
+        assert self.process.stdin is not None
         self.process.stdin.write(utf8bytes(str(val)))
         self.process.stdin.flush()
 
-    def writeln(self, val):
+    def writeln(self, val: Any) -> None:
+        assert self.process.stdin is not None
         self.process.stdin.write(utf8bytes(str(val) + '\n'))
         self.process.stdin.flush()
 
-    def close(self):
+    def close(self) -> None:
+        assert self.process.stdin is not None
         self.process.stdin.close()
 
 
 class InteractiveGrader(StandardGrader):
-    def _interact_with_process(self, case, result, input):
+    check: CheckerOutput
+
+    def _interact_with_process(self, case: TestCase, result: Result, input: bytes) -> bytes:
+        assert self._current_proc is not None
+        assert self._current_proc.stderr is not None
+
         interactor = Interactor(self._current_proc)
         self.check = False
         self.feedback = None
@@ -106,7 +123,7 @@ class InteractiveGrader(StandardGrader):
         self._current_proc.wait()
         return self._current_proc.stderr.read()
 
-    def check_result(self, case, result):
+    def check_result(self, case: TestCase, result: Result) -> CheckerOutput:
         if result.result_flag:
             # This is usually because of a TLE verdict caused by printing stuff after the interactor
             # has issued the AC verdict
@@ -116,5 +133,5 @@ class InteractiveGrader(StandardGrader):
             return CheckerResult(self.check, case.points if self.check else 0.0, feedback=self.feedback)
         return self.check
 
-    def interact(self, case, interactor):
+    def interact(self, case: TestCase, interactor: Interactor) -> CheckerOutput:
         raise NotImplementedError

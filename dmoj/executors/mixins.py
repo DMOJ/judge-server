1056c6acd14f30c1517830a31c73a25049e5cd4e
Quantum <quantum2048@gmail.com>
2021-09-22 03:24:09 -0400

    executors: add types to executor mixins

--- dmoj/executors/mixins.py
+++ dmoj/executors/mixins.py
@@ -1,8 +1,13 @@
 import os
 import re
+from abc import ABCMeta
+from typing import Any, Dict
+
+from dmoj.executors.base_executor import BaseExecutor
+from dmoj.executors.compiled_executor import CompiledExecutor
 
 
-class NullStdoutMixin:
+class NullStdoutMixin(CompiledExecutor, metaclass=ABCMeta):
     """
     Some compilers print a lot of debug info to stdout even with successful compiles. This mixin pipes that generally-
     useless data into os.devnull so that the user never sees it.
@@ -12,16 +17,16 @@ class NullStdoutMixin:
         self._devnull = open(os.devnull, 'w')
         super().__init__(*args, **kwargs)
 
-    def cleanup(self):
+    def cleanup(self) -> None:
         if hasattr(self, '_devnull'):
             self._devnull.close()
         super().cleanup()
 
-    def get_compile_popen_kwargs(self):
+    def get_compile_popen_kwargs(self) -> Dict[str, Any]:
         result = super().get_compile_popen_kwargs()
         result['stdout'] = self._devnull
         return result
 
 
-class SingleDigitVersionMixin:
+class SingleDigitVersionMixin(BaseExecutor, metaclass=ABCMeta):
     version_regex = re.compile(r'.*?(\d+(?:\.\d+)*)', re.DOTALL)

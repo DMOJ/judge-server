875094d51464e3b34158f9157fe6318da28305d9
Quantum <quantum2048@gmail.com>
2021-09-22 03:38:58 -0400

    executors: add types to PyPy executor

--- dmoj/executors/PYPY.py
+++ dmoj/executors/PYPY.py
@@ -1,5 +1,6 @@
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
+from dmoj.executors.base_executor import RuntimeVersionList, VersionTuple
 from dmoj.executors.python_executor import PythonExecutor
 
 
@@ -13,7 +14,7 @@ if sys.version_info.major == 2:
     _pypy_versions: List[Tuple[int, ...]]
 
     @classmethod
-    def parse_version(cls, command, output):
+    def parse_version(cls, command: str, output: str) -> Optional[VersionTuple]:
         try:
             cls._pypy_versions = [tuple(map(int, version.split('.'))) for version in cls.version_regex.findall(output)]
             return cls._pypy_versions[1]
@@ -21,6 +22,6 @@ if sys.version_info.major == 2:
             return None
 
     @classmethod
-    def get_runtime_versions(cls):
+    def get_runtime_versions(cls) -> RuntimeVersionList:
         # A little hack to report implemented Python version too
-        return tuple(list(super().get_runtime_versions()) + [('implementing python', cls._pypy_versions[0])])
+        return list(super().get_runtime_versions()) + [('implementing python', cls._pypy_versions[0])]

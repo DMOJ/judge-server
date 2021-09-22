8e7795fd6371995c47983d199832b7d72c9f144f
Quantum <quantum2048@gmail.com>
2021-09-22 03:00:19 -0400

    executors: add types to D executor

--- dmoj/executors/D.py
+++ dmoj/executors/D.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from dmoj.cptbox.filesystem_policies import ExactFile
 from dmoj.executors.compiled_executor import CompiledExecutor
 
@@ -18,5 +20,8 @@ void main() {
 """
     source_filename_format = 'main.{ext}'
 
-    def get_compile_args(self):
-        return [self.get_command(), '-O', '-inline', '-release', '-w', self._code, '-of%s' % self.problem]
+    def get_compile_args(self) -> List[str]:
+        command = self.get_command()
+        assert command is not None
+        assert self._code is not None
+        return [command, '-O', '-inline', '-release', '-w', self._code, f'-of{self.problem}']

132ca375afb00d52a897ceee892257fa2d8c62d2
Quantum <quantum2048@gmail.com>
2021-09-22 03:36:49 -0400

    executors: add types to Pascal executor

--- dmoj/executors/PAS.py
+++ dmoj/executors/PAS.py
@@ -1,3 +1,6 @@
+from typing import List
+
+from dmoj.cptbox import TracedPopen
 from dmoj.cptbox.filesystem_policies import ExactFile
 from dmoj.executors.compiled_executor import CompiledExecutor
 from dmoj.executors.mixins import NullStdoutMixin
@@ -18,13 +21,16 @@ begin
 end.
 """
 
-    def get_compile_args(self):
-        return [self.get_command(), '-Fe/dev/stderr', '-So', '-O2', self._code]
+    def get_compile_args(self) -> List[str]:
+        command = self.get_command()
+        assert command is not None
+        assert self._code is not None
+        return [command, '-Fe/dev/stderr', '-So', '-O2', self._code]
 
-    def get_compile_output(self, process):
+    def get_compile_output(self, process: TracedPopen) -> bytes:
         output = super().get_compile_output(process)
-        return output if b'Fatal:' in output or b'Warning:' in output or b'Note:' in output else ''
+        return output if b'Fatal:' in output or b'Warning:' in output or b'Note:' in output else b''
 
     @classmethod
-    def get_version_flags(cls, command):
+    def get_version_flags(cls, command: str) -> List[str]:
         return ['-help']

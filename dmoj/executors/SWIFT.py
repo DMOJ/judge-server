f3724f0e06079986a9918bf2b332d75ba7739f80
Quantum <quantum2048@gmail.com>
2021-09-22 03:50:38 -0400

    executors: add types to Swift executor

--- dmoj/executors/SWIFT.py
+++ dmoj/executors/SWIFT.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from dmoj.cptbox.filesystem_policies import ExactFile, RecursiveDir
 from dmoj.executors.compiled_executor import CompiledExecutor
 
@@ -15,5 +17,8 @@ class Executor(CompiledExecutor):
     compiler_required_dirs = ['~/.cache']
     test_program = 'print(readLine()!)'
 
-    def get_compile_args(self):
-        return [self.get_command(), self._code]
+    def get_compile_args(self) -> List[str]:
+        command = self.get_command()
+        assert command is not None
+        assert self._code is not None
+        return [command, self._code]

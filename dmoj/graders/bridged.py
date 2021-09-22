2a22a33b476171feba7fc2339140bb0b2a33faa7
Quantum <quantum2048@gmail.com>
2021-09-22 19:00:38 -0400

    graders: add types to BridgedInteractiveGrader

--- dmoj/graders/bridged.py
+++ dmoj/graders/bridged.py
@@ -1,17 +1,30 @@
 import os
 import shlex
 import subprocess
+from typing import TYPE_CHECKING
 
+from dmoj.checkers import CheckerOutput
+from dmoj.config import ConfigNode
 from dmoj.contrib import contrib_modules
 from dmoj.error import CompileError, InternalError
+from dmoj.executors.base_executor import BaseExecutor
 from dmoj.graders.standard import StandardGrader
 from dmoj.judgeenv import env, get_problem_root
+from dmoj.problem import Problem, TestCase
+from dmoj.result import Result
 from dmoj.utils.helper_files import compile_with_auxiliary_files, mktemp
 from dmoj.utils.unicode import utf8text
 
+if TYPE_CHECKING:
+    from dmoj.judge import JudgeWorker
+
 
 class BridgedInteractiveGrader(StandardGrader):
-    def __init__(self, judge, problem, language, source):
+    handler_data: ConfigNode
+    interactor_binary: BaseExecutor
+    contrib_type: str
+
+    def __init__(self, judge: 'JudgeWorker', problem: Problem, language: str, source: bytes) -> None:
         super().__init__(judge, problem, language, source)
         self.handler_data = self.problem.config.interactive
 
@@ -23,12 +36,13 @@ class BridgedInteractiveGrader(StandardGrader):
 
         self.contrib_type = self.handler_data.get('type', 'default')
         if self.contrib_type not in contrib_modules:
-            raise InternalError('%s is not a valid contrib module' % self.contrib_type)
+            raise InternalError(f'{self.contrib_type} is not a valid contrib module')
 
-    def check_result(self, case, result):
+    def check_result(self, case: TestCase, result: Result) -> CheckerOutput:
         # We parse the return code first in case the grader crashed, so it can raise the IE.
         # Usually a grader crash will result in IR/RTE/TLE,
         # so checking submission return code first will cover up the grader fail.
+        assert self._interactor.stderr is not None
         stderr = self._interactor.stderr.read()
         parsed_result = contrib_modules[self.contrib_type].ContribModule.parse_return_code(
             self._interactor,
@@ -43,7 +57,7 @@ class BridgedInteractiveGrader(StandardGrader):
 
         return (not result.result_flag) and parsed_result
 
-    def _launch_process(self, case):
+    def _launch_process(self, case: TestCase) -> None:
         self._interactor_stdin_pipe, submission_stdout_pipe = os.pipe()
         submission_stdin_pipe, self._interactor_stdout_pipe = os.pipe()
         self._current_proc = self.binary.launch(
@@ -58,7 +72,10 @@ class BridgedInteractiveGrader(StandardGrader):
         os.close(submission_stdin_pipe)
         os.close(submission_stdout_pipe)
 
-    def _interact_with_process(self, case, result, input):
+    def _interact_with_process(self, case: TestCase, result: Result, input: bytes) -> bytes:
+        assert self._current_proc is not None
+        assert self._current_proc.stderr is not None
+
         judge_output = case.output_data()
         # Give TL + 2s by default, so we do not race (and incorrectly throw IE) if submission gets TLE
         self._interactor_time_limit = (self.handler_data.preprocessing_time or 2) + self.problem.time_limit
@@ -97,13 +114,15 @@ class BridgedInteractiveGrader(StandardGrader):
 
             return self._current_proc.stderr.read()
 
-    def _generate_interactor_binary(self):
+    def _generate_interactor_binary(self) -> BaseExecutor:
         files = self.handler_data.files
         if isinstance(files, str):
             filenames = [files]
         elif isinstance(files.unwrap(), list):
             filenames = list(files.unwrap())
-        filenames = [os.path.join(get_problem_root(self.problem.id), f) for f in filenames]
+        problem_root = get_problem_root(self.problem.id)
+        assert problem_root is not None
+        filenames = [os.path.join(problem_root, f) for f in filenames]
         flags = self.handler_data.get('flags', [])
         unbuffered = self.handler_data.get('unbuffered', True)
         return compile_with_auxiliary_files(

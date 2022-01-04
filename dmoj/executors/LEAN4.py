b9f87b3117d102d549cb73f77e0f449f9e995819
int-y1 <jason_yuen2007@hotmail.com>
2022-01-04 04:57:24 -0500

    mypy

--- dmoj/executors/LEAN4.py
+++ dmoj/executors/LEAN4.py
@@ -18,10 +18,13 @@ def main : IO Unit := do
 
     def compile(self) -> str:
         # lean -c f2.c f1.lean && leanc -o f3 f2.c -O3 -DNDEBUG
+        command = self.get_command()
+        assert command is not None
+        assert self._code is not None
         c_file = f'{self.problem}.c'
-        proc1 = self.create_compile_process([self.get_command(), '-c', c_file, self._code])  # type: ignore
+        proc1 = self.create_compile_process([command, '-c', c_file, self._code])
         out1 = self.get_compile_output(proc1)
-        proc2 = self.create_compile_process([f'{self.get_command()}c', '-o', self.problem, c_file, '-O3', '-DNDEBUG'])
+        proc2 = self.create_compile_process([f'{command}c', '-o', self.problem, c_file, '-O3', '-DNDEBUG'])
         out2 = self.get_compile_output(proc2)
         self.warning = b'\n'.join(filter(None, [out1, out2]))
         self._executable = self.get_compiled_file()

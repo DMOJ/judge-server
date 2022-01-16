92a795d7df90cec33306ffe53da9168766509487
Quantum <quantum2048@gmail.com>
2022-01-16 18:09:09 -0500

    cptbox: allow FreeBSD 13 syscalls

--- dmoj/executors/RUBY2.py
+++ dmoj/executors/RUBY2.py
@@ -11,7 +11,7 @@ class Executor(ScriptExecutor):
     test_program = 'puts gets'
     nproc = -1
     command_paths = ['ruby2.%d' % i for i in reversed(range(0, 8))] + ['ruby2%d' % i for i in reversed(range(0, 8))]
-    syscalls = ['thr_set_name', 'eventfd2']
+    syscalls = ['thr_set_name', 'eventfd2', 'specialfd']
     fs = [ExactFile('/proc/self/loginuid')]
 
     def get_fs(self):

c56fa02d495f1cba57d731edc5268f74e6ffe956
Tudor Brindus <me@tbrindus.ca>
2022-10-02 00:46:43 -0400

    executors/GO: allow Go to tell if it's running with THPs

--- dmoj/executors/GO.py
+++ dmoj/executors/GO.py
@@ -1,6 +1,7 @@
 import os
 import re
 
+from dmoj.cptbox.filesystem_policies import ExactFile
 from dmoj.error import CompileError
 from dmoj.executors.compiled_executor import CompiledExecutor
 
@@ -21,6 +22,11 @@ class Executor(CompiledExecutor):
     command = 'go'
     syscalls = ['mincore', 'epoll_create1', 'epoll_ctl', 'epoll_pwait', 'pselect6', 'mlock', 'setrlimit']
     compiler_syscalls = ['setrlimit']
+    fs = [
+        # Go will start without THP information, but has some tuning for when
+        # it is available -- so let's allow it to tell.
+        ExactFile('/sys/kernel/mm/transparent_hugepage/hpage_pmd_size'),
+    ]
     test_name = 'echo'
     test_program = """\
 package main

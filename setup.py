b3a2573d478620d757913b0a1fb9996fa038d8b4
Tudor Brindus <me@tbrindus.ca>
2022-11-01 01:14:22 -0400

    Report Python 3.11 support

--- setup.py
+++ setup.py
@@ -198,6 +198,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Education',
         'Topic :: Software Development',
     ],

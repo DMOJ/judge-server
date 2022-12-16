f59531265b63e3ed067e10f490a07035fae77b6b
Tudor Brindus <me@tbrindus.ca>
2022-12-16 14:17:04 -0500

    executors/RUBY: extend search path to cover Ruby 3.1

--- dmoj/executors/RUBY.py
+++ dmoj/executors/RUBY.py
@@ -10,7 +10,9 @@ class Executor(ScriptExecutor):
     test_program = 'puts gets'
     nproc = -1
     command_paths = (
-        ['ruby3.0'] + ['ruby2.%d' % i for i in reversed(range(0, 8))] + ['ruby2%d' % i for i in reversed(range(0, 8))]
+        ['ruby3.%d' % i for i in reversed(range(0, 1))]
+        + ['ruby2.%d' % i for i in reversed(range(0, 8))]
+        + ['ruby2%d' % i for i in reversed(range(0, 8))]
     )
     syscalls = ['thr_set_name', 'eventfd2', 'specialfd']
     fs = [ExactFile('/proc/self/loginuid')]

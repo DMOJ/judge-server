c84f519cda18e6cefa13e31fd6d25dc71a3343af
Quantum <quantum2048@gmail.com>
2022-12-28 01:40:54 -0500

    Switch GASARM to use PlatformARMMixin

--- dmoj/executors/GASARM.py
+++ dmoj/executors/GASARM.py
@@ -1,19 +1,8 @@
-from dmoj.cptbox import PTBOX_ABI_ARM
-from dmoj.executors.asm_executor import GASExecutor
-from dmoj.judgeenv import env
+from dmoj.executors.asm_executor import GASExecutor, PlatformARMMixin
 
 
-class Executor(GASExecutor):
-    abi = PTBOX_ABI_ARM
+class Executor(PlatformARMMixin, GASExecutor):
     as_name = 'as_arm'
-    ld_name = 'ld_arm'
-    ld_m = 'armelf_linux_eabi'
-    platform_prefixes = ['arm-linux-gnueabihf']
-
-    qemu_path = env.runtime.qemu_arm
-    dynamic_linker = env.runtime['ld.so_arm'] or '/lib/ld-linux-armhf.so.3'
-    crt_pre = env.runtime.crt_pre_arm or ['/usr/lib/arm-linux-gnueabihf/crt1.o', '/usr/lib/arm-linux-gnueabihf/crti.o']
-    crt_post = env.runtime.crt_post_arm or ['/usr/lib/arm-linux-gnueabihf/crtn.o']
     test_program = r"""
 .global _start
 _start:

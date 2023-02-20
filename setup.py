9c1a7a6eeb53e0fff96d24b28cd45a5568651b16
Tudor Brindus <me@tbrindus.ca>
2023-02-20 17:12:54 -0500

    Bump version to 4.1.0

--- setup.py
+++ setup.py
@@ -161,7 +161,7 @@ with io.open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf
 
 setup(
     name='dmoj',
-    version='4.0.1',
+    version='4.1.0',
     packages=find_packages(),
     package_data={
         'dmoj.cptbox': ['syscalls/aliases.list', 'syscalls/*.tbl'],

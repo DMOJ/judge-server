4eb00500cf9396d50eda47cf32ee6c234537cbe8
Quantum <quantum2048@gmail.com>
2023-03-13 21:59:34 -0400

    Add C++ name demangling helper

---
+++ dmoj/utils/cpp_demangle.py
@@ -0,0 +1,39 @@
+from ctypes import CDLL, POINTER, byref, c_char_p, c_int, c_size_t, c_void_p, string_at
+from ctypes.util import find_library
+
+__all__ = ['demangle']
+
+libc = CDLL(find_library('c'))
+libstdcxx = CDLL(find_library('stdc++'))
+
+# Signature: char *__cxa_demangle(const char *mangled_name, char *output_buffer, size_t *length, int *status)
+__cxa_demangle = libstdcxx.__cxa_demangle
+__cxa_demangle.argtypes = [c_char_p, c_char_p, POINTER(c_size_t), POINTER(c_int)]
+__cxa_demangle.restype = c_void_p
+
+# Signature: void free(void *ptr)
+free = libc.free
+free.argtypes = [c_void_p]
+free.restype = None
+
+
+def demangle(name: bytes) -> bytes:
+    status = c_int()
+    result = __cxa_demangle(name, None, None, byref(status))
+
+    if result:
+        value = string_at(result)
+        free(result)
+        return value
+
+    if status.value == -1:
+        # Memory allocation failure
+        raise MemoryError()
+    elif status.value == -2:
+        # `mangled_name` is not a valid mangled name
+        return name
+    elif status.value == -3:
+        # Invalid argument
+        raise RuntimeError('__cxa_demangle reported invalid argument')
+    else:
+        raise RuntimeError(f'unknown return value {status.value} from __cxa_demangle')

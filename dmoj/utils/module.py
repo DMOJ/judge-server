36e172d482db3939e3084a7e1989557f5a30b71c
kiritofeng <me@kirito.ca>
2023-03-21 22:35:21 -0400

    utils: allow `load_module_from_file` to take `Path` object

--- dmoj/utils/module.py
+++ dmoj/utils/module.py
@@ -1,19 +1,19 @@
-import os
 import types
-from typing import Any, Optional
+from pathlib import Path
+from typing import Any, Optional, Union
 
 
-def load_module(name: str, code: str, filename: Optional[str] = None) -> Any:
+def load_module(name: str, code: str, filename: Optional[Union[str, Path]] = None) -> Any:
     mod = types.ModuleType(name)
     if filename is not None:
-        mod.__file__ = filename
+        mod.__file__ = str(filename)
     exec(compile(code, filename or '<string>', 'exec'), mod.__dict__)
     return mod
 
 
-def load_module_from_file(filename: str) -> Any:
-    path, name = os.path.split(filename)
-    name, ext = os.path.splitext(name)
+def load_module_from_file(filename: Union[str, Path]) -> Any:
+    filename = Path(filename).resolve(strict=True)
+    name = filename.stem
 
     with open(filename) as f:
-        return load_module(name, f.read(), os.path.abspath(filename))
+        return load_module(name, f.read(), filename)

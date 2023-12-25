e17de35f0d366889581ea04fba3d1b38157a260a
kiritofeng <me@kirito.ca>
2023-12-25 16:32:45 -0500

    executors: fix types in Clang executors

--- dmoj/executors/clang_executor.py
+++ dmoj/executors/clang_executor.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from dmoj.executors.base_executor import VersionFlags
 from .gcc_executor import GCCExecutor, MAX_ERRORS
 
 CLANG_VERSIONS: List[str] = ['3.9', '3.8', '3.7', '3.6', '3.5']
@@ -12,5 +13,5 @@ class ClangExecutor(GCCExecutor):
         return self.flags + [f'-ferror-limit={MAX_ERRORS}']
 
     @classmethod
-    def get_version_flags(cls, command: str) -> List[str]:
+    def get_version_flags(cls, command: str) -> List[VersionFlags]:
         return ['--version']

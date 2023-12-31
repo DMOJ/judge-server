99368d0b2a14cfd462d71b8cc2eb8d5cbdbff874
Tudor Brindus <me@tbrindus.ca>
2023-12-31 13:45:08 -0500

    .

--- dmoj/graders/base.py
+++ dmoj/graders/base.py
@@ -37,21 +37,8 @@ class BaseGrader:
     def _generate_binary(self) -> BaseExecutor:
         raise NotImplementedError
 
-<<<<<<< HEAD
-    def abort_grading(self) -> None:
-        self._abort_requested = True
-        if self._current_proc:
-            try:
-                self._current_proc.kill()
-            except OSError:
-                pass
-
     def _resolve_testcases(self, cfg, batch_no=0) -> List[BaseTestCase]:
         cases: List[BaseTestCase] = []
-=======
-    def _resolve_testcases(self, cfg, batch_no=0):
-        cases = []
->>>>>>> 662a3127 (judge: implement instant aborts)
         for case_config in cfg:
             if 'batched' in case_config.raw_config:
                 self._batch_counter += 1

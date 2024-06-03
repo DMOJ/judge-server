3dc2c298c517b5865cc1eacf23598769b9cd54e7
Tudor Brindus <me@tbrindus.ca>
2024-06-02 23:11:13 -0400

    executors/python: `pytb` is aliased to `py3tb`; use `py2tb` explicitly

--- dmoj/executors/PY2.py
+++ dmoj/executors/PY2.py
@@ -4,7 +4,7 @@ from dmoj.executors.python_executor import PythonExecutor
 class Executor(PythonExecutor):
     command = 'python'
     command_paths = ['python2.7', 'python2', 'python']
-    pygments_traceback_lexer = 'pytb'
+    pygments_traceback_lexer = 'py2tb'
     test_program = """
 import sys
 if sys.version_info.major == 2:

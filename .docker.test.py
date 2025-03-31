ff54e799f4d4ca2c7536e7f4110632f9545b1857
Roger Fu <me@kirito.ca>
2025-03-31 05:05:05 -0400

    ci: Allow for SWIFT to fail; refs DMOJ/runtimes-docker#83.

--- .docker.test.py
+++ .docker.test.py
@@ -6,12 +6,12 @@ from dmoj.citest import ci_test
 from dmoj.executors import get_available
 
 arch = platform.machine()
-ALLOW_FAIL = {'GASARM'}
+ALLOW_FAIL = {'GASARM', 'SWIFT'}
 EXECUTORS = get_available()
 
 if arch == 'aarch64':
     ALLOW_FAIL -= {'GASARM'}
-    ALLOW_FAIL |= {'D', 'GAS32', 'GAS64', 'LEAN4', 'NASM', 'NASM64', 'SWIFT', 'TUR'}
+    ALLOW_FAIL |= {'D', 'GAS32', 'GAS64', 'LEAN4', 'NASM', 'NASM64', 'TUR'}
 elif arch != 'x86_64':
     raise AssertionError('invalid architecture')
 

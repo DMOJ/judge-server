f0945989fdb7b05190d370b7ad5afed4ef481983
kiritofeng <me@kirito.ca>
2026-03-15 00:05:36 -0400

    ci: No longer allow Scala to fail

--- .docker.test.py
+++ .docker.test.py
@@ -6,7 +6,7 @@ from dmoj.citest import ci_test
 from dmoj.executors import get_available
 
 arch = platform.machine()
-ALLOW_FAIL = {'GASARM', 'ICK', 'SCALA', 'SWIFT'}
+ALLOW_FAIL = {'GASARM', 'ICK', 'SWIFT'}
 EXECUTORS = get_available()
 
 if arch == 'aarch64':

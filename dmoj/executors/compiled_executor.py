ddb786468e9ae1cb870c5acc47501ef376ec092d
Roger Fu <me@kirito.ca>
2026-06-08 18:29:47 -0400

    nit: Fix units in compiler OLE error message
    "Might as well write it properly" - @quantum5

--- dmoj/executors/compiled_executor.py
+++ dmoj/executors/compiled_executor.py
@@ -200,7 +200,7 @@ class CompiledExecutor(BaseExecutor, metaclass=_CompiledExecutorMeta):
         try:
             output = safe_communicate(process, None, outlimit=limit, errlimit=limit)[self.compile_output_index]
         except OutputLimitExceeded:
-            output = b'compiler output too long (> %d kb)' % (limit // 1024)
+            output = b'compiler output too long (> %d kiB)' % (limit // 1024)
 
         if self.is_failed_compile(process):
             if process.is_tle:
